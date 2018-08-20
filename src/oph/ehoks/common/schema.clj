22439c8730d0eb02ff10fcb99a6b1f6cfba89dc4
Matti Ahinko <matti.ahinko@gmail.com>
2018-08-20 15:45:44 +0300

    Add common schema

---
+++ src/oph/ehoks/common/schema.clj
@@ -0,0 +1,13 @@
+(ns oph.ehoks.common.schema
+  (:require [schema.core :as s]))
+
+(s/defschema Translated
+  "Translated string"
+  {:fi s/Str
+   (s/optional-key :en) (s/maybe s/Str)
+   (s/optional-key :sv) (s/maybe s/Str)})
+
+(s/defschema Information
+  "Basic service and process information"
+  {:basic-information Translated
+   :hoks-process Translated})
