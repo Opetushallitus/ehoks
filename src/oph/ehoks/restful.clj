1aea9cb8402d435a511548bd5b8147c96236135b
Matti Ahinko <matti.ahinko@gmail.com>
2018-08-22 09:40:13 +0300

    Rename arg

--- src/oph/ehoks/restful.clj
+++ src/oph/ehoks/restful.clj
@@ -1,10 +1,10 @@
 (ns oph.ehoks.restful
   (:require [ring.util.http-response :refer [ok]]))
 
-(defn response [body & meta]
-  (let [meta-map (apply hash-map meta)]
+(defn response [body & meta-data]
+  (let [meta-map (apply hash-map meta-data)]
     {:meta (get :meta meta-map {})
      :data body}))
 
-(defn rest-ok [body & data]
-  (ok (apply response body data)))
+(defn rest-ok [body & meta-data]
+  (ok (apply response body meta-data)))
