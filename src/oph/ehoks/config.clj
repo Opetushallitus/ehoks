5d1030c8777805a84336a04b7ed88291cbbb0838
Matti Ahinko <matti.ahinko@gmail.com>
2018-08-22 08:42:34 +0300

    Rename symbol and use param

--- src/oph/ehoks/config.clj
+++ src/oph/ehoks/config.clj
@@ -3,13 +3,13 @@
             [clojure.java.io :as io]
             [clojure.edn :as edn]))
 
-(def ^:private file (get env :config "config/prod.edn"))
+(def ^:private env-file (get env :config "config/prod.edn"))
 
 (def ^:private defaults-file "config/defaults.edn")
 
-(defn- load-config []
+(defn- load-config [file]
   (with-open [reader (io/reader file)]
     (edn/read (java.io.PushbackReader. reader))))
 
 (def config (merge (load-config defaults-file)
-                   (load-config file)))
+                   (load-config env-file)))
