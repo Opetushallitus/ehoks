1a47c2532a0c1484ec5bbc4020043c7d764c1085
Matti Ahinko <matti.ahinko@gmail.com>
2018-08-22 09:15:04 +0300

    Move tests to own handlers

--- test/oph/ehoks/handler_test.clj
+++ test/oph/ehoks/handler_test.clj
@@ -1,41 +1,7 @@
 (ns oph.ehoks.handler-test
-  (:require [cheshire.core :as cheshire]
-            [clojure.test :refer [deftest testing is]]
+  (:require [clojure.test :refer [deftest testing is]]
             [oph.ehoks.handler :refer [app]]
             [ring.mock.request :as mock]))
-
-(defn parse-body [body]
-  (cheshire/parse-string (slurp body) true))
-
-(deftest education-info
-  (testing "GET education info"
-    (let [response (app (mock/request :get "/api/v1/education/info/"))
-          body (parse-body (:body response))
-          info (first (:data body))]
-      (is (= (:status response) 200))
-      (is (= (count (:data body)) 1))
-      (is (some? (get-in info [:basic-information :fi])))
-      (is (some? (get-in info [:hoks-process :fi]))))))
-
-(deftest work-info
-  (testing "GET work info"
-    (let [response (app (mock/request :get "/api/v1/work/info/"))
-          body (parse-body (:body response))
-          info (first (:data body))]
-      (is (= (:status response) 200))
-      (is (= (count (:data body)) 1))
-      (is (some? (get-in info [:basic-information :fi])))
-      (is (some? (get-in info [:hoks-process :fi]))))))
-
-(deftest student-info
-  (testing "GET student info"
-    (let [response (app (mock/request :get "/api/v1/student/info/"))
-          body (parse-body (:body response))
-          info (first (:data body))]
-      (is (= (:status response) 200))
-      (is (= (count (:data body)) 1))
-      (is (some? (get-in info [:basic-information :fi])))
-      (is (some? (get-in info [:hoks-process :fi]))))))
 
 (deftest not-found
   (testing "GET route which does not exists"
