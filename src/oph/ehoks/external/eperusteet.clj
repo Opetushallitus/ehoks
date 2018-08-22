c0f49a6fb3f45ee65d58046ec95decfd7a0ecd92
Matti Ahinko <matti.ahinko@gmail.com>
2018-08-22 09:16:21 +0300

    Rename arg

--- src/oph/ehoks/external/eperusteet.clj
+++ src/oph/ehoks/external/eperusteet.clj
@@ -3,9 +3,9 @@
             [clj-http.client :as client]
             [cheshire.core :as cheshire]))
 
-(defn search-qualification-info [name]
+(defn search-qualification-info [quali-name]
   (-> (client/get (format "%s/perusteet/info" (:eperusteet-url config))
-                  {:query-params {"nimi" name}})
+                  {:query-params {"nimi" quali-name}})
       :body
       (cheshire/parse-string true)
       :data))
