39e966aee0a8b108fdfe882030f8fb4d5faf70a0
Matti Ahinko <matti.ahinko@gmail.com>
2018-08-28 15:50:54 +0300

    Add auth handler

--- src/oph/ehoks/handler.clj
+++ src/oph/ehoks/handler.clj
@@ -6,6 +6,7 @@
             [oph.ehoks.education.handler :as education-handler]
             [oph.ehoks.work.handler :as work-handler]
             [oph.ehoks.student.handler :as student-handler]
+            [oph.ehoks.auth.auth-handler :as auth-handler]
             [oph.ehoks.config :refer [config]]
             [oph.ehoks.redis :refer [redis-store]]))
 
@@ -23,7 +24,8 @@
 
       education-handler/routes
       work-handler/routes
-      student-handler/routes)
+      student-handler/routes
+      auth-handler/routes)
 
     (context "*" []
       (GET "*" []
