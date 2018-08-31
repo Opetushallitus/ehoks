0c6c96d8660a5d9a77969473e8d327e19d08ee4e
Matti Ahinko <matti.ahinko@gmail.com>
2018-08-31 13:58:01 +0300

    Add exclusions and set dependencies for them

--- project.clj
+++ project.clj
@@ -3,25 +3,29 @@
   :min-lein-version "2.8.1"
   :pedantic? :abort
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 ; Force dependency versions of dependenciens for avoiding
+                 ; confusing overrides
+                 [joda-time "2.10"]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [ring/ring-codec "1.1.1"]
+                 [commons-codec "1.11"]
+
                  [metosin/compojure-api "2.0.0-alpha23"
-                  :exclusions [ring/ring-core ring/ring-codec
-                               commons-fileupload joda-time clj-time
-                               com.fasterxml.jackson.core/jackson-core]]
-                 [ring/ring-jetty-adapter "1.6.3"
-                  :exclusions [ring/ring-core commons-fileupload joda-time
-                               ring/ring-servlet]]
+                  :exclusions [org.clojure/tools.namespace
+                               ring/ring-codec joda-time]]
+                 [ring/ring-jetty-adapter "1.6.3"]
                  [org.flywaydb/flyway-core "5.1.4"]
                  [org.clojure/java.jdbc "0.7.8"]
                  [org.postgresql/postgresql "42.2.5"]
                  [com.layerware/hugsql "0.4.9"]
                  [environ "1.1.0"]
-                 [clj-http "3.9.1"]
-                 [com.taoensso/carmine "2.18.1"]
+                 [clj-http "3.9.1" :exclusions [commons-codec]]
+                 [com.taoensso/carmine "2.18.1" :exclusions [commons-codec]]
                  [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.12.4"]
             [lein-cljfmt "0.6.0" :exclusions [org.clojure/tools.cli]]
             [lein-kibit "0.1.6"]
-            [lein-bikeshed "0.5.1"]
+            [lein-bikeshed "0.5.1" :exclusions [org.clojure/tools.namespace]]
             [jonase/eastwood "0.2.9"]
             [lein-auto "0.1.3"]
             [lein-ancient "0.6.15"]]
@@ -36,9 +40,10 @@
   :cljfmt {:indents {#".*" [[:block 0]]}}
   :profiles {:dev {:dependencies
                    [[javax.servlet/javax.servlet-api "4.0.1"]
-                    [cheshire "5.8.0"]
-                    [ring/ring-mock "0.3.2"]
+                    [cheshire "5.8.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
+                    [ring/ring-mock "0.3.2" :exclusions [ring/ring-codec]]
                     [ring/ring-devel "1.7.0-RC2"
-                     :exclusions [ring/ring-core ring/ring-codec joda-time]]]
+                     :exclusions [org.clojure/tools.namespace
+                                  ring/ring-core ring/ring-codec joda-time]]]
                    :resource-paths ["resources/dev" "resources/dev/src"]
                    :ring {:handler oph.ehoks.dev-server/dev-app}}})
