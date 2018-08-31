00a350dd7d4217b04799bfe7d3527b5f6a9e198b
Matti Ahinko <matti.ahinko@gmail.com>
2018-08-31 10:11:32 +0300

    Change to use given file

--- src/oph/ehoks/config.clj
+++ src/oph/ehoks/config.clj
@@ -1,15 +1,11 @@
 (ns oph.ehoks.config
-  (:require [environ.core :refer [env]]
-            [clojure.java.io :as io]
+  (:require [clojure.java.io :as io]
             [clojure.edn :as edn]))
 
-(def ^:private defaults-file "config/defaults.edn")
+(def ^:private default-file "config/default.edn")
 
 (defn- load-config [file]
   (with-open [reader (io/reader file)]
     (edn/read (java.io.PushbackReader. reader))))
 
-(def ^:private default-config (load-config defaults-file))
-
-(def config (merge default-config
-                   (select-keys env (keys default-config))))
+(def config (load-config (or (System/getenv "CONFIG_FILE") default-file)))
