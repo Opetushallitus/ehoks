dbc0da833902e484ac0917cede6bd8a9a4629443
Matti Ahinko <matti.ahinko@gmail.com>
2018-08-31 09:13:55 +0300

    Exlude overlapping libraries

--- project.clj
+++ project.clj
@@ -3,8 +3,13 @@
   :min-lein-version "2.8.1"
   :pedantic? :abort
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [metosin/compojure-api "2.0.0-alpha23"]
-                 [ring/ring-jetty-adapter "1.6.3"]
+                 [metosin/compojure-api "2.0.0-alpha23"
+                  :exclusions [ring/ring-core ring/ring-codec
+                               commons-fileupload joda-time clj-time
+                               com.fasterxml.jackson.core/jackson-core]]
+                 [ring/ring-jetty-adapter "1.6.3"
+                  :exclusions [ring/ring-core commons-fileupload joda-time
+                               ring/ring-servlet]]
                  [org.flywaydb/flyway-core "5.1.4"]
                  [org.clojure/java.jdbc "0.7.6"]
                  [org.postgresql/postgresql "42.2.2"]
@@ -14,7 +19,7 @@
                  [com.taoensso/carmine "2.18.1"]
                  [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.12.4"]
-            [lein-cljfmt "0.6.0"]
+            [lein-cljfmt "0.6.0" :exclusions [org.clojure/tools.cli]]
             [lein-kibit "0.1.6"]
             [lein-bikeshed "0.5.1"]
             [jonase/eastwood "0.2.9"]
@@ -29,9 +34,11 @@
                         ["eastwood"]
                         ["cljfmt" "check"]]}
   :cljfmt {:indents {#".*" [[:block 0]]}}
-  :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
-                                  [cheshire "5.8.0"]
-                                  [ring/ring-mock "0.3.2"]
-                                  [ring/ring-devel "1.7.0-RC1"]]
+  :profiles {:dev {:dependencies
+                   [[javax.servlet/javax.servlet-api "3.1.0"]
+                    [cheshire "5.8.0"]
+                    [ring/ring-mock "0.3.2"]
+                    [ring/ring-devel "1.7.0-RC2"
+                     :exclusions [ring/ring-core ring/ring-codec joda-time]]]
                    :resource-paths ["resources/dev" "resources/dev/src"]
                    :ring {:handler oph.ehoks.dev-server/dev-app}}})
