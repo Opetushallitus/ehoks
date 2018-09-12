703f4d54fa54a240c65370f1f283659961fdaca7
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-12 14:23:00 +0300

    Change to use helper function

--- src/oph/ehoks/external/oppijanumerorekisteri.clj
+++ src/oph/ehoks/external/oppijanumerorekisteri.clj
@@ -1,13 +1,8 @@
 (ns oph.ehoks.external.oppijanumerorekisteri
   (:require [oph.ehoks.config :refer [config]]
-            [clj-http.client :as client]
-            [cheshire.core :as cheshire]))
+            [oph.ehoks.external.connection :as connection]))
 
 (defn find-student-by-nat-id [nat-id]
-  (-> (client/get (format "%s/henkilo/"
-                          (:oppijanumerorekisteri-url config))
-                  {:query-params {"hetu" nat-id}
-                   :headers {"External-Permission-Service" "KOSKI"}})
-      :body
-      (cheshire/parse-string true)
-      :data))
+  (connection/api-get (format "%s/henkilo/"
+                              (:oppijanumerorekisteri-url config))
+                      {:query-params {"hetu" nat-id}}))
