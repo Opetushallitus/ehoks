be901ca4971692d5ee6f3b7b88b74877fc65f9d0
Silvast <anne-mari.silvast@gofore.com>
2018-09-13 15:25:35 +0300

    eh-63: corrected indentation

--- src/oph/ehoks/external/localization.clj
+++ src/oph/ehoks/external/localization.clj
@@ -1,10 +1,10 @@
 (ns oph.ehoks.external.localization
-    (:require [oph.ehoks.config :refer [config]]
-              [clj-http.client :as client]
-              [cheshire.core :as cheshire]))
-  
+  (:require [oph.ehoks.config :refer [config]]
+            [clj-http.client :as client]
+            [cheshire.core :as cheshire]))
+
 (defn get-localization-results  [& {:keys [category] :or {category "ehoks"}}]
-    (-> (client/get (format "%s/cxf/rest/v1/localisation" (:localization-url config))
-    {:query-params {"category" category}})
-    :body
-    (cheshire/parse-string true)))
+  (-> (client/get (format "%s/cxf/rest/v1/localisation" (:localization-url config))
+                  {:query-params {"category" category}})
+      :body
+      (cheshire/parse-string true)))
