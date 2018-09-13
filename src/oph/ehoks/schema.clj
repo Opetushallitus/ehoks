6d272075ec8bbc2ef191b8922b54ed507ed4f621
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-13 16:46:45 +0300

    Add full user info

--- src/oph/ehoks/schema.clj
+++ src/oph/ehoks/schema.clj
@@ -6,3 +6,13 @@
              {:first-name s/Str
               :common-name s/Str
               :surname s/Str})
+
+(s/defschema UserInfo
+  "Full user info"
+  {:first-names s/Str
+   :surname s/Str
+   :common-name s/Str
+   :oid s/Str
+   (s/optional-key :contact-values-group) [{:id s/Int
+                                            :contact [{:value s/Str
+                                                       :type s/Str}]}]})
