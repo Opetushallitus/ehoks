44886728a8e13801ba0a059b377b997e43785d94
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-13 17:02:03 +0300

    Fix indent

--- src/oph/ehoks/schema.clj
+++ src/oph/ehoks/schema.clj
@@ -2,17 +2,18 @@
   (:require [schema.core :as s]))
 
 (s/defschema User
-  "User"
-  {:first-name s/Str
-   :common-name s/Str
-   :surname s/Str})
+             "User"
+             {:first-name s/Str
+              :common-name s/Str
+              :surname s/Str})
 
 (s/defschema UserInfo
-  "Full user info"
-  {:first-names s/Str
-   :surname s/Str
-   :common-name s/Str
-   :oid s/Str
-   (s/optional-key :contact-values-group) [{:id s/Int
-                                            :contact [{:value s/Str
-                                                       :type s/Str}]}]})
+             "Full user info"
+             {:first-names s/Str
+              :surname s/Str
+              :common-name s/Str
+              :oid s/Str
+              (s/optional-key :contact-values-group)
+              [{:id s/Int
+                :contact [{:value s/Str
+                           :type s/Str}]}]})
