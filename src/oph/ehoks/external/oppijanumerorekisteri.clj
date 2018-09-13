e073247e78e734d8c79ee1992b4df6682baa481b
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-13 13:00:04 +0300

    Add using path

--- src/oph/ehoks/external/oppijanumerorekisteri.clj
+++ src/oph/ehoks/external/oppijanumerorekisteri.clj
@@ -3,6 +3,7 @@
             [oph.ehoks.external.connection :as connection]))
 
 (defn find-student-by-nat-id [nat-id]
-  (connection/api-get (format "%s/henkilo/"
-                              (:oppijanumerorekisteri-url config))
-                      {:query-params {:hetu nat-id}}))
+  (connection/api-get
+    (:oppijanumerorekisteri-url config)
+    "henkilo"
+    {:query-params {:hetu nat-id}}))
