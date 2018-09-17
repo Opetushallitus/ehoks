c1b9e465df2688ccf8c42dfd419742c153396aa0
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-17 10:19:57 +0300

    Move updating session user info

--- src/oph/ehoks/auth/handler.clj
+++ src/oph/ehoks/auth/handler.clj
@@ -23,6 +23,23 @@
           (rest/rest-ok [(onr/convert-student-info user-info)])
           (throw (ex-info "External system error" user-info-response)))))
 
+    (c-api/POST "/update-user-info" [:as request]
+      :summary "Updates session user info from Oppijanumerorekisteri"
+      :return (rest/response [schema/User])
+      (let [session-user (get-in request [:session :user])
+            user-info-response (onr/find-student-by-nat-id (:hetu session-user))
+            user-info (get-in user-info-response [:body :results])
+            oid (:oidHenkilo (first user-info))]
+        (when-not (= (:status user-info-response) 200)
+          (throw (ex-info "External integration error" user-info-response)))
+        (if (seq user-info)
+          (assoc-in
+            (rest/rest-ok [(-> (get-in request [:session :user])
+                               (select-keys [:first-name :common-name :surname])
+                               (assoc :oid oid))])
+            [:session :user :oid] oid)
+          (throw (ex-info "No user found" user-info-response)))))
+
     (c-api/GET "/opintopolku/" [:as request]
       :summary "Get current Opintopolku session"
       :return (rest/response [schema/User] :opintopolku-login-url s/Str)
@@ -42,17 +59,10 @@
       :summary "Creates new Opintopolku session and redirects to frontend"
       :description "Creates new Opintopolku session. After storing session
                     http status 'See Other' (303) will be returned with url of
-                    frontend in configuration.
-                    User info is being downloaded from Oppijanumerorekisteri."
+                    frontend in configuration."
       (when (not= (get-in request [:headers "referer"])
                   (:opintopolku-login-url config))
         (response/bad-request! "Misconfigured authentication"))
-      (let [values (opintopolku/parse (:form-params request))
-            user-info-response (onr/find-student-by-nat-id (:hetu values))
-            user-info (get-in user-info-response [:body :results])
-            user (assoc values :oid (:oidHenkilo (first user-info)))]
-        (if (and (= (:status user-info-response) 200)
-                 (seq user-info))
-          (assoc-in (response/see-other (:frontend-url config))
-                    [:session :user] user)
-          (throw (ex-info "No user found" user-info-response)))))))
+      (let [user (opintopolku/parse (:form-params request))]
+        (assoc-in (response/see-other (:frontend-url config))
+                  [:session :user] user)))))
