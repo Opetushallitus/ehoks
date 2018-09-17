226dc6e936f49d44b7610d8350aaf5f57432d608
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-17 09:55:16 +0300

    Change requires to as

--- src/oph/ehoks/auth/handler.clj
+++ src/oph/ehoks/auth/handler.clj
@@ -1,46 +1,44 @@
 (ns oph.ehoks.auth.handler
-  (:require [compojure.api.sweet :refer [context GET POST DELETE]]
-            [ring.util.http-response :refer [bad-request! see-other]]
+  (:require [compojure.api.sweet :as c-api]
+            [ring.util.http-response :as response]
             [schema.core :as s]
             [oph.ehoks.schema :as schema]
-            [oph.ehoks.restful :refer [response rest-ok]]
+            [oph.ehoks.restful :as rest]
             [oph.ehoks.config :refer [config]]
             [oph.ehoks.auth.opintopolku :as opintopolku]
-            [oph.ehoks.external.oppijanumerorekisteri
-             :refer [find-student-by-nat-id convert-student-info
-                     find-student-by-oid]]))
+            [oph.ehoks.external.oppijanumerorekisteri :as onr]))
 
 (def routes
-  (context "/session" []
+  (c-api/context "/session" []
 
-    (GET "/user-info" [:as request]
+    (c-api/GET "/user-info" [:as request]
       :summary "Get current user info"
-      :return (response [schema/UserInfo])
+      :return (rest/response [schema/UserInfo])
       (let [user-info-response
-            (find-student-by-oid
+            (onr/find-student-by-oid
               (get-in request [:session :user :oid]))
             user-info (:body user-info-response)]
         (if (and (= (:status user-info-response) 200)
                  (seq user-info))
-          (rest-ok [(convert-student-info user-info)])
+          (rest/rest-ok [(onr/convert-student-info user-info)])
           (throw (ex-info "External system error" user-info-response)))))
 
-    (GET "/opintopolku/" [:as request]
+    (c-api/GET "/opintopolku/" [:as request]
       :summary "Get current Opintopolku session"
-      :return (response [schema/User] :opintopolku-login-url s/Str)
+      :return (rest/response [schema/User] :opintopolku-login-url s/Str)
       (let [{{:keys [user]} :session} request]
-        (rest-ok
+        (rest/rest-ok
           (if (some? user)
             [(select-keys user [:first-name :common-name :surname])]
             [])
           :opintopolku-login-url (:opintopolku-login-url config))))
 
-    (DELETE "/opintopolku/" []
+    (c-api/DELETE "/opintopolku/" []
       :summary "Delete Opintopolku session (logout)"
-      :return (response [s/Any])
-      (assoc (rest-ok []) :session nil))
+      :return (rest/response [s/Any])
+      (assoc (rest/rest-ok []) :session nil))
 
-    (POST "/opintopolku/" [:as request]
+    (c-api/POST "/opintopolku/" [:as request]
       :summary "Creates new Opintopolku session and redirects to frontend"
       :description "Creates new Opintopolku session. After storing session
                     http status 'See Other' (303) will be returned with url of
@@ -48,13 +46,13 @@
                     User info is being downloaded from Oppijanumerorekisteri."
       (when (not= (get-in request [:headers "referer"])
                   (:opintopolku-login-url config))
-        (bad-request! "Misconfigured authentication"))
+        (response/bad-request! "Misconfigured authentication"))
       (let [values (opintopolku/parse (:form-params request))
-            user-info-response (find-student-by-nat-id (:hetu values))
+            user-info-response (onr/find-student-by-nat-id (:hetu values))
             user-info (get-in user-info-response [:body :results])
             user (assoc values :oid (:oidHenkilo (first user-info)))]
         (if (and (= (:status user-info-response) 200)
                  (seq user-info))
-          (assoc-in (see-other (:frontend-url config))
+          (assoc-in (response/see-other (:frontend-url config))
                     [:session :user] user)
           (throw (ex-info "No user found" user-info-response)))))))
