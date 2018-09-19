0506f31801b2e67151e6163887e7fe2c5088623e
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-19 10:20:37 +0300

    Add session handling routes

--- src/oph/ehoks/handler.clj
+++ src/oph/ehoks/handler.clj
@@ -37,6 +37,8 @@
 
 (def public-routes
   [{:uri #"/ehoks/api/v1/session/opintopolku/" :request-method :get}
+   {:uri #"/ehoks/api/v1/session/opintopolku/" :request-method :delete}
+   {:uri #"/ehoks/api/v1/session/opintopolku/" :request-method :options}
    {:uri #"/ehoks/api/v1/session/opintopolku/" :request-method :post}
    {:uri #"/ehoks/api/v1/healthcheck" :request-method :get}])
 
