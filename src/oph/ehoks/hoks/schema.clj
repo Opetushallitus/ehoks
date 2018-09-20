8e5beaac8b3027e150dea347e07b3bc9c7f2a615
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-20 08:56:32 +0300

    Reuse schemas

--- src/oph/ehoks/hoks/schema.clj
+++ src/oph/ehoks/hoks/schema.clj
@@ -1,19 +1,21 @@
 (ns oph.ehoks.hoks.schema
   (:require [schema.core :as s]))
 
-(s/defschema Document
-  "HOKS document"
-  {:id s/Int
-   :version s/Int
-   :created-by-oid s/Str
-   :updated-by-oid s/Str
-   :create-acceptor-oid s/Str
-   :update-acceptor-oid s/Str
-   :created-at s/Inst
-   :accepted-at s/Inst
-   :updated-at s/Inst
-   :student-oid s/Str})
-
 (s/defschema DocumentValues
-  "HOKS document values for creating one"
-  {:student-oid s/Str})
+             "HOKS document values for creating one"
+             {:student-oid s/Str})
+
+(s/defschema Document
+             "HOKS document"
+             (merge
+               DocumentValues
+               {:id s/Int
+                :version s/Int
+                :created-by-oid s/Str
+                :updated-by-oid s/Str
+                :create-acceptor-oid s/Str
+                :update-acceptor-oid s/Str
+                :created-at s/Inst
+                :accepted-at s/Inst
+                :updated-at s/Inst
+                :student-oid s/Str}))
