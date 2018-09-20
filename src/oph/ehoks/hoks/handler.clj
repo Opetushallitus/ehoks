2b5c88e67cd675453d3e86ee91b5c7fa1f64c4e1
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-20 15:07:34 +0300

    Rename terms to Finnish

--- src/oph/ehoks/hoks/handler.clj
+++ src/oph/ehoks/hoks/handler.clj
@@ -9,24 +9,30 @@
   (c-api/context "/hoks" []
 
     (c-api/GET "/:id" [:as id]
-      :summary "Get HOKS document"
-      :return (response hoks-schema/Document)
+      :summary "Palauttaa HOKSin"
+      :return (response hoks-schema/HOKS)
       (rest-ok {}))
 
     (c-api/POST "/" []
-      :summary "Creates new HOKS document"
-      :body [_ hoks-schema/DocumentValues]
+      :summary "Luo uuden HOKSin"
+      :body [_ hoks-schema/HOKSArvot]
       :return (response schema/POSTResponse)
       (rest-ok {:uri ""}))
 
-    (c-api/POST "/:id/competences/" []
-      :summary "Creates competence for HOKS document"
-      :body [_ hoks-schema/Competence]
+    (c-api/POST "/:id/osaamiset/" []
+      :summary "Listaa HOKSiin liitetyt osaamiset"
+      :body [_ hoks-schema/Osaaminen]
       :return (response schema/POSTResponse)
       (rest-ok {:uri ""}))
 
-    (c-api/POST "/:id/educations/" []
-      :summary "Creates education for HOKS document"
-      :body [_ hoks-schema/Education]
+    (c-api/POST "/:id/koulutukset/" []
+      :summary "Listaa HOKSiin liitetyt koulutukset"
+      :body [_ hoks-schema/Koulutus]
+      :return (response schema/POSTResponse)
+      (rest-ok {:uri ""}))
+
+    (c-api/POST "/:id/suunnitellut-osaamiset/" []
+      :summary "Listaa HOKSiin suunnitellut osaamiset"
+      :body [_ hoks-schema/SuunniteltuOsaaminen]
       :return (response schema/POSTResponse)
       (rest-ok {:uri ""}))))
--- src/oph/ehoks/hoks/schema.clj
+++ src/oph/ehoks/hoks/schema.clj
@@ -1,60 +1,59 @@
 (ns oph.ehoks.hoks.schema
   (:require [schema.core :as s]))
 
-(s/defschema Education
-             "HOKS education"
+(s/defschema Koulutus
+             "HOKS koulutus"
              {:id s/Int
-              :type s/Str
-              :code s/Str
-              :document-id s/Int})
+              :tyyppi s/Str
+              :diaarinumero s/Str
+              :hoks-id s/Int})
 
-(s/defschema Competence
-             "HOKS student competence"
-             {:type s/Str
-              :document-id s/Int
-              :qualification-register-number s/Str
-              :competence-area-code s/Str
-              :completed s/Inst})
+(s/defschema Osaaminen
+             "HOKSin olemassa oleva osaaminen"
+             {:tyyppi s/Str
+              :hoks-id s/Int
+              :diaarinumero s/Str
+              :osaamisalakoodi s/Str
+              :suorituspvm s/Inst})
 
-(s/defschema PlannedCompetence
-             "HOKS student planned competence"
-             {:type s/Str
-              :document-id s/Int
-              :qualification-register-number s/Str
-              :competence-area-code s/Str
-              :acquiring-type s/Str
-              :description s/Str
-              :start s/Inst
-              :end s/Inst
-              :organisation s/Str
-              :core-tasks [s/Str]
-              :is-guided s/Bool
-              :has-special-support s/Bool})
+(s/defschema SuunniteltuOsaaminen
+             "HOKSin puuttuvan osaamisen hankkimisen suunnitelma"
+             {:tyyppi s/Str
+              :hoks-id s/Int
+              :diaarinumero s/Str
+              :osaamisalakoodi s/Str
+              :suoritustapa s/Str
+              :sisalto s/Str
+              :alku s/Inst
+              :loppu s/Inst
+              :organisaatio s/Str
+              :keskeiset-tehtavat [s/Str]
+              :ohjaus-ja-tuki s/Bool
+              :erityinen-tuki s/Bool})
 
-(s/defschema DocumentValues
-             "HOKS document values for creating one"
-             {:student-oid s/Str
-              :career-objective s/Str
-              :study-objective s/Str
-              :qualification-register-number s/Str
-              :competence-area-code s/Str
-              :study-right-start s/Inst
-              :study-finish-estimate s/Inst})
+(s/defschema HOKSArvot
+             "HOKS arvot uuden HOKSin luomiseen"
+             {:oppijan-oid s/Str
+              :uratavoite s/Str
+              :tutkintotavoite s/Str
+              :tutkinto-diaarinumero s/Str
+              :osaamisalakoodi s/Str
+              :opiskeluoikeus-alkupvm s/Inst
+              :opiskeluoikeus-paattymispvm s/Inst})
 
-(s/defschema Document
-             "HOKS document"
+(s/defschema HOKS
+             "HOKS"
              (merge
-               DocumentValues
+               HOKSArvot
                {:id s/Int
-                :version s/Int
-                :created-by-oid s/Str
-                :updated-by-oid s/Str
-                :create-acceptor-oid s/Str
-                :update-acceptor-oid s/Str
-                :created-at s/Inst
-                :accepted-at s/Inst
-                :updated-at s/Inst
-                :student-oid s/Str
-                :competences [Competence]
-                :educations [Education]
-                :planned-competencies [PlannedCompetence]}))
+                :versio s/Int
+                :luojan-oid s/Str
+                :paivittajan-oid s/Str
+                :luonnin-hyvaksyjan-oid s/Str
+                :paivityksen-hyvaksyjan-oid s/Str
+                :luotu s/Inst
+                :hyvaksytty s/Inst
+                :paivitetty s/Inst
+                :osaamiset [Osaaminen]
+                :koulutukset [Koulutus]
+                :suunnitellut-osaamiset [SuunniteltuOsaaminen]}))
