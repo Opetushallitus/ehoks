85fc2be59292bbf4de029da28e3797fa507ae30f
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-20 09:08:31 +0300

    Use as require

--- src/oph/ehoks/hoks/handler.clj
+++ src/oph/ehoks/hoks/handler.clj
@@ -1,14 +1,14 @@
 (ns oph.ehoks.hoks.handler
-  (:require [compojure.api.sweet :refer [context POST DELETE]]
+  (:require [compojure.api.sweet :as c-api]
             [ring.util.http-response :refer [accepted]]
             [oph.ehoks.schema :as schema]
             [oph.ehoks.hoks.schema :as hoks-schema]
             [oph.ehoks.restful :refer [response rest-ok]]))
 
 (def routes
-  (context "/hoks" []
+  (c-api/context "/hoks" []
 
-    (POST "/" []
+    (c-api/POST "/" []
       :summary "Creates new HOKS document"
       :body [_ hoks-schema/DocumentValues]
       :return (response schema/POSTResponse)
