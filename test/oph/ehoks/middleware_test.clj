fd7ede85368b158e31acb95613e24c4dc6732633
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-24 12:38:11 +0300

    Add tests for middleware

---
+++ test/oph/ehoks/middleware_test.clj
@@ -0,0 +1,90 @@
+(ns oph.ehoks.middleware-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [compojure.api.sweet :as c-api]
+            [ring.util.http-response :refer [ok]]
+            [ring.mock.request :as mock]
+            [oph.ehoks.middleware :as middleware]
+            [ring.middleware.session :as session]
+            [clojure.core.async :as async]))
+
+(defn- handle-async [app request]
+  (let [c (async/chan)]
+    (async/go
+      (app
+        request
+       (fn [r]
+         (async/go (async/>! c r)))
+       (fn [e]
+         (async/go (async/>! c {:exception e})))))
+    (let [result (async/<!! c)]
+      (when-let [e (:exception result)]
+        (throw (:exception result)))
+      result)))
+
+(defn- get-cookie [app]
+  (-> (mock/request :post "/sync/authenticate")
+      (app)
+      (get-in [:headers "Set-Cookie"])
+      (first)))
+
+(defn- with-authentication-async [app request]
+  (let [cookie (get-cookie app)]
+    (handle-async app (mock/header request :cookie cookie))))
+
+(defn- with-authentication [app request]
+  (let [cookie (get-cookie app)]
+    (app (mock/header request :cookie cookie))))
+
+(def async-routes
+  (c-api/context "/async" []
+    (c-api/GET "/" [] (async/go (ok "")))
+    (c-api/GET "/public" [] (async/go (ok "")))))
+
+(def sync-routes
+  (c-api/context "/sync" []
+    (c-api/GET "/" [] (ok ""))
+    (c-api/GET "/public" [] (ok ""))
+    (c-api/POST "/authenticate" [] (assoc (ok "") :session {:user "User"}))))
+
+(def public-routes
+  [{:uri #"^/async/public$" :request-method :get}
+   {:uri #"^/async/public$" :request-method :post}
+   {:uri #"^/sync/public$" :request-method :get}
+   {:uri #"^/sync/authenticate$" :request-method :post}])
+
+(def app
+  (-> (c-api/api async-routes sync-routes)
+      (middleware/wrap-public public-routes)
+      (session/wrap-session)))
+
+(deftest private-sync
+  (testing "Private sync route"
+    (let [response (with-authentication app (mock/request :get "/sync"))]
+      (is (= (:status response) 200)))))
+
+(deftest public-sync
+  (testing "Public sync route"
+    (let [response (app (mock/request :get "/sync/public"))]
+      (is (= (:status response) 200)))))
+
+(deftest private-sync-unauthorized
+  (testing "Private sync route without authorization"
+    (is (thrown-with-msg?
+          clojure.lang.ExceptionInfo #"HTTP 401"
+          (app (mock/request :get "/sync/"))))))
+
+(deftest private-async
+  (testing "Private async route"
+    (let [response (with-authentication-async app (mock/request :get "/async"))]
+      (is (= (:status response) 200)))))
+
+(deftest public-async
+  (testing "Public async route"
+    (let [response (handle-async app (mock/request :get "/async/public"))]
+      (is (= (:status response) 200)))))
+
+(deftest private-async-unauthorized
+  (testing "Private async route without authorization"
+    (is (thrown-with-msg?
+          clojure.lang.ExceptionInfo #"HTTP 401"
+          (handle-async app (mock/request :get "/async/"))))))
