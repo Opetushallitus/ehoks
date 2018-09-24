9365ab5e0dbe2d8aedfc30448f49af514a859c71
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-24 15:54:17 +0300

    Add auth helping routes

--- test/oph/ehoks/utils.clj
+++ test/oph/ehoks/utils.clj
@@ -1,5 +1,25 @@
 (ns oph.ehoks.utils
-  (:require [cheshire.core :as cheshire]))
+  (:require [cheshire.core :as cheshire]
+            [ring.mock.request :as mock]
+            [oph.ehoks.config :refer [config]]))
+
+(defn get-auth-cookie [app]
+  (-> (mock/request
+        :post "/ehoks/api/v1/session/opintopolku/"
+        {"FirstName" "Teuvo Taavetti"
+         "cn" "Teuvo"
+         "givenName" "Teuvo"
+         "hetu" "190384-9245"
+         "sn" "Testaaja"})
+      (mock/header "referer" (:opintopolku-login-url config))
+      (app)
+      (get-in [:headers "Set-Cookie"])
+      (first)))
+
+
+(defn with-authentication [app request]
+  (let [cookie (get-auth-cookie app)]
+    (app (mock/header request :cookie cookie))))
 
 (defn parse-body [body]
   (cheshire/parse-string (slurp body) true))
