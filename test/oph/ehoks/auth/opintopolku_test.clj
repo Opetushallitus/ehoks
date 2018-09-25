dd34e1a355c2bb66996c9cb972850e33662af8e5
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-25 13:57:57 +0300

    Change to use require as

--- test/oph/ehoks/auth/opintopolku_test.clj
+++ test/oph/ehoks/auth/opintopolku_test.clj
@@ -1,7 +1,7 @@
 (ns oph.ehoks.auth.opintopolku-test
   (:require [clojure.test :refer [deftest testing is]]
             [clojure.edn :as edn]
-            [oph.ehoks.auth.opintopolku :refer [convert convert-to-utf-8 parse]]
+            [oph.ehoks.auth.opintopolku :as o]
             [clojure.java.io :as io])
   (:import [java.nio.charset StandardCharsets]))
 
@@ -9,13 +9,13 @@
   (testing "Converting string encoding"
     (let [str-utf-8 "Example string"]
       (is (= (-> str-utf-8
-                 (convert StandardCharsets/UTF_8 StandardCharsets/ISO_8859_1)
-                 (convert StandardCharsets/ISO_8859_1 StandardCharsets/UTF_8))
+                 (o/convert StandardCharsets/UTF_8 StandardCharsets/ISO_8859_1)
+                 (o/convert StandardCharsets/ISO_8859_1 StandardCharsets/UTF_8))
              str-utf-8)))))
 
 (deftest convert-to-utf-8-test
   (testing "Converting ISO-8859-1 string to UTF-8"
-    (is (= (convert-to-utf-8 "Example string") "Example string"))))
+    (is (= (o/convert-to-utf-8 "Example string") "Example string"))))
 
 (deftest parse-test
   (testing "Parsing authentication headers"
@@ -24,7 +24,7 @@
                        "givenName" "Teuvo"
                        "hetu" "010203-XXXX"
                        "sn" "Testaaja"}]
-      (is (= (parse auth-header)
+      (is (= (o/parse auth-header)
              {:first-name "Teuvo Taavetti"
               :common-name "Teuvo"
               :given-name "Teuvo"
