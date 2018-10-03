103b7b7ba9385d1391d736006f93d8cf578f1fdc
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-03 13:01:53 +0300

    Add some function tests

--- test/oph/ehoks/external/oppijanumerorekisteri_test.clj
+++ test/oph/ehoks/external/oppijanumerorekisteri_test.clj
@@ -1,14 +1,38 @@
 (ns oph.ehoks.external.oppijanumerorekisteri-test
   (:require [clojure.test :refer [deftest testing is]]
-            [oph.ehoks.external.oppijanumerorekisteri
-             :refer [convert-student-info]]))
+            [oph.ehoks.external.connection :as c]
+            [oph.ehoks.external.oppijanumerorekisteri :as onr]
+            [clj-time.core :as t]))
+
+(defn with-service-ticket [f & args]
+  (with-redefs [clj-http.client/post (fn [_ __] {:body "test-ticket"})
+                clj-http.client/get (fn [_ __]
+                                      {:body "{\"data\": [{\"cn\": \"Testi\"}]}"
+                                       :status 200})]
+    (reset! c/service-ticket {:url "http://ticket.url"
+                              :expires (t/plus (t/now) (t/hours 2))})
+    (apply f args)))
+
+(deftest test-find-student-by-nat-id
+  (testing "Finding student by national ID returns unmangled data"
+    (let [response (with-service-ticket
+                    onr/find-student-by-nat-id "010101-1100")]
+      (is (= (:body response) {:data [{:cn "Testi"}]}))
+      (is (:status response) 200))))
+
+(deftest test-find-student-by-nat-id
+  (testing "Finding student by oid returns unmangled data"
+    (let [response (with-service-ticket
+                    onr/find-student-by-nat-id "1234.4567.89")]
+      (is (= (:body response) {:data [{:cn "Testi"}]}))
+      (is (:status response) 200))))
 
 (deftest test-convert-student-info
   (testing "Converting Oppijanumerorekisteri student info"
-    (is (= (convert-student-info
+    (is (= (onr/convert-student-info
              {})
            {}))
-    (is (= (convert-student-info
+    (is (= (onr/convert-student-info
              {:oidHenkilo "1.2.246.562.24.78058065184"
               :hetu "190384-9245"
               :etunimet "Teuvo Taavetti"
