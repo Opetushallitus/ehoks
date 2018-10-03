d384d08f50b66ded6df0320b57be5f965a294ae1
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-03 10:38:53 +0300

    Rename argument

--- src/oph/ehoks/external/utils.clj
+++ src/oph/ehoks/external/utils.clj
@@ -4,12 +4,12 @@
 (defmacro with-timeout
   "Simple macro for creating asyncronous timeout-safe function calls.
    `body` will be wrapped inside go block.
-  Returns channel which returns either result of `body` or `error` depending on
-  if given time exceeds."
-  [time-ms body error]
+  Returns channel which returns either result of `body` or `timeout` depending
+  on if given time exceeds."
+  [time-ms body timeout]
   `(a/go
      (let [c# (a/go ~body)
            [v# p#] (a/alts! [c# (a/timeout ~time-ms)])]
        (if (and (not= p# c#) (nil? v#))
-         ~error
+         ~timeout
          v#))))
