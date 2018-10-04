02c49af10495d25bbc0061e7c25d435b9d6122b8
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-04 12:34:24 +0300

    Update eastwood

--- project.clj
+++ project.clj
@@ -71,7 +71,7 @@
   :plugins [[lein-cljfmt "0.6.0" :exclusions [org.clojure/tools.cli]]
             [lein-kibit "0.1.6"]
             [lein-bikeshed "0.5.1"]
-            [jonase/eastwood "0.2.9"]
+            [jonase/eastwood "0.3.1"]
             [lein-auto "0.1.3"]
             [lein-ancient "0.6.15"]
             [lein-cloverage "1.0.13"]]
