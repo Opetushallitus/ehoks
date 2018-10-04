9a848d876cc3a8c044d7b7c17c3a4e9d92c542e1
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-04 12:20:39 +0300

    Add tests for Koodisto integration

---
+++ test/oph/ehoks/external/koodisto_test.clj
@@ -0,0 +1,42 @@
+(ns oph.ehoks.external.koodisto-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [oph.ehoks.external.koodisto :as k]
+            [clojure.java.io :as io]
+            [cheshire.core :as cheshire]))
+
+(def mock-koodisto-value
+  (-> (io/resource "mock/koodisto-value.json")
+      slurp
+      (cheshire/parse-string true)))
+
+(deftest test-filter-koodisto-values
+  (testing "Filtering Koodisto values"
+    (is (= (k/filter-koodisto-values {}) {}))
+    (is (= (k/filter-koodisto-values nil) {}))
+    (let [filtered (k/filter-koodisto-values mock-koodisto-value)]
+      (is (get #{"PASSIIVINEN" "LUONNOS" "HYVAKSYTTY"} (:tila filtered)))
+      (is (= (:koodiArvo filtered) "4"))
+      (is (nil? (:voimassaLoppuPvm filtered)))
+      (is (string? (:voimassaAlkuPvm filtered)))
+      (is (string? (:resourceUri filtered)))
+      (is (map? (:koodisto filtered)))
+      (is (string? (get-in filtered [:koodisto :koodistoUri])))
+      (is (string? (get-in filtered [:koodisto :organisaatioOid])))
+      (is (coll? (get-in filtered [:koodisto :koodistoVersios])))
+      (is (= (get-in filtered [:koodisto :koodistoVersios 0]) 2))
+      (is (int? (:versio filtered)))
+      (is (string? (:koodiUri filtered)))
+      (is (int? (:paivitysPvm filtered)))
+      (is (int? (:version filtered)))
+      (is (coll? (:metadata filtered)))
+      (let [metadata (first (:metadata filtered))]
+        (is (string? (:nimi metadata)))
+        (is (string? (:kuvaus metadata)))
+        (is (string? (:lyhytNimi metadata)))
+        (is (string? (:kayttoohje metadata)))
+        (is (string? (:kasite metadata)))
+        (is (string? (:sisaltaaMerkityksen metadata)))
+        (is (string? (:eiSisallaMerkitysta metadata)))
+        (is (string? (:huomioitavaKoodi metadata)))
+        (is (string? (:sisaltaaKoodiston metadata)))
+        (is (= (:kieli metadata) "FI"))))))
