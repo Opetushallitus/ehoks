ed1d6226d1208645700ae9025c310ac518668da2
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-04 12:19:22 +0300

    Add endpoint for downloading Koodisto stuff

--- src/oph/ehoks/external/handler.clj
+++ src/oph/ehoks/external/handler.clj
@@ -6,10 +6,22 @@
             [oph.ehoks.external.utils :as utils]
             [oph.ehoks.restful :as rest]
             [oph.ehoks.external.eperusteet :as eperusteet]
+            [oph.ehoks.external.koodisto :as koodisto]
             [oph.ehoks.config :refer [config]]))
 
 (def routes
   (c-api/context "/external" []
+
+    (c-api/GET "/koodistokoodi/:uri/:versio" []
+      :summary "Hakee koodisto koodin tietoja Kooidsto-palvelusta"
+      :path-params [uri :- s/Str, versio :- s/Int]
+      :return (rest/response schema/ExtendedKoodistoKoodi)
+      (utils/with-timeout
+        (:service-timeout-ms config)
+        (-> (koodisto/get-koodi-versio uri versio)
+            koodisto/filter-koodisto-values
+            rest/rest-ok)
+        (response/internal-server-error {:error "Service timeout exceeded"})))
 
     (c-api/GET "/eperusteet/" [:as request]
       :summary "Hakee perusteiden tietoja ePerusteet-palvelusta"
