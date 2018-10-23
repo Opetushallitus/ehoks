81b906cb1db813e62040253c7c59f7525fda22e9
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-23 14:48:58 +0300

    Remove individual routes for now

--- src/oph/ehoks/hoks/handler.clj
+++ src/oph/ehoks/hoks/handler.clj
@@ -19,38 +19,3 @@
       :return (response schema/POSTResponse)
       (rest-ok {:uri ""}))
 
-    (c-api/POST "/:id/olemassa-olevat-osaamiset/" []
-      :summary "Lisää HOKSiin olemassa oleva osaaminen"
-      :body [_ hoks-schema/OlemassaOlevaOsaaminen]
-      :return (response schema/POSTResponse)
-      (rest-ok {:uri ""}))
-
-    (c-api/POST "/:id/muut-todennetut-osaamiset/" []
-      :summary "Lisää HOKSiin muu todennettu osaaminen"
-      :body [_ hoks-schema/MuuTodennettuOsaaminen]
-      :return (response schema/POSTResponse)
-      (rest-ok {:uri ""}))
-
-    (c-api/POST "/:id/tukevat-opinnot/" []
-      :summary "Lisää HOKSiin opiskeluvalmiuksia tukeva opinto"
-      :body [_ hoks-schema/TukevaOpinto]
-      :return (response schema/POSTResponse)
-      (rest-ok {:uri ""}))
-
-    (c-api/POST "/:id/puuttuvat-osaamiset/" []
-      :summary "Lisää HOKSiin puuttuvan osaamisen hankkiminen"
-      :body [_ hoks-schema/PuuttuvaOsaaminen]
-      :return (response schema/POSTResponse)
-      (rest-ok {:uri ""}))
-
-    (c-api/POST "/:id/tyopaikalla-hankittavat-osaamiset/" []
-      :summary "Lisää HOKSiin työpaikalla tapahtuvan osaamisen tiedot"
-      :body [_ hoks-schema/TyopaikallaHankittavaOsaaminen]
-      :return (response schema/POSTResponse)
-      (rest-ok {:uri ""}))
-
-    (c-api/POST "/:id/osaamisen-osoittamiset/" []
-      :summary "Lisää HOKSiin hankitun osaamisen osoittaminen/näyttö"
-      :body [_ hoks-schema/HankitunOsaamisenNaytto]
-      :return (response schema/POSTResponse)
-      (rest-ok {:uri ""}))))
