947b6a2a2c3232a88540571aaaf1c73ef152dd9f
Tuomas Kareinen <tuomas.kareinen@gofore.com>
2018-11-15 11:49:46 +0200

    ops: use Log4j2 logging backend
    Makes log configuration easier for AWS envs since we don't have to
    override Logback configs.

--- src/oph/ehoks/schema.clj
+++ src/oph/ehoks/schema.clj
@@ -40,5 +40,4 @@
               :session-max-age s/Int
               :service-timeout-ms s/Int
               :koodisto-url s/Str
-              :koski-url s/Str
-              (s/optional-key :log-level) s/Keyword})
+              :koski-url s/Str})
