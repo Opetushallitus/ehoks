db9a245ac0106d38743e5aa1b76dfe4e77ae51d8
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-20 08:43:04 +0200

    Remove unused config

--- src/oph/ehoks/schema.clj
+++ src/oph/ehoks/schema.clj
@@ -41,5 +41,4 @@
               :service-timeout-ms s/Int
               :koodisto-url s/Str
               :koski-url s/Str
-              :ext-cache-lifetime-minutes s/Int
-              (s/optional-key :log-level) s/Keyword})
+              :ext-cache-lifetime-minutes s/Int})
