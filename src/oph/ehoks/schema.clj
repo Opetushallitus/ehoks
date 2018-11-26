8e3addd944602d53fe6b17a2098e89b47ff43314
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-26 10:44:40 +0200

    Add allow mock http client

--- src/oph/ehoks/schema.clj
+++ src/oph/ehoks/schema.clj
@@ -45,4 +45,5 @@
               :service-timeout-ms s/Int
               :koodisto-url s/Str
               :koski-url s/Str
-              :ext-cache-lifetime-minutes s/Int})
+              :ext-cache-lifetime-minutes s/Int
+              (s/optional-key :allow-mock-http?) s/Bool})
