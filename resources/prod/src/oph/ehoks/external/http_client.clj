f2e15b70ce9bb4565fcf9ceb40ab1a9ea7905fdf
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-27 09:50:41 +0200

    Remove mocking from prod version

--- resources/prod/src/oph/ehoks/external/http_client.clj
+++ resources/prod/src/oph/ehoks/external/http_client.clj
@@ -1,24 +1,7 @@
 (ns oph.ehoks.external.http-client
   (:refer-clojure :exclude [get])
-  (:require [clj-http.client :as client]
-            [oph.ehoks.config :refer [config]]))
+  (:require [clj-http.client :as client]))
 
-(def ^:private client-functions
-  (atom {:get client/get
-         :post client/post}))
+(def get client/get)
 
-(defn get [url options]
-  ((:get @client-functions) url options))
-
-(defn set-get! [f]
-  (when-not (:allow-mock-http? config)
-    (throw (Exception. "Mocking HTTP is not allowed")))
-  (swap! client-functions assoc :get f))
-
-(defn post [url options]
-  ((:post @client-functions) url options))
-
-(defn set-post! [f]
-  (when-not (:allow-mock-http? config)
-    (throw (Exception. "Mocking HTTP is not allowed")))
-  (swap! client-functions assoc :post f))
+(def post client/post)
