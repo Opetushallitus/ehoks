6c97248a14ac13e7f31c8e84b8c365353c494317
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-27 09:51:07 +0200

    Add mockable test http client

---
+++ resources/test/src/oph/ehoks/external/http_client.clj
@@ -0,0 +1,20 @@
+(ns oph.ehoks.external.http-client
+  (:refer-clojure :exclude [get])
+  (:require [clj-http.client :as client]
+            [oph.ehoks.config :refer [config]]))
+
+(def ^:private client-functions
+  (atom {:get client/get
+         :post client/post}))
+
+(defn get [url options]
+  ((:get @client-functions) url options))
+
+(defn set-get! [f]
+  (swap! client-functions assoc :get f))
+
+(defn post [url options]
+  ((:post @client-functions) url options))
+
+(defn set-post! [f]
+  (swap! client-functions assoc :post f))
