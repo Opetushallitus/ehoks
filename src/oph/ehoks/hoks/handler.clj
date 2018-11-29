d18b041f1b7ecfa7098e6ac52aa11657f567df6a
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-29 15:55:07 +0200

    Use namespace

--- src/oph/ehoks/hoks/handler.clj
+++ src/oph/ehoks/hoks/handler.clj
@@ -1,9 +1,9 @@
 (ns oph.ehoks.hoks.handler
   (:require [compojure.api.sweet :as c-api]
-            [ring.util.http-response :refer [no-content]]
+            [ring.util.http-response :as response]
             [oph.ehoks.schema :as schema]
             [oph.ehoks.hoks.schema :as hoks-schema]
-            [oph.ehoks.restful :refer [response rest-ok]]))
+            [oph.ehoks.restful :as rest]))
 
 (def routes
   (c-api/context "/hoks" []
@@ -11,16 +11,17 @@
 
     (c-api/GET "/:id" [:as id]
       :summary "Palauttaa HOKSin"
-      :return (response hoks-schema/HOKS)
-      (rest-ok {}))
+      :return (rest/response hoks-schema/HOKS)
+      (rest/rest-ok {}))
 
     (c-api/POST "/" []
       :summary "Luo uuden HOKSin"
       :body [_ hoks-schema/HOKSArvot]
-      :return (response schema/POSTResponse)
-      (rest-ok {:uri ""}))
+      :return (rest/response schema/POSTResponse)
+      (rest/rest-ok {:uri ""}))
 
     (c-api/PUT "/:id" []
       :summary "Päivittää olemassa olevaa HOKSia"
       :body [_ hoks-schema/HOKSArvot]
-      (no-content))))
+      (response/no-content))
+    ))
