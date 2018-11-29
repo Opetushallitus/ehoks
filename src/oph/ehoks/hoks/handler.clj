aa2cf27f1d0a9a087d3e5ab3ee64cd6dcb9c0f1f
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-29 15:55:36 +0200

    Add routes for puuttuva ammatillinen osaaminen

--- src/oph/ehoks/hoks/handler.clj
+++ src/oph/ehoks/hoks/handler.clj
@@ -5,6 +5,31 @@
             [oph.ehoks.hoks.schema :as hoks-schema]
             [oph.ehoks.restful :as rest]))
 
+(def ^:private puuttuva-ammatillinen-osaaminen
+  (c-api/context "/:hoks-eid/puuttuva-ammatillinen-osaaminen" [hoks-eid]
+    (c-api/GET "/:eid" [:as eid]
+      :summary "Palauttaa HOKSin puuttuvan ammatillisen osaamisen"
+      :return (rest/response hoks-schema/PuuttuvaAmmatillinenOsaaminen)
+      (rest/rest-ok {}))
+
+    (c-api/POST "/" []
+      :summary
+      "Luo (tai korvaa vanhan) puuttuvan ammatillisen osaamisen HOKSiin"
+      :body [_ hoks-schema/PuuttuvaAmmatillinenOsaaminen]
+      :return (rest/response schema/POSTResponse)
+      (rest/rest-ok {:uri ""}))
+
+    (c-api/PUT "/:eid" []
+      :summary "Päivittää HOKSin puuttuvan ammatillisen osaamisen"
+      :body [_ hoks-schema/PuuttuvaAmmatillinenOsaaminen]
+      (response/no-content))
+
+    (c-api/PATCH "/:eid" []
+      :summary
+      "Päivittää HOKSin puuttuvan ammatillisen osaamisen arvoa tai arvoja"
+      :body [_ hoks-schema/PuuttuvaAmmatillinenOsaaminen]
+      (response/no-content))))
+
 (def routes
   (c-api/context "/hoks" []
     :tags ["hoks"]
