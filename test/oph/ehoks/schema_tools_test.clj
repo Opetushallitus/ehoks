5c6f8395455e1450955778bf03ededea12773f4b
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-30 13:20:19 +0200

    Fix indent

--- test/oph/ehoks/schema_tools_test.clj
+++ test/oph/ehoks/schema_tools_test.clj
@@ -3,28 +3,28 @@
             [oph.ehoks.schema-tools :as st]
             [schema.core :as s]))
 
+(s/defschema
+  TestSchema
+  "Original Test Schema"
+  {:key1 s/Str
+   :key2 s/Int
+   (s/optional-key :key3) s/Bool})
+
+(s/defschema
+  ModifiedTestSchema
+  "Modified Test Schema"
+  {:key1 s/Str
+   :key2 s/Int
+   (s/optional-key :key3) s/Bool})
+
+(s/defschema
+  ExtraOptional
+  "Schema with one extra optional and first removed"
+  {(s/optional-key :key2) s/Int
+   (s/optional-key :key3) s/Bool})
+
 (deftest test-modify
   (testing "Modify schema"
-    (s/defschema
-      TestSchema
-      "Original Test Schema"
-      {:key1 s/Str
-       :key2 s/Int
-       (s/optional-key :key3) s/Bool})
-
-    (s/defschema
-      ModifiedTestSchema
-      "Modified Test Schema"
-      {:key1 s/Str
-       :key2 s/Int
-       (s/optional-key :key3) s/Bool})
-
-    (s/defschema
-      ExtraOptional
-      "Schema with one extra optional and first removed"
-      {(s/optional-key :key2) s/Int
-       (s/optional-key :key3) s/Bool})
-
     (is (= ModifiedTestSchema
            (st/modify TestSchema "Modified Test Schema" [] [])))
     (is (= ExtraOptional
