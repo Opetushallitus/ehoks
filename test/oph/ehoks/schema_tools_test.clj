59dbf4d8a7bde5753759a06e9a082d662590fdf2
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-30 10:12:29 +0200

    Add tests for schema tools

---
+++ test/oph/ehoks/schema_tools_test.clj
@@ -0,0 +1,35 @@
+(ns oph.ehoks.schema-tools-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [oph.ehoks.schema-tools :as st]
+            [schema.core :as s]))
+
+(deftest test-modify
+  (testing "Modify schema"
+    (s/defschema
+      TestSchema
+      "Original Test Schema"
+      {:key1 s/Str
+       :key2 s/Int
+       (s/optional-key :key3) s/Bool})
+
+    (s/defschema
+      ModifiedTestSchema
+      "Modified Test Schema"
+      {:key1 s/Str
+       :key2 s/Int
+       (s/optional-key :key3) s/Bool})
+
+    (s/defschema
+      ExtraOptional
+      "Schema with one extra optional and first removed"
+      {(s/optional-key :key2) s/Int
+       (s/optional-key :key3) s/Bool})
+
+    (is (= ModifiedTestSchema
+           (st/modify TestSchema "Modified Test Schema" [] [])))
+    (is (= ExtraOptional
+           (st/modify
+             TestSchema
+             "Schema with one extra optional and first removed"
+             [:key1]
+             [:key2 :key3])))))
