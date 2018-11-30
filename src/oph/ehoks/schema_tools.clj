28dc9b4929d65654fb7cd3ae09fdb31a079639b9
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-30 09:51:20 +0200

    Add modifying schema

--- src/oph/ehoks/schema_tools.clj
+++ src/oph/ehoks/schema_tools.clj
@@ -1,5 +1,8 @@
 (ns oph.ehoks.schema-tools
-  (:require [ring.swagger.json-schema :as rsjs]))
+  (:require [ring.swagger.json-schema :as rsjs]
+            [schema.core :as s]
+            [schema-tools.core :as st]
+            [clojure.set :refer [rename-keys]]))
 
 (defn describe [description & kvds]
   (assert (or (seq kvds) (zero? (mod (count kvds) 3)))
@@ -12,3 +15,16 @@
       {}
       (partition 3 kvds))
     {:description description}))
+
+(defn modify [schema description removed optionals]
+  (st/merge
+    (describe description)
+    (as-> schema x
+      (apply st/dissoc x removed)
+      (rename-keys
+        x
+        (reduce
+          (fn [c n]
+            (assoc c n (s/optional-key n)))
+          {}
+          optionals)))))
