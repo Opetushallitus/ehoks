04a76f4f4848288e2de898707586eb6c817ca980
Matti Ahinko <matti.ahinko@gmail.com>
2018-12-11 16:20:35 +0200

    Add better test compare

--- test/oph/ehoks/utils.clj
+++ test/oph/ehoks/utils.clj
@@ -1,6 +1,9 @@
 (ns oph.ehoks.utils
   (:require [cheshire.core :as cheshire]
-            [ring.mock.request :as mock]))
+            [ring.mock.request :as mock]
+            [clojure.test :refer [is]]
+            [clojure.data :as d]
+            [clojure.pprint :as p]))
 
 (defn get-auth-cookie [app]
   (-> (mock/request :get "/ehoks-backend/api/v1/session/opintopolku/")
@@ -19,3 +22,14 @@
 
 (defn parse-body [body]
   (cheshire/parse-string (slurp body) true))
+
+(defn eq [value expect]
+  (when (not= value expect)
+    (let [diff (d/diff value expect)]
+      (when (seq (first diff))
+        (prn "Not expected:")
+        (p/pprint (first diff)))
+      (when (seq (second diff))
+        (prn "Missing:")
+        (p/pprint (second diff)))))
+  (is (= value expect)))
