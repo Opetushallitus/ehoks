4d72153c073e8acf7ac32a488d82a49921554837
Matti Ahinko <matti.ahinko@gmail.com>
2019-01-08 13:26:57 +0200

    Add base for simple memory db

---
+++ src/oph/ehoks/db/memory.clj
@@ -0,0 +1,16 @@
+(ns oph.ehoks.db.memory)
+
+(defonce hoks-store (atom []))
+
+(defn get-next-id []
+  (if (empty? @hoks-store)
+    1
+    (inc (apply max (map :eid @hoks-store)))))
+
+(defn create-hoks! [hoks]
+  (let [h (assoc hoks :eid (get-next-id))]
+    (swap! hoks-store conj h)
+    h))
+
+(defn get-hoks [eid]
+  (some #(when (= (:eid %) eid) %) @hoks-store))
