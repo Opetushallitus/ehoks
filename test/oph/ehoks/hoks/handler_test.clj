1426fbfb01413deeaafe3b4f0b38a89563574da6
Matti Ahinko <matti.ahinko@gmail.com>
2019-01-09 13:42:32 +0200

    Add put test

--- test/oph/ehoks/hoks/handler_test.clj
+++ test/oph/ehoks/hoks/handler_test.clj
@@ -419,3 +419,39 @@
               :hyvaksytty (:hyvaksytty hoks)
               :paivitetty (:paivitetty hoks)
               :versio 2)))))))
+
+(deftest put-created-hoks
+  (testing "PUT updates created HOKS"
+    (db/clear)
+    (let [hoks-data {:opiskeluoikeus {:oid "1.3.444.555.66.77777777777"
+                                      :tutkinto {:laajuus 5 :nimi "Test"}}
+                     :oppijan-oid "1.2.333.444.55.66666666666"
+                     :luonut "Teppo Tekijä"
+                     :opiskeluoikeus-oid "1.3.444.555.66.77777777777"
+                     :paivittanyt "Pekka Päivittäjä"
+                     :hyvaksynyt "Heikki Hyväksyjä"}
+          response
+          (utils/with-authentication
+            app
+            (-> (mock/request :post url)
+                (mock/json-body hoks-data)))
+          body (utils/parse-body (:body response))]
+      (let [hoks (-> (get-in body [:data :uri]) get-authenticated :data)
+            put-response
+            (utils/with-authentication
+              app
+              (-> (mock/request :put (get-in body [:data :uri]))
+                  (mock/json-body
+                    (-> hoks
+                        (assoc :paivittanyt "Teuvo Testaaja")
+                        (dissoc :luonut :luotu :versio :paivitetty)))))]
+        (is (= (:status put-response) 204))
+        (let [updated-hoks
+              (-> (get-in body [:data :uri]) get-authenticated :data)]
+          (eq
+            updated-hoks
+            (assoc
+              hoks
+              :paivitetty (:paivitetty updated-hoks)
+              :versio 2
+              :paivittanyt "Teuvo Testaaja")))))))
