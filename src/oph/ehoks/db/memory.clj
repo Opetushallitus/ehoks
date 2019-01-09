f29199da8ec83bf92dcabadc83483b9ee61039ad
Matti Ahinko <matti.ahinko@gmail.com>
2019-01-09 10:35:40 +0200

    Add different get functions

--- src/oph/ehoks/db/memory.clj
+++ src/oph/ehoks/db/memory.clj
@@ -7,6 +7,14 @@
     1
     (inc (apply max (map :eid @hoks-store)))))
 
+(defn get-hoks-by-opiskeluoikeus [oid]
+  (some #(when (= (get-in % [:opiskeluoikeus :oid]) oid) %) @hoks-store))
+
+(defn get-hoks-by-eid [eid]
+  (when (some? eid)
+    (let [h (filter #(= (:eid %) eid) @hoks-store)]
+      (last (sort-by :versio h)))))
+
 (defn create-hoks! [hoks]
   (let [h (assoc hoks :eid (get-next-id))]
     (swap! hoks-store conj h)
