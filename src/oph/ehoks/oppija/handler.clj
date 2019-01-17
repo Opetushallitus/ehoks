10bcfde3144821d6a50a833d46e4561515cb2795
Matti Ahinko <matti.ahinko@gmail.com>
2019-01-17 16:18:58 +0200

    Enrich HOKS with Koodisto

--- src/oph/ehoks/oppija/handler.clj
+++ src/oph/ehoks/oppija/handler.clj
@@ -1,10 +1,13 @@
 (ns oph.ehoks.oppija.handler
   (:require [compojure.api.sweet :as c-api]
             [ring.util.http-response :as response]
+            [schema.core :as s]
             [oph.ehoks.restful :as rest]
             [oph.ehoks.hoks.schema :as hoks-schema]
             [oph.ehoks.common.schema :as common-schema]
-            [oph.ehoks.db.memory :as db]))
+            [oph.ehoks.schema :as schema]
+            [oph.ehoks.db.memory :as db]
+            [oph.ehoks.external.koodisto :as koodisto]))
 
 (def routes
   (c-api/context "/oppijat" []
@@ -19,7 +22,22 @@
 
       (c-api/GET "/hoks" [:as request]
         :summary "Oppijan HOKSit kokonaisuudessaan"
-        :return (rest/response [hoks-schema/HOKS])
+        :return {:data [hoks-schema/HOKS]
+                 :meta schema/KoodistoErrorMeta}
         (if (= (get-in request [:session :user :oid]) oid)
-          (rest/rest-ok (db/get-all-hoks-by-oppija oid))
+          (let [hokses (db/get-all-hoks-by-oppija oid)]
+            (if (empty? hokses)
+              (response/not-found "No HOKSes found")
+              (response/ok
+                (try
+                  {:data (mapv #(koodisto/enrich % [:urasuunnitelma]) hokses)
+                   :meta {}}
+                  (catch Exception e
+                    (let [data (ex-data e)]
+                      (when (not= (:type data) :not-found) (throw e))
+                      {:data hokses
+                       :meta {:error {:error-type :not-found
+                                      :keys [:urasuunnitelma]
+                                      :uri (:uri data)
+                                      :version (:version data)}}}))))))
           (response/unauthorized))))))
