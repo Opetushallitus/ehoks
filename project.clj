240d308e46bc92c957f4ac677d3e75fbfae23af1
Matti Ahinko <matti.ahinko@gmail.com>
2019-02-05 10:16:58 +0200

    Add alias for migrations

--- project.clj
+++ project.clj
@@ -96,7 +96,8 @@
                         ["cljfmt" "check"]]
             "gendoc" ["do"
                       ["run" "-m" "oph.ehoks.hoks-doc/write-doc!" "doc/hoks.md"]
-                      ["run" "-m" "oph.ehoks.hoks-doc-html/write-doc!" "resources/public/hoks-doc/index.html"]]}
+                      ["run" "-m" "oph.ehoks.hoks-doc-html/write-doc!" "resources/public/hoks-doc/index.html"]]
+            "dbmigrate" ["run" "-m" "oph.ehoks.db.migrations/migrate"]}
   :cljfmt {:indents {#".*" [[:block 0]]}}
   :profiles {:test {:resource-paths ["resources/test"
                                      "resources/test/src"]
