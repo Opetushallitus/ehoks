2c8cad45dec829c66b7f459797623664caa2e534
Matti Ahinko <matti.ahinko@gmail.com>
2019-02-14 16:30:11 +0200

    Add opisjeluoikeus functions

--- src/oph/ehoks/external/koski.clj
+++ src/oph/ehoks/external/koski.clj
@@ -13,3 +13,16 @@
      :path (format "api/oppija/%s" oid)
      :options {:basic-auth [(:cas-username config) (:cas-password config)]
                :as :json}}))
+
+(defn get-opiskeluoikeus-info [oid]
+  (c/with-api-headers
+    {:method :get
+     :service (:koski-url config)
+     :path (format "api/opiskeluoikeus/%s" oid)
+     :options {:basic-auth [(:cas-username config) (:cas-password config)]
+               :as :json}}))
+
+(defn get-opiskeluoikeus-oppilaitos-oid [opiskeluoikeus-oid]
+  (get-in
+    (get-opiskeluoikeus-info opiskeluoikeus-oid)
+    [:body :oppilaitos :oid]))
