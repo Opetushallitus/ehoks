3a3419165c9c7dfa1bc5d292c654e85357ae5bcf
Matti Ahinko <matti.ahinko@gmail.com>
2019-02-14 13:05:53 +0200

    Change form of URI

--- resources/dev/src/oph/ehoks/hoks_doc.clj
+++ resources/dev/src/oph/ehoks/hoks_doc.clj
@@ -49,8 +49,10 @@
 (defn get-regex-translation [s]
   (case s
     "#\"^tutkinnonosat_\\d+$\"" "Merkkijono, esim. tutkinnonosat_123456"
-    "#\"^osaamisenhankkimistapa_\\d+$\""
-    "Merkkijono, esim. osaamisenhankkimistapa_123456"
+    "#\"^osaamisenhankkimistapa_.+$\""
+    "Merkkijono, esim. osaamisenhankkimistapa_oppisopimus"
+    "#\"^ammatillisenoppiaineet_.+$\""
+    "Merkkijono, esim. ammatillisenoppiaineet_aa"
     s))
 
 (defn get-enum-translation [n]
