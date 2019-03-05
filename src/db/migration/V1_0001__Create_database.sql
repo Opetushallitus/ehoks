485221422d23678a5506865ad9a358fc9a70604b
Matti Ahinko <matti.ahinko@gmail.com>
2019-03-05 17:07:36 +0200

    Add puuttuva paikallinen tutkinnon osa

--- src/db/migration/V1_0001__Create_database.sql
+++ src/db/migration/V1_0001__Create_database.sql
@@ -116,3 +116,117 @@ CREATE TABLE olemassa_olevan_ammatillisen_tutkinnon_osan_hankitun_osaamisen_nayt
   hankitun_osaamisen_naytto_id INTEGER REFERENCES hankitun_osaamisen_naytot(id),
   PRIMARY KEY(olemassa_oleva_ammatillinen_tutkinnon_osa_id, hankitun_osaamisen_naytto_id)
 );
+
+CREATE TABLE olemassa_olevat_paikalliset_tutkinnon_osat(
+  id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  deleted_at TIMESTAMP WITH TIME ZONE,
+  version INTEGER DEFAULT 0,
+  hoks_id INTEGER REFERENCES hoksit(id),
+  laajuus INTEGER,
+  nimi TEXT,
+  tavoitteet_ja_sisallot TEXT,
+  amosaa_tunniste TEXT,
+  koulutuksen_jarjestaja_oid VARCHAR(26),
+  vaatimuksista_tai_tavoitteista_poikkeaminen TEXT
+);
+
+CREATE TABLE puuttuvat_paikalliset_tutkinnon_osat(
+  id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  deleted_at TIMESTAMP WITH TIME ZONE,
+  version INTEGER DEFAULT 0,
+  hoks_id INTEGER REFERENCES hoksit(id),
+  laajuus INTEGER,
+  nimi TEXT,
+  tavoitteet_ja_sisallot TEXT,
+  amosaa_tunniste TEXT,
+  hankitun_osaamisen_naytto INTEGER REFERENCES hankitun_osaamisen_naytto_hankitun_paikallisen_osaamisen_naytto(id),
+  osaamisen_hankkimistavat INTEGER REFERENCES osaamisen_hankkimistavat_osaamisen_hankkimistapa(id),
+  koulutuksen_jarjestaja_oid VARCHAR(26),
+  vaatimuksista_tai_tavoitteista_poikkeaminen TEXT
+);
+
+CREATE TABLE puuttuvan_paikallisen_tutkinnon_osan_hankitun_osaamisen_naytto(
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  deleted_at TIMESTAMP WITH TIME ZONE,
+  puuttuva_paikallinen_tutkinnon_osa_id INTEGER REFERENCES puuttuvat_paikalliset_tutkinnon_osat(id)
+  hankitun_osaamisen_naytto_id INTEGER REFERENCES hankitun_osaamisen_naytot(id),
+  PRIMARY KEY(puuttuva_paikallinen_tutkinnon_osa_id, hankitun_osaamisen_naytto_id)
+);
+
+CREATE TABLE tyopaikalla_hankittavat_osaamiset(
+  id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  deleted_at TIMESTAMP WITH TIME ZONE,
+  version INTEGER DEFAULT 0,
+  vastuullinen_ohjaaja_nimi TEXT,
+  vastuullinen_ohjaaja_sahkoposti TEXT,
+  tyopaikan_nimi TEXT,
+  tyopaikan_y_tunnus TEXT,
+  lisatiedot BOOLEAN
+);
+
+CREATE TABLE tyopaikalla_hankittavat_osaamisen_henkilot(
+  id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  deleted_at TIMESTAMP WITH TIME ZONE,
+  version INTEGER DEFAULT 0,
+  tyopaikalla_hankittava_osaaminen_id INTEGER REFERENCES tyopaikalla_hankittavat_osaamiset(id),
+  organisaatio_nimi TEXT,
+  organisaatio_y_tunnus TEXT,
+  nimi TEXT,
+  rooli TEXT
+);
+
+CREATE TABLE tyopaikalla_hankittavat_osaamisen_tyotehtavat(
+  id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  deleted_at TIMESTAMP WITH TIME ZONE,
+  version INTEGER DEFAULT 0,
+  tyopaikalla_hankittava_osaaminen_id INTEGER REFERENCES tyopaikalla_hankittavat_osaamiset(id),
+  tyotehtava TEXT
+);
+
+CREATE TABLE osaamisen_hankkimistavat(
+  id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  deleted_at TIMESTAMP WITH TIME ZONE,
+  version INTEGER DEFAULT 0,
+  jarjestajan_edustaja_nimi TEXT,
+  jarjestajan_edustaja_rooli TEXT,
+  jarjestajan_edustaja_oppilaitos_oid VARCHAR(26),
+  ajanjakson_tarkenne TEXT,
+  osamisen_hankkimistapa_koodi_uri VARCHAR(256),
+  hankkijan_edustaja_nimi TEXT,
+  hankkijan_edustaja_rooli TEXT,
+  hankkijan_edustaja_oppilaitos_oid VARCHAR(26),
+  alku TIMESTAMP WITH TIME ZONE,
+  loppu TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE muut_oppimisymparistot(
+  id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  deleted_at TIMESTAMP WITH TIME ZONE,
+  version INTEGER DEFAULT 0,
+  oppimisymparisto_koodi_uri TEXT,
+  selite TEXT,
+  lisatiedot BOOLEAN,
+  osaamisen_hankkimistapa_id INTEGER REFERENCES osaamisen_hankkimistavat(id)
+);
+
+CREATE TABLE puuttuvan_paikallisen_tutkinnon_osan_osaamisen_hankkimistavat(
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  deleted_at TIMESTAMP WITH TIME ZONE,
+  puuttuva_paikallinen_tutkinnon_osa_id INTEGER REFERENCES puuttuvat_paikalliset_tutkinnon_osat(id)
+  osaamisen_hankkimistapa_id INTEGER REFERENCES osaamisen_hankkimistavat(id),
+  PRIMARY KEY(puuttuva_paikallinen_tutkinnon_osa_id, osaamisen_hankkimistapa_id)
+);
