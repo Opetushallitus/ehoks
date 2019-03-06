44cf6e591c84f956bde05359fc3e65493e651b56
Matti Ahinko <matti.ahinko@gmail.com>
2019-03-06 16:25:52 +0200

    Remove enriching

--- src/oph/ehoks/oppija/handler.clj
+++ src/oph/ehoks/oppija/handler.clj
@@ -4,77 +4,14 @@
             [schema.core :as s]
             [ring.util.http-response :as response]
             [oph.ehoks.restful :as rest]
-            [oph.ehoks.hoks.schema :as hoks-schema]
             [oph.ehoks.common.schema :as common-schema]
             [oph.ehoks.schema :as schema]
+            [oph.ehoks.oppija.schema :as oppija-schema]
             [oph.ehoks.db.memory :as db]
             [oph.ehoks.external.koodisto :as koodisto]
             [oph.ehoks.external.koski :as koski]
             [oph.ehoks.external.eperusteet :as eperusteet]
             [oph.ehoks.middleware :refer [wrap-authorize]]))
-
-(defn with-opiskeluoikeus [hokses]
-  (mapv
-    #(assoc
-       %
-       :opiskeluoikeus
-       (koski/get-opiskeluoikeus-info (:opiskeluoikeus-oid %)))
-    hokses))
-
-(defn enrich-koodi-all [c kk ks]
-  (mapv #(koodisto/enrich % (get % kk) ks) c))
-
-(defn enrich-tutkinnon-osa-koodi-all [c]
-  (enrich-koodi-all c :tutkinnon-osa-koodi-uri [:tutkinnon-osa-koodisto-koodi]))
-
-(defn enrich-yto-koodi-all [c]
-  (enrich-tutkinnon-osa-koodi-all
-    (mapv
-      #(update
-         %
-         :osa-alueet
-         enrich-koodi-all
-         :osa-alue-koodi-uri
-         [:osa-alue-koodisto-koodi])
-      c)))
-
-(defn enrich-koodit [hoks]
-  (-> hoks
-      (koodisto/enrich (:urasuunnitelma-koodi-uri hoks) [:urasuunnitelma])
-      (update
-        :olemassa-olevat-ammatilliset-tutkinnon-osat
-        enrich-tutkinnon-osa-koodi-all)
-      (update
-        :olemassa-olevat-yhteiset-tutkinnon-osat enrich-yto-koodi-all)
-      (update
-        :puuttuvat-ammatilliset-tutkinnon-osat enrich-tutkinnon-osa-koodi-all)
-      (update
-        :puuttuvat-yhteiset-tutkinnon-osat enrich-yto-koodi-all)))
-
-(defn enrich-koodisto-koodit [m hoks]
-  (try
-    (update
-      m
-      :data
-      conj
-      (enrich-koodit hoks))
-    (catch Exception e
-      (let [data (ex-data e)]
-        (when (not= (:type data) :not-found) (throw e))
-        (update-in
-          (update m :data conj hoks)
-          [:meta :errors]
-          conj
-          {:error-type :not-found
-           :keys [:urasuunnitelma]
-           :path (:path data)})))))
-
-(defn with-koodisto [hokses]
-  (reduce
-    enrich-koodisto-koodit
-    {:data []
-     :meta {:errors []}}
-    hokses))
 
 (def routes
   (c-api/context "/oppija" []
@@ -114,14 +51,10 @@
 
           (c-api/GET "/hoks" [:as request]
             :summary "Oppijan HOKSit kokonaisuudessaan"
-            :return {:data [hoks-schema/OppijaHOKS]
-                     :meta schema/KoodistoErrorMeta}
+            :return (rest/response [oppija-schema/OppijaHOKS])
             (if (= (get-in request [:session :user :oid]) oid)
               (let [hokses (db/get-all-hoks-by-oppija oid)]
                 (if (empty? hokses)
                   (response/not-found "No HOKSes found")
-                  (-> (map #(dissoc % :id) hokses)
-                      with-opiskeluoikeus
-                      with-koodisto
-                      response/ok)))
+                  (response/ok (map #(dissoc % :id) hokses))))
               (response/unauthorized))))))))
