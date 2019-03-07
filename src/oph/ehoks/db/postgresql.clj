e349e791a997ab71da16592c71d0d05576a11bc4
Matti Ahinko <matti.ahinko@gmail.com>
2019-03-07 13:03:31 +0200

    Add hoks functions

--- src/oph/ehoks/db/postgresql.clj
+++ src/oph/ehoks/db/postgresql.clj
@@ -1,6 +1,25 @@
 (ns oph.ehoks.db.postgresql
   (:require [clojure.java.jdbc :as jdbc]
-            [oph.ehoks.config :refer [config]]))
+            [oph.ehoks.config :refer [config]]
+            [oph.ehoks.db.hoks :as h]
+            [clj-time.coerce :as c]
+            [oph.ehoks.db.queries :as queries]))
 
-(def pg-uri
-  {:connection-uri (:database-url config)})
+(extend-protocol jdbc/ISQLValue
+  java.util.Date
+  (sql-value [value] (c/to-sql-date value)))
+
+(defn clear! []
+  (jdbc/delete! {:connection-uri (:database-url config)} :hoksit []))
+
+(defn select-hoks-by-oppija-oid [oid]
+  (jdbc/query
+    {:connection-uri (:database-url config)}
+    [queries/select-hoks-by-oppija-oid oid]
+    {:row-fn h/hoks-from-sql}))
+
+(defn insert-hoks! [hoks]
+  (jdbc/insert!
+    {:connection-uri (:database-url config)}
+    :hoksit
+    (h/hoks-to-sql hoks)))
