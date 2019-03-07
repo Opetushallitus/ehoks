2d1f1f07f720d044e225aa3f6c3e9cfc0a2b02bd
Matti Ahinko <matti.ahinko@gmail.com>
2019-03-07 16:11:12 +0200

    Add tutkinnon osa queries

--- src/oph/ehoks/db/queries.clj
+++ src/oph/ehoks/db/queries.clj
@@ -4,4 +4,6 @@
 (defmacro defq [name filename]
   `(def ~name (slurp (io/file (io/resource ~filename))))) ;~(slurp (io/file (io/resource filename)))
 
-(defq select-hoks-by-oppija-oid "hoks/select_by_oppija_oid.sql")
+(defq select-hoks-by-oppija-oid "hoksit/select_by_oppija_oid.sql")
+(defq select-olemassa-olevat-ammatilliset-tutkinnon-osat-by-hoks-id
+  "olemassa-olevat-ammatilliset-tutkinnon-osat/select_by_hoks_id.sql")
