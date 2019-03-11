bd3fb8139ef88860041abe3b850ed4cbe374ad50
Matti Ahinko <matti.ahinko@gmail.com>
2019-03-11 09:52:50 +0200

    Remove comment

--- src/oph/ehoks/db/queries.clj
+++ src/oph/ehoks/db/queries.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :as io]))
 
 (defmacro defq [name filename]
-  `(def ~name (slurp (io/file (io/resource ~filename))))) ;~(slurp (io/file (io/resource filename)))
+  `(def ~name (slurp (io/file (io/resource ~filename)))))
 
 (defq select-hoks-by-oppija-oid "hoksit/select_by_oppija_oid.sql")
 (defq select-olemassa-olevat-ammatilliset-tutkinnon-osat-by-hoks-id
