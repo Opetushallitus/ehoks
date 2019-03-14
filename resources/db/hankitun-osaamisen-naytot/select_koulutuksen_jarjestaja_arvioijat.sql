061d37fcdeb3d372ddc4d4a805d52db9d51abb60
Matti Ahinko <matti.ahinko@gmail.com>
2019-03-14 13:20:32 +0200

    Change to left join

--- resources/db/hankitun-osaamisen-naytot/select_koulutuksen_jarjestaja_arvioijat.sql
+++ resources/db/hankitun-osaamisen-naytot/select_koulutuksen_jarjestaja_arvioijat.sql
@@ -1,5 +1,5 @@
 SELECT k.* FROM koulutuksen_jarjestaja_arvioijat AS k
-  RIGHT OUTER JOIN hankitun_osaamisen_nayton_koulutuksen_jarjestaja_arvioija AS h
+  LEFT OUTER JOIN hankitun_osaamisen_nayton_koulutuksen_jarjestaja_arvioija AS h
     ON (h.hankitun_osaamisen_naytto_id = k.id)
   WHERE h.hankitun_osaamisen_naytto_id = ? AND k.deleted_at IS NULL
   ORDER BY k.version DESC
--- resources/db/puuttuvat-paikalliset-tutkinnon-osat/select_hankitun_osaamisen_naytot.sql
+++ resources/db/puuttuvat-paikalliset-tutkinnon-osat/select_hankitun_osaamisen_naytot.sql
@@ -1,5 +1,5 @@
 SELECT h.* FROM hankitun_osaamisen_naytot AS h
-  RIGHT OUTER JOIN puuttuvan_paikallisen_tutkinnon_osan_hankitun_osaamisen_naytto AS p
+  LEFT OUTER JOIN puuttuvan_paikallisen_tutkinnon_osan_hankitun_osaamisen_naytto AS p
     ON (p.hankitun_osaamisen_naytto_id = h.id)
   WHERE p.puuttuva_paikallinen_tutkinnon_osa_id = ? AND h.deleted_at IS NULL
   ORDER BY h.version DESC
