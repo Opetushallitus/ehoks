b8b7f36668e6d55aa1db30a308f89aaecb2c487c
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-03-20 16:13:18 +0200

    changed permissions of run_postgres.sh

--- scripts/travis-scripts/run_postgres.sh
+++ scripts/travis-scripts/run_postgres.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 psql -c "create database ehoks_test;" -U postgres
-psql -d ehoks_test -f init_travis_postgres.sql
+psql -d ehoks_test -f scripts/travis-scripts/init_travis_postgres.sql
