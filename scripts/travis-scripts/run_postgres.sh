b52b483eae9af08abf9a7b9e352da0d5e6c05fcf
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-03-20 17:15:49 +0200

    added -d to docker run

--- scripts/travis-scripts/run_postgres.sh
+++ scripts/travis-scripts/run_postgres.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd scripts/postgres-docker
 docker build -t ehoks-postgres .
-docker run --rm --name ehoks-postgres -p 5433:5432 ehoks-postgres
+docker run --rm --name ehoks-postgres -p 5433:5432 -d ehoks-postgres
