62b448d1f3a42b119a8b7a438ceac9748f6b806a
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-03-20 14:33:22 +0200

    formatted test file

--- test/oph/ehoks/db/connect_test.clj
+++ test/oph/ehoks/db/connect_test.clj
@@ -6,10 +6,10 @@
 (def pg-uri
   {:connection-uri (:database-url config)})
 
-  (def create-table
-    (jdbc/create-table-ddl :test_table
-                           [[:name "varchar(32)"]
-                            [:value :int]]))
+(def create-table
+  (jdbc/create-table-ddl :test_table
+                         [[:name "varchar(32)"]
+                          [:value :int]]))
 
 (defn clear-table []
   (jdbc/db-do-commands pg-uri (str "DROP TABLE IF EXISTS test_table;")))
@@ -20,7 +20,7 @@
     (let [response-create (jdbc/db-do-commands pg-uri create-table)
           response-insert
           (jdbc/insert-multi! pg-uri :test_table
-          [{:name "First Name" :value 24}
-           {:name "Second Name" :value 49}])
+                              [{:name "First Name" :value 24}
+                               {:name "Second Name" :value 49}])
           response-query  (jdbc/query pg-uri ["select * from test_table"])]
       (is (= (:value (first response-query)) 24)))))
