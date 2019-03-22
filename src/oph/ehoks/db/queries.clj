2e0eb90158850232c7902a69aa4d1b151c5bddba
Matti Ahinko <matti.ahinko@gmail.com>
2019-03-22 12:11:03 +0200

    Rename macro

--- src/oph/ehoks/db/queries.clj
+++ src/oph/ehoks/db/queries.clj
@@ -1,44 +1,44 @@
 (ns oph.ehoks.db.queries
   (:require [clojure.java.io :as io]))
 
-(defmacro defquery [name filename]
+(defmacro query [name filename]
   `(def ~name (slurp (io/file (io/resource ~filename)))))
 
-(defquery select-hoks-by-oppija-oid "hoksit/select_by_oppija_oid.sql")
-(defquery select-hoks-by-id "hoksit/select_by_id.sql")
-(defquery select-olemassa-olevat-ammatilliset-tutkinnon-osat-by-hoks-id
+(query select-hoks-by-oppija-oid "hoksit/select_by_oppija_oid.sql")
+(query select-hoks-by-id "hoksit/select_by_id.sql")
+(query select-olemassa-olevat-ammatilliset-tutkinnon-osat-by-hoks-id
   "olemassa-olevat-ammatilliset-tutkinnon-osat/select_by_hoks_id.sql")
-(defquery select-hankitun-osaamisen-naytot-by-ooato-id
+(query select-hankitun-osaamisen-naytot-by-ooato-id
   (str "olemassa-olevat-ammatilliset-tutkinnon-osat/"
        "select_hankitun_osaamisen_naytot.sql"))
-(defquery select-puuttuvat-paikalliset-tutkinnon-osat-by-hoks-id
+(query select-puuttuvat-paikalliset-tutkinnon-osat-by-hoks-id
   "puuttuvat-paikalliset-tutkinnon-osat/select_by_hoks_id.sql")
-(defquery select-puuttuva-paikallinen-tutkinnon-osa-by-id
+(query select-puuttuva-paikallinen-tutkinnon-osa-by-id
   "puuttuvat-paikalliset-tutkinnon-osat/select_by_id.sql")
-(defquery select-olemassa-olevat-paikalliset-tutkinnon-osat-by-hoks-id
+(query select-olemassa-olevat-paikalliset-tutkinnon-osat-by-hoks-id
   "olemassa-olevat-paikalliset-tutkinnon-osat/select_by_hoks_id.sql")
-(defquery select-olemassa-olevat-yhteiset-tutkinnon-osat-by-hoks-id
+(query select-olemassa-olevat-yhteiset-tutkinnon-osat-by-hoks-id
   "olemassa-olevat-yhteiset-tutkinnon-osat/select_by_hoks_id.sql")
-(defquery select-hankitun-osaamisen-naytot-by-ppto-id
+(query select-hankitun-osaamisen-naytot-by-ppto-id
   "puuttuvat-paikalliset-tutkinnon-osat/select_hankitun_osaamisen_naytot.sql")
-(defquery select-koulutuksen-jarjestaja-arvioijat-by-hon-id
+(query select-koulutuksen-jarjestaja-arvioijat-by-hon-id
   "hankitun-osaamisen-naytot/select_koulutuksen_jarjestaja_arvioijat.sql")
-(defquery select-tyoelama-arvioijat-by-hon-id
+(query select-tyoelama-arvioijat-by-hon-id
   "hankitun-osaamisen-naytot/select_tyoelama_arvioijat.sql")
-(defquery select-nayttoymparisto-by-id "nayttoymparistot/select_by_id.sql")
-(defquery select-tyotehtavat-by-hankitun-osaamisen-naytto-id
+(query select-nayttoymparisto-by-id "nayttoymparistot/select_by_id.sql")
+(query select-tyotehtavat-by-hankitun-osaamisen-naytto-id
   "hankitun-osaamisen-naytot/select_tyotehtavat.sql")
-(defquery select-osaamisen-hankkmistavat-by-ppto-id
+(query select-osaamisen-hankkmistavat-by-ppto-id
   "puuttuvat-paikalliset-tutkinnon-osat/select_osaamisen_hankkimistavat.sql")
-(defquery select-tyopaikalla-hankittava-osaaminen-by-id
+(query select-tyopaikalla-hankittava-osaaminen-by-id
   "tyopaikalla-hankittavat-osaamiset/select_by_id.sql")
-(defquery select-henkilot-by-tho-id
+(query select-henkilot-by-tho-id
   "tyopaikalla-hankittavat-osaamiset/select_henkilot.sql")
-(defquery select-tyotehtavat-by-tho-id
+(query select-tyotehtavat-by-tho-id
   "tyopaikalla-hankittavat-osaamiset/select_tyotehtavat.sql")
-(defquery select-muut-oppimisymparistot-by-osaamisen-hankkimistapa-id
+(query select-muut-oppimisymparistot-by-osaamisen-hankkimistapa-id
   "muut-oppimisymparistot/select_by_osaamisen_hankkimistapa_id.sql")
-(defquery select-todennettu-arviointi-lisatiedot-by-id
+(query select-todennettu-arviointi-lisatiedot-by-id
   "todennettu-arviointi-lisatiedot/select_by_id.sql")
-(defquery select-arvioijat-by-todennettu-arviointi-id
+(query select-arvioijat-by-todennettu-arviointi-id
   "todennettu-arviointi-lisatiedot/select_arvioijat.sql")
