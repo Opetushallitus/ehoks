5403d8d6cf747637122551744c2b471f71221f0c
Matti Ahinko <matti.ahinko@gmail.com>
2019-03-22 12:32:49 +0200

    Fix indent

--- src/oph/ehoks/db/queries.clj
+++ src/oph/ehoks/db/queries.clj
@@ -7,38 +7,40 @@
 (defq select-hoks-by-oppija-oid "hoksit/select_by_oppija_oid.sql")
 (defq select-hoks-by-id "hoksit/select_by_id.sql")
 (defq select-olemassa-olevat-ammatilliset-tutkinnon-osat-by-hoks-id
-  "olemassa-olevat-ammatilliset-tutkinnon-osat/select_by_hoks_id.sql")
+      "olemassa-olevat-ammatilliset-tutkinnon-osat/select_by_hoks_id.sql")
 (defq select-hankitun-osaamisen-naytot-by-ooato-id
-  (str "olemassa-olevat-ammatilliset-tutkinnon-osat/"
-       "select_hankitun_osaamisen_naytot.sql"))
+      (str "olemassa-olevat-ammatilliset-tutkinnon-osat/"
+           "select_hankitun_osaamisen_naytot.sql"))
 (defq select-puuttuvat-paikalliset-tutkinnon-osat-by-hoks-id
-  "puuttuvat-paikalliset-tutkinnon-osat/select_by_hoks_id.sql")
+      "puuttuvat-paikalliset-tutkinnon-osat/select_by_hoks_id.sql")
 (defq select-puuttuva-paikallinen-tutkinnon-osa-by-id
-  "puuttuvat-paikalliset-tutkinnon-osat/select_by_id.sql")
+      "puuttuvat-paikalliset-tutkinnon-osat/select_by_id.sql")
 (defq select-olemassa-olevat-paikalliset-tutkinnon-osat-by-hoks-id
-  "olemassa-olevat-paikalliset-tutkinnon-osat/select_by_hoks_id.sql")
+      "olemassa-olevat-paikalliset-tutkinnon-osat/select_by_hoks_id.sql")
 (defq select-olemassa-olevat-yhteiset-tutkinnon-osat-by-hoks-id
-  "olemassa-olevat-yhteiset-tutkinnon-osat/select_by_hoks_id.sql")
+      "olemassa-olevat-yhteiset-tutkinnon-osat/select_by_hoks_id.sql")
 (defq select-hankitun-osaamisen-naytot-by-ppto-id
-  "puuttuvat-paikalliset-tutkinnon-osat/select_hankitun_osaamisen_naytot.sql")
+      (str "puuttuvat-paikalliset-tutkinnon-osat/"
+           "select_hankitun_osaamisen_naytot.sql"))
 (defq select-koulutuksen-jarjestaja-arvioijat-by-hon-id
-  "hankitun-osaamisen-naytot/select_koulutuksen_jarjestaja_arvioijat.sql")
+      "hankitun-osaamisen-naytot/select_koulutuksen_jarjestaja_arvioijat.sql")
 (defq select-tyoelama-arvioijat-by-hon-id
-  "hankitun-osaamisen-naytot/select_tyoelama_arvioijat.sql")
+      "hankitun-osaamisen-naytot/select_tyoelama_arvioijat.sql")
 (defq select-nayttoymparisto-by-id "nayttoymparistot/select_by_id.sql")
 (defq select-tyotehtavat-by-hankitun-osaamisen-naytto-id
-  "hankitun-osaamisen-naytot/select_tyotehtavat.sql")
+      "hankitun-osaamisen-naytot/select_tyotehtavat.sql")
 (defq select-osaamisen-hankkmistavat-by-ppto-id
-  "puuttuvat-paikalliset-tutkinnon-osat/select_osaamisen_hankkimistavat.sql")
+      "puuttuvat-paikalliset-tutkinnon-osat/"
+      "select_osaamisen_hankkimistavat.sql")
 (defq select-tyopaikalla-hankittava-osaaminen-by-id
-  "tyopaikalla-hankittavat-osaamiset/select_by_id.sql")
+      "tyopaikalla-hankittavat-osaamiset/select_by_id.sql")
 (defq select-henkilot-by-tho-id
-  "tyopaikalla-hankittavat-osaamiset/select_henkilot.sql")
+      "tyopaikalla-hankittavat-osaamiset/select_henkilot.sql")
 (defq select-tyotehtavat-by-tho-id
-  "tyopaikalla-hankittavat-osaamiset/select_tyotehtavat.sql")
+      "tyopaikalla-hankittavat-osaamiset/select_tyotehtavat.sql")
 (defq select-muut-oppimisymparistot-by-osaamisen-hankkimistapa-id
-  "muut-oppimisymparistot/select_by_osaamisen_hankkimistapa_id.sql")
+      "muut-oppimisymparistot/select_by_osaamisen_hankkimistapa_id.sql")
 (defq select-todennettu-arviointi-lisatiedot-by-id
-  "todennettu-arviointi-lisatiedot/select_by_id.sql")
+      "todennettu-arviointi-lisatiedot/select_by_id.sql")
 (defq select-arvioijat-by-todennettu-arviointi-id
-  "todennettu-arviointi-lisatiedot/select_arvioijat.sql")
+      "todennettu-arviointi-lisatiedot/select_arvioijat.sql")
