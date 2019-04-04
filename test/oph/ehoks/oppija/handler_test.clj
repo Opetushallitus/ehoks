29cc4b4db5fd24cb80f9c500f23922b52024b9b4
Matti Ahinko <matti.ahinko@gmail.com>
2019-04-04 12:16:54 +0300

    Fix cleaning test data

--- test/oph/ehoks/oppija/handler_test.clj
+++ test/oph/ehoks/oppija/handler_test.clj
@@ -3,134 +3,60 @@
             [oph.ehoks.handler :refer [create-app]]
             [ring.mock.request :as mock]
             [oph.ehoks.utils :as utils :refer [eq]]
-            [oph.ehoks.db.memory :as db]
             [oph.ehoks.external.http-client :as client]
-            [oph.ehoks.session-store :refer [test-session-store]]))
+            [oph.ehoks.session-store :refer [test-session-store]]
+            [oph.ehoks.hoks.hoks :as h]
+            [oph.ehoks.db.migrations :as m]
+            [oph.ehoks.hoks.hoks-test :refer [hoks-data]]
+            [clojure.walk :as w]))
 
 (def url "/ehoks-backend/api/v1/oppija/oppijat")
 
-(def hoks
-  {:puuttuvat-paikalliset-tutkinnon-osat
-   [{:koulutuksen-jarjestaja-oid "1.2.246.562.10.54453921329"
-     :hankitun-osaamisen-naytto []
-     :osaamisen-hankkimistavat []
-     :tavoitteet-ja-sisallot ""
-     :nimi "Orientaatio alaan"}
-    {:koulutuksen-jarjestaja-oid "1.2.246.562.10.54453921329"
-     :hankitun-osaamisen-naytto []
-     :osaamisen-hankkimistavat []
-     :nimi "Infotilaisuus"}
-    {:koulutuksen-jarjestaja-oid "1.2.246.562.10.54453921329"
-     :hankitun-osaamisen-naytto []
-     :osaamisen-hankkimistavat []
-     :nimi "Opintojen ohjaus"}
-    {:koulutuksen-jarjestaja-oid "1.2.246.562.10.54453921329"
-     :hankitun-osaamisen-naytto []
-     :osaamisen-hankkimistavat []
-     :nimi "Tutkintotilaisuus"}
-    {:koulutuksen-jarjestaja-oid "1.2.246.562.10.54453921329"
-     :hankitun-osaamisen-naytto []
-     :osaamisen-hankkimistavat []
-     :nimi "Työpaikalla oppiminen "}]
-   :ensikertainen-hyvaksyminen (java.time.LocalDate/now)
-   :luotu (java.util.Date.)
-   :urasuunnitelma-koodi-uri "urasuunnitelma_0001"
-   :urasuunnitelma-koodi-versio 1
-   :hyvaksytty (java.util.Date.)
-   :olemassa-olevat-ammatilliset-tutkinnon-osat []
-   :olemassa-olevat-yhteiset-tutkinnon-osat []
-   :puuttuvat-ammatilliset-tutkinnon-osat
-   [{:tutkinnon-osa-koodi-uri "tutkinnonosat_103590"
-     :tutkinnon-osa-koodi-versio 1
-     :koulutuksen-jarjestaja-oid "1.2.246.562.10.54453921329"
-     :hankitun-osaamisen-naytto
-     [{:alku (java.time.LocalDate/now)
-       :loppu (java.time.LocalDate/now)
-       :jarjestaja {:oppilaitos-oid "1.2.246.562.10.54453921329"}
-       :nayttoymparisto {:nimi "" :y-tunnus ""}
-       :koulutuksen-jarjestaja-arvioijat
-       [{:nimi "Olson,Wava"
-         :organisaatio {:oppilaitos-oid "1.2.246.562.10.54453921329"}}]}]
-     :osaamisen-hankkimistavat []}
-    {:tutkinnon-osa-koodi-uri "tutkinnonosat_103590"
-     :tutkinnon-osa-koodi-versio 1
-     :koulutuksen-jarjestaja-oid "1.2.246.562.10.54453921329"
-     :hankitun-osaamisen-naytto
-     [{:alku (java.time.LocalDate/now)
-       :loppu (java.time.LocalDate/now)
-       :jarjestaja {:oppilaitos-oid "1.2.246.562.10.54453921329"}
-       :nayttoymparisto {:nimi "", :y-tunnus ""}
-       :koulutuksen-jarjestaja-arvioijat
-       [{:nimi "Moen,Pearl"
-         :organisaatio {:oppilaitos-oid "1.2.246.562.10.54453921329"}}]}]
-     :osaamisen-hankkimistavat []}]
-   :puuttuvat-yhteiset-tutkinnon-osat
-   [{:tutkinnon-osa-koodi-uri "tutkinnonosat_103596"
-     :tutkinnon-osa-koodi-versio 1
-     :osa-alueet
-     [{:osa-alue-koodi-uri "ammatillisenoppiaineet_fk"
-       :osa-alue-koodi-versio 1}]
-     :koulutuksen-jarjestaja-oid "1.2.246.562.10.54453921329"}]
-   :opiskeluoikeus-oid "1.2.246.562.15.76811932037"
-   :laatija {:nimi "Simonis,Hollie"}
-   :versio 0
-   :paivitetty (java.util.Date.)
-   :eid "0000"
-   :paivittaja {:nimi "Ei tietoa"}
-   :oppija-oid "1.2.246.562.24.29790141661"
-   :hyvaksyja {:nimi "Ei tietoa"}
-   :opiskeluvalmiuksia-tukevat-opinnot []})
+(defn with-database [f]
+  (f)
+  (m/clean!)
+  (m/migrate!))
 
-(defn set-hoks-data! [h]
-  (reset!
-    db/hoks-store
-    [(assoc h :versio 1 :paivittaja {:nimi "Tapio Testaaja"})
-     h]))
+(defn create-db [f]
+  (m/migrate!)
+  (f)
+  (m/clean!))
+
+(use-fixtures :each with-database)
+
+(use-fixtures :once create-db)
+
+(def dates #{:alku :loppu :lahetetty-arvioitavaksi :ensikertainen-hyvaksyminen})
+
+(defn v-to-str [m k]
+  (if (some? (get m k))
+    (update m k str)
+    m))
+
+(defn dates-to-str [c]
+  (w/postwalk
+    #(if (map? %)
+       (reduce v-to-str % dates)
+       %)
+    c))
 
 (deftest get-hoks
   (testing "GET enriched HOKS"
-    (set-hoks-data! hoks)
-    (let [store (atom {})
+    (h/save-hoks! hoks-data)
+    (let [oppija-oid (:oppija-oid hoks-data)
+          store (atom {})
           app (create-app (test-session-store store))
           response
           (utils/with-authenticated-oid
             store
-            (:oppija-oid hoks)
+            oppija-oid
             app
             (mock/request
               :get
-              (format "%s/%s/hoks" url (:oppija-oid hoks))))]
+              (format "%s/%s/hoks" url oppija-oid)))]
       (is (= (:status response) 200))
       (let [body (utils/parse-body (:body response))]
         (eq
-          (update-in
-            body
-            [:data 0]
-            dissoc
-            :luotu :paivitetty :hyvaksytty :ensikertainen-hyvaksyminen)
-          {:data
-           [(-> hoks
-                (dissoc
-                  :luotu
-                  :paivitetty
-                  :hyvaksytty
-                  :ensikertainen-hyvaksyminen)
-                (assoc
-                  :paivittaja {:nimi "Tapio Testaaja"}
-                  :versio 1)
-                (update
-                  :puuttuvat-ammatilliset-tutkinnon-osat
-                  (fn [oc]
-                    (mapv
-                      (fn [o]
-                        (update
-                          o
-                          :hankitun-osaamisen-naytto
-                          (fn [c]
-                            (mapv
-                              #(update
-                                 (update %  :alku str)
-                                 :loppu str)
-                              c))))
-                      oc))))]
-           :meta {}})))))
+          (:data body)
+          [(dates-to-str
+             (assoc hoks-data :eid (get-in body [:data 0 :eid])))])))))
