691781a373958de8a2a88a7af97730210b8cbfa0
Matti Ahinko <matti.ahinko@gmail.com>
2019-04-18 14:01:49 +0300

    Add running migrations on start

--- src/oph/ehoks/main.clj
+++ src/oph/ehoks/main.clj
@@ -1,13 +1,19 @@
 (ns oph.ehoks.main
-  (:gen-class))
+  (:gen-class)
+  (:require [oph.ehoks.db.migrations :as m]
+            [clojure.string :refer [lower-case]]
+            [clojure.tools.logging :as log]))
 
-(defn -main []
+(defn -main [& args]
   (require 'ring.adapter.jetty)
   (require 'oph.ehoks.config)
   (require 'oph.ehoks.handler)
   (let [run-jetty (resolve 'ring.adapter.jetty/run-jetty)
-        config    (var-get (resolve 'oph.ehoks.config/config))
-        app       (resolve 'oph.ehoks.handler/app)]
+        config (var-get (resolve 'oph.ehoks.config/config))
+        app (resolve 'oph.ehoks.handler/app)]
+    (when-not (some #(when (= (lower-case %) "--no-migrations") %) args)
+      (log/info "Running migrations")
+      (m/migrate!))
     (run-jetty app {:port  (:port config)
                     :join? true
                     :async? true})))
