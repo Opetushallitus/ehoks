173c34749b6754358f2d51c9633f315548703ff3
Matti Ahinko <matti.ahinko@gmail.com>
2019-05-08 10:30:11 +0300

    Add support for create app without session store

--- src/oph/ehoks/common/api.clj
+++ src/oph/ehoks/common/api.clj
@@ -18,9 +18,11 @@
        (log/error ex (.getMessage ex)))
      (response/internal-server-error {:type "unknown-exception"}))})
 
-(defn create-app [app-routes session-store]
-  (-> app-routes
-      (middleware/wrap-cache-control-no-cache)
-      (session/wrap-session
-        {:store (or session-store (mem/memory-store))
-         :cookie-attrs {:max-age (:session-max-age config (* 60 60 4))}})))
+(defn create-app
+  ([app-routes session-store]
+    (-> app-routes
+        (middleware/wrap-cache-control-no-cache)
+        (session/wrap-session
+          {:store (or session-store (mem/memory-store))
+           :cookie-attrs {:max-age (:session-max-age config (* 60 60 4))}})))
+  ([app-routes] (create-app app-routes nil)))
