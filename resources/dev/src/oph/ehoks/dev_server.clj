cea8b930f56988eda23ee5d51a7493f77eeb34dc
Matti Ahinko <matti.ahinko@gmail.com>
2019-05-08 10:28:20 +0300

    Use splitted backends

--- resources/dev/src/oph/ehoks/dev_server.clj
+++ resources/dev/src/oph/ehoks/dev_server.clj
@@ -1,5 +1,7 @@
 (ns oph.ehoks.dev-server
-  (:require [oph.ehoks.handler :refer [app]]
+  (:require [oph.ehoks.handler :as hoks-api-handler]
+            [oph.ehoks.virkailija.handler :as virkailija-handler]
+            [oph.ehoks.common.api :as common-api]
             [compojure.core :refer [GET defroutes routes]]
             [ring.util.http-response :refer [ok not-found]]
             [ring.adapter.jetty :as jetty]
@@ -12,7 +14,8 @@
             [ring.middleware.cookies :refer [wrap-cookies]]
             [ring.middleware.params :refer [wrap-params]]
             [clojure.tools.logging :as log]
-            [cheshire.core :as cheshire]))
+            [cheshire.core :as cheshire]
+            [oph.ehoks.redis :refer [redis-store]]))
 
 (defn uri-to-filename [uri]
   (-> uri
@@ -30,7 +33,7 @@
     #(when (= (.getName %) filename) %)
     (file-seq (io/file (io/resource "dev-routes")))))
 
-(defroutes dev-routes
+(defroutes dev-hoks-api-routes
   (GET "/dev-routes/*" request
     (let [filename (uri-to-filename (:uri request))
           file (find-dev-route-file filename)]
@@ -43,6 +46,20 @@
           (ok (slurp file))
           (not-found "{}"))
         [:headers "Content-Type"] "application/json"))))
+
+(def hoks-api-app
+  (common-api/create-app
+    hoks-api-handler/app-routes
+    (when (seq (:redis-url config))
+      (redis-store {:pool {}
+                    :spec {:uri (:redis-url config)}}))))
+
+(def virkailija-app
+  (common-api/create-app
+    virkailija-handler/app-routes
+    (when (seq (:redis-url config))
+      (redis-store {:pool {}
+                    :spec {:uri (:redis-url config)}}))))
 
 (defn set-cors [response]
   (-> response
@@ -63,12 +80,19 @@
       (let [response (handler request)]
         (set-cors response)))))
 
-(def dev-app
+(def dev-virkailija-app
+  (wrap-dev-cors
+    (routes
+;      (wrap-params (wrap-cookies (wrap-reload #'mock/mock-routes)))
+
+      (wrap-reload #'virkailija-app))))
+
+(def dev-hoks-api-app
   (wrap-dev-cors
     (routes
       (wrap-params (wrap-cookies (wrap-reload #'mock/mock-routes)))
-      (wrap-reload #'dev-routes)
-      (wrap-reload #'app))))
+      (wrap-reload #'dev-hoks-api-routes)
+      (wrap-reload #'hoks-api-app))))
 
 (defn start-server
   ([config-file]
@@ -82,10 +106,14 @@
      (require 'oph.ehoks.external.oph-url :reload))
    (log/info "Starting development server...")
    (log/info "Not safe for production or public environments.")
-   (jetty/run-jetty dev-app
-                    {:port (:port config)
-                     :join? false
-                     :async? true}))
+   {:virkailija (jetty/run-jetty dev-virkailija-app
+                                 {:port (:virkailija-backend-port config)
+                                  :join? false
+                                  :async? true})
+    :hoks-api (jetty/run-jetty dev-hoks-api-app
+                               {:port (:port config)
+                                :join? false
+                                :async? true})})
   ([] (start-server nil)))
 
 (defn -main []
