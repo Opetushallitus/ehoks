6bc0010101c5c073c2b89e618c1776107dc01f6c
Matti Ahinko <matti.ahinko@gmail.com>
2019-06-05 15:34:27 +0300

    Move creating app to function

--- src/oph/ehoks/ehoks_app.clj
+++ src/oph/ehoks/ehoks_app.clj
@@ -6,12 +6,13 @@
             [oph.ehoks.config :refer [config]]
             [clojure.string :refer [lower-case]]))
 
-(def app
-  (when-not *compile-files*
-    (common-api/create-app
-      (if (= (lower-case (or (System/getProperty "NAME") "")) "virkailija")
-        virkailija-handler/app-routes
-        oppija-handler/app-routes)
-      (when (seq (:redis-url config))
-        (redis-store {:pool {}
-                      :spec {:uri (:redis-url config)}})))))
+(defn create-app [app-name]
+  (common-api/create-app
+    (if (= app-name "virkailija")
+      virkailija-handler/app-routes
+      oppija-handler/app-routes)
+    (when (seq (:redis-url config))
+      (redis-store {:pool {}
+                    :spec {:uri (:redis-url config)}}))))
+
+(def app (create-app (lower-case (or (System/getProperty "NAME") ""))))
