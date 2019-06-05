914702f507c41ba6a4680ae30923ab6efb909b87
Matti Ahinko <matti.ahinko@gmail.com>
2019-06-05 14:55:45 +0300

    Add selecting app

--- src/oph/ehoks/ehoks_app.clj
+++ src/oph/ehoks/ehoks_app.clj
@@ -1,12 +1,17 @@
 (ns oph.ehoks.ehoks-app
   (:require [oph.ehoks.common.api :as common-api]
             [oph.ehoks.redis :refer [redis-store]]
-            [oph.ehoks.handler :as handler]
-            [oph.ehoks.config :refer [config]]))
+            [oph.ehoks.oppija.handler :as oppija-handler]
+            [oph.ehoks.virkailija.handler :as virkailija-handler]
+            [oph.ehoks.config :refer [config]]
+            [clojure.string :refer [lower-case]]))
 
 (def app
-  (common-api/create-app
-    handler/app-routes
-    (when (seq (:redis-url config))
-      (redis-store {:pool {}
-                    :spec {:uri (:redis-url config)}}))))
+  (when-not *compile-files*
+    (common-api/create-app
+      (if (= (lower-case (or (System/getProperty "NAME") "")) "virkailija")
+        virkailija-handler/app-routes
+        oppija-handler/app-routes)
+      (when (seq (:redis-url config))
+        (redis-store {:pool {}
+                      :spec {:uri (:redis-url config)}})))))
