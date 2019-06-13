7bfa936948d412e41bb0817a5266bc2c77755871
Matti Ahinko <matti.ahinko@gmail.com>
2019-06-13 08:31:19 +0300

    Add route for virkailija session

--- src/oph/ehoks/virkailija/auth.clj
+++ src/oph/ehoks/virkailija/auth.clj
@@ -6,7 +6,9 @@
             [oph.ehoks.external.cas :as cas]
             [oph.ehoks.user :as user]
             [oph.ehoks.external.oph-url :as u]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [oph.ehoks.virkailija.schema :as schema]
+            [oph.ehoks.restful :as restful]))
 
 (def routes
   (c-api/context "/session" []
@@ -26,6 +28,14 @@
           (do (log/warnf "Ticket validation failed: %s"
                          (:error validation-data))
               (response/unauthorized {:error "Invalid ticket"})))))
+
+    (c-api/GET "/" request
+      :summary "Virkailijan istunto"
+      :return (restful/response schema/VirkailijaSession)
+      (if-let [virkailija-user (get-in request [:session :virkailija-user])]
+        (restful/rest-ok
+          (select-keys virkailija-user [:oidHenkilo :organisation-privileges]))
+        (response/unauthorized {:info "User is not authorized"})))
 
     (c-api/DELETE "/" []
       :summary "Virkailijan istunnon päättäminen"
