8c44102d81dddfbd5a78762d10f081a9e26fd0f0
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2019-06-17 15:14:11 +0300

    Removed unused test data, uncommented failing test for PATCH of hyto.

--- test/oph/ehoks/hoks/handler_test.clj
+++ test/oph/ehoks/hoks/handler_test.clj
@@ -743,27 +743,6 @@
                                        {:nimi "Organisaation nimi"}}]}]}]
    :koulutuksen-jarjestaja-oid "1.2.246.562.10.00000000009"})
 
-(def hyto-patch-one-data
-  {:tutkinnon-osa-koodi-uri "tutkinnonosat_3002683"
-   :tutkinnon-osa-koodi-versio 1
-   :osa-alueet
-   [{:osa-alue-koodi-uri "ammatillisenoppiaineet_ku"
-     :osa-alue-koodi-versio 1
-     :osaamisen-hankkimistavat
-     [{:alku "2018-12-13"
-       :loppu "2018-12-22"
-       :osaamisen-hankkimistapa-koodi-uri "osaamisenhankkimistapa_oppisopimus"
-       :osaamisen-hankkimistapa-koodi-versio 1}]
-     :osaamisen-osoittaminen
-     [{:jarjestaja {:oppilaitos-oid "1.2.246.562.10.00000000010"}
-       :nayttoymparisto {:nimi "aaa2"}
-       :alku "2018-12-15"
-       :loppu "2018-12-21"
-       :sisallon-kuvaus ["Kuvaus"]
-       :tyoelama-osaamisen-arvioijat [{:nimi "Nimi" :organisaatio
-                                       {:nimi "Organisaation nimi"}}]}]}]
-   :koulutuksen-jarjestaja-oid "1.2.246.562.10.00000000011"})
-
 (deftest post-and-get-hankittava-yhteinen-tukinnon-osa
   (testing "POST hankittavat yhteisen tutkinnon osat"
     (with-hoks
@@ -802,31 +781,29 @@
 ;                  (assoc hyto-data :id 1))))]
 ;      (is (= (:status response) 204)))))
 
-;(deftest patch-one-hankittava-yhteinen-tutkinnon-osa
-;  (testing "PATCH one value hankittavat yhteisen tutkinnon osat"
-;    (db/clear)
-;    (let [post-response
-;          (utils/with-service-ticket
-;            (create-app nil)
-;            (-> (mock/request
-;                  :post
-;                  (format
-;                    "%s/1/%s"
-;                    url hyto-path))
-;                (mock/json-body hyto-data)))
-;          response
-;          (utils/with-service-ticket
-;            (create-app nil)
-;            (-> (mock/request
-;                  :patch
-;                  (format
-;                    "%s/1/%s/1"
-;                    url hyto-path))
-;                (mock/json-body
-;                  {:koulutuksen-jarjestaja-oid
-;                   "1.2.246.562.10.00000000012"})))]
-;      (is (= (:status response) 204)))))
-;
+(deftest patch-one-value-of-hankittava-yhteinen-tutkinnon-osa
+  (testing "PATCH one value hankittavat yhteisen tutkinnon osat"
+    (let [post-response
+          (utils/with-service-ticket
+            (create-app nil)
+            (-> (mock/request
+                  :post
+                  (format
+                    "%s/1/%s"
+                    url hyto-path))
+                (mock/json-body hyto-data)))
+          response
+          (utils/with-service-ticket
+            (create-app nil)
+            (-> (mock/request
+                  :patch
+                  (format
+                    "%s/1/%s/1"
+                    url hyto-path))
+                (mock/json-body
+                  {:koulutuksen-jarjestaja-oid "1.2.246.562.10.00000000012"})))]
+      (is (= (:status response) 204)))))
+
 ;(deftest patch-all-hankittavat-yhteiset-tutkinnon-osat
 ;  (testing "PATCH all hankittavat yhteisen tutkinnon osat"
 ;    (db/clear)
