ca64c7f2a2ec55d9f3b6b9c4dcd15c21f5bd614e
Joona Haavisto <joona.haavisto@gofore.com>
2019-07-05 14:26:37 +0300

    Lisätty testi opiskeluoikeuksien haulle uudella Koski endpointilla

--- test/oph/ehoks/external/koski_test.clj
+++ test/oph/ehoks/external/koski_test.clj
@@ -1,6 +1,8 @@
 (ns oph.ehoks.external.koski-test
   (:require [clojure.test :refer [deftest testing is]]
-            [oph.ehoks.external.koski :as k]))
+            [oph.ehoks.external.koski :as k]
+            [oph.ehoks.external.http-client :as client]
+            [cheshire.core :as json]))
 
 (deftest test-filter-oppija
   (testing "Filtering Oppija values"
@@ -21,3 +23,33 @@
                       :kutsumanimi "Aarto"
                       :sukunimi "Väisänen-perftest"}
             :opiskeluoikeudet [{}]}))))
+
+(deftest test-get-oppija-opiskeluoikeudet
+  (testing "Get opiskeluoikeudet for oppija"
+    (client/set-post!
+      (fn [url options]
+        (cond
+          (.endsWith
+            url "/koski/api/sure/oids")
+          (let [oids (json/parse-string (:body options))]
+            {:status 200
+             :body (map
+                     (fn [oid]
+                       {:henkilö {:oid oid}
+                        :opiskeluoikeudet
+                                 [{:oid "1.2.246.562.15.55003456345"
+                                   :oppilaitos
+                                        {:oid "1.2.246.562.10.12944436166"}}
+                                  {:oid "1.2.246.562.15.55003456345"
+                                   :oppilaitos
+                                        {:oid "1.2.246.562.10.12944436166"}}]})
+                        oids)}))))
+
+    (is (= (k/get-oppija-opiskeluoikeudet "1.2.246.562.24.51659804532")
+           [{:oid "1.2.246.562.15.55003456345"
+             :oppilaitos
+                  {:oid "1.2.246.562.10.12944436166"}}
+            {:oid "1.2.246.562.15.55003456345"
+             :oppilaitos
+                  {:oid "1.2.246.562.10.12944436166"}}]))
+    (client/reset-functions!)))
