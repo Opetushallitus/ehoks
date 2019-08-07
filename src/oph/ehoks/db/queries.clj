1970097702b728751542f8566434bc7df03d72dc
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-08-07 13:07:22 +0300

    eh-314 fixed indentation

--- src/oph/ehoks/db/queries.clj
+++ src/oph/ehoks/db/queries.clj
@@ -208,81 +208,81 @@
      :secondary-column "osaamisen_osoittaminen_id"
      :primary-column "id"
      :column "yhteisen_tutkinnon_osan_osa_alue_id"}))
- (def select-ahyto-ids-by-hoks-id
-   (generate-select-id-by
-     {:id "id"
-      :table "aiemmin_hankitut_yhteiset_tutkinnon_osat"
-      :column "hoks_id"}))
- (def select-oo-ids-by-ahyto-id
-   (generate-select-id-by
-     {:id "osaamisen_osoittaminen_id"
-      :table "aiemmin_hankitun_yhteisen_tutkinnon_osan_naytto"
-      :column "aiemmin_hankittu_yhteinen_tutkinnon_osa_id"}))
- (def select-ahyto-osa-alue-ids-by-ahyto-id
-   (generate-select-id-by
-     {:id "id"
-      :table "aiemmin_hankitut_yto_osa_alueet"
-      :column "aiemmin_hankittu_yhteinen_tutkinnon_osa_id"}))
- (def select-oo-ids-by-ahyto-osa-alue-id
-   (generate-select-id-by
-     {:id "osaamisen_osoittaminen_id"
-      :table "aiemmin_hankitun_yto_osa_alueen_naytto"
-      :column "aiemmin_hankittu_yto_osa_alue_id"}))
- (def select-ahato-ids-by-hoks-id
-   (generate-select-id-by
-     {:id "id"
-      :table "aiemmin_hankitut_ammat_tutkinnon_osat"
-      :column "hoks_id"}))
- (def select-oo-ids-by-ahato-id
-   (generate-select-id-by
-     {:id "osaamisen_osoittaminen_id"
-      :table "aiemmin_hankitun_ammat_tutkinnon_osan_naytto"
-      :column "aiemmin_hankittu_ammat_tutkinnon_osa_id"}))
- (def select-ahpto-ids-by-hoks-id
-   (generate-select-id-by
-     {:id "id"
-      :table "aiemmin_hankitut_paikalliset_tutkinnon_osat"
-      :column "hoks_id"}))
- (def select-oo-ids-by-ahpto-id
-   (generate-select-id-by
-     {:id "osaamisen_osoittaminen_id"
-      :table "aiemmin_hankitun_paikallisen_tutkinnon_osan_naytto"
-      :column "aiemmin_hankittu_paikallinen_tutkinnon_osa_id"}))
- (def select-hato-ids-by-hoks-id
-   (generate-select-id-by
-     {:id "id"
-      :table "hankittavat_ammat_tutkinnon_osat"
-      :column "hoks_id"}))
- (def select-oo-ids-by-hato-id
-   (generate-select-id-by
-     {:id "osaamisen_osoittaminen_id"
-      :table "hankittavan_ammat_tutkinnon_osan_naytto"
-      :column "hankittava_ammat_tutkinnon_osa_id"}))
- (def select-hpto-ids-by-hoks-id
-   (generate-select-id-by
-     {:id "id"
-      :table "hankittavat_paikalliset_tutkinnon_osat"
-      :column "hoks_id"}))
- (def select-oo-ids-by-hpto-id
-   (generate-select-id-by
-     {:id "osaamisen_osoittaminen_id"
-      :table "hankittavan_paikallisen_tutkinnon_osan_naytto"
-      :column "hankittava_paikallinen_tutkinnon_osa_id"}))
- (def select-hyto-ids-by-hoks-id
-   (generate-select-id-by
-     {:id "id"
-      :table "hankittavat_yhteiset_tutkinnon_osat"
-      :column "hoks_id"}))
- (def select-hyto-osa-alue-ids-by-hyto-id
-   (generate-select-id-by
-     {:id "id"
-      :table "yhteisen_tutkinnon_osan_osa_alueet"
-      :column "yhteinen_tutkinnon_osa_id"}))
- (def select-oo-ids-by-hyto-osa-alue-id
-   (generate-select-id-by
-     {:id "osaamisen_osoittaminen_id"
-      :table "yhteisen_tutkinnon_osan_osa_alueen_naytot"
-      :column "yhteisen_tutkinnon_osan_osa_alue_id"}))
+(def select-ahyto-ids-by-hoks-id
+  (generate-select-id-by
+    {:id "id"
+     :table "aiemmin_hankitut_yhteiset_tutkinnon_osat"
+     :column "hoks_id"}))
+(def select-oo-ids-by-ahyto-id
+  (generate-select-id-by
+    {:id "osaamisen_osoittaminen_id"
+     :table "aiemmin_hankitun_yhteisen_tutkinnon_osan_naytto"
+     :column "aiemmin_hankittu_yhteinen_tutkinnon_osa_id"}))
+(def select-ahyto-osa-alue-ids-by-ahyto-id
+  (generate-select-id-by
+    {:id "id"
+     :table "aiemmin_hankitut_yto_osa_alueet"
+     :column "aiemmin_hankittu_yhteinen_tutkinnon_osa_id"}))
+(def select-oo-ids-by-ahyto-osa-alue-id
+  (generate-select-id-by
+    {:id "osaamisen_osoittaminen_id"
+     :table "aiemmin_hankitun_yto_osa_alueen_naytto"
+     :column "aiemmin_hankittu_yto_osa_alue_id"}))
+(def select-ahato-ids-by-hoks-id
+  (generate-select-id-by
+    {:id "id"
+     :table "aiemmin_hankitut_ammat_tutkinnon_osat"
+     :column "hoks_id"}))
+(def select-oo-ids-by-ahato-id
+  (generate-select-id-by
+    {:id "osaamisen_osoittaminen_id"
+     :table "aiemmin_hankitun_ammat_tutkinnon_osan_naytto"
+     :column "aiemmin_hankittu_ammat_tutkinnon_osa_id"}))
+(def select-ahpto-ids-by-hoks-id
+  (generate-select-id-by
+    {:id "id"
+     :table "aiemmin_hankitut_paikalliset_tutkinnon_osat"
+     :column "hoks_id"}))
+(def select-oo-ids-by-ahpto-id
+  (generate-select-id-by
+    {:id "osaamisen_osoittaminen_id"
+     :table "aiemmin_hankitun_paikallisen_tutkinnon_osan_naytto"
+     :column "aiemmin_hankittu_paikallinen_tutkinnon_osa_id"}))
+(def select-hato-ids-by-hoks-id
+  (generate-select-id-by
+    {:id "id"
+     :table "hankittavat_ammat_tutkinnon_osat"
+     :column "hoks_id"}))
+(def select-oo-ids-by-hato-id
+  (generate-select-id-by
+    {:id "osaamisen_osoittaminen_id"
+     :table "hankittavan_ammat_tutkinnon_osan_naytto"
+     :column "hankittava_ammat_tutkinnon_osa_id"}))
+(def select-hpto-ids-by-hoks-id
+  (generate-select-id-by
+    {:id "id"
+     :table "hankittavat_paikalliset_tutkinnon_osat"
+     :column "hoks_id"}))
+(def select-oo-ids-by-hpto-id
+  (generate-select-id-by
+    {:id "osaamisen_osoittaminen_id"
+     :table "hankittavan_paikallisen_tutkinnon_osan_naytto"
+     :column "hankittava_paikallinen_tutkinnon_osa_id"}))
+(def select-hyto-ids-by-hoks-id
+  (generate-select-id-by
+    {:id "id"
+     :table "hankittavat_yhteiset_tutkinnon_osat"
+     :column "hoks_id"}))
+(def select-hyto-osa-alue-ids-by-hyto-id
+  (generate-select-id-by
+    {:id "id"
+     :table "yhteisen_tutkinnon_osan_osa_alueet"
+     :column "yhteinen_tutkinnon_osa_id"}))
+(def select-oo-ids-by-hyto-osa-alue-id
+  (generate-select-id-by
+    {:id "osaamisen_osoittaminen_id"
+     :table "yhteisen_tutkinnon_osan_osa_alueen_naytot"
+     :column "yhteisen_tutkinnon_osan_osa_alue_id"}))
 (defq select-oppilaitos-oids
       "oppijat/select_oppilaitos_oids.sql")
 (defq select-oppilaitos-oids-by-koulutustoimija-oid
