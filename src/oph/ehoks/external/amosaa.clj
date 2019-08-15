c5f0706fd5274b3884bcf7602d0e4bfceae1d565
Matti Ahinko <matti.ahinko@gmail.com>
2019-08-15 15:32:45 +0300

    Remove getting data

--- src/oph/ehoks/external/amosaa.clj
+++ src/oph/ehoks/external/amosaa.clj
@@ -3,11 +3,11 @@
             [oph.ehoks.external.oph-url :as u]))
 
 (defn get-tutkinnon-osa-by-koodi [^String koodi]
-  (get-in
+  (get
     (cache/with-cache!
       {:method :get
        :service (u/get-url "amosaa-service-url")
        :url (u/get-url "amosaa-service.get-tutkinnon-osa-by-koodi"
                        (format "paikallinen_tutkinnonosa_%s" koodi))
        :options {:as :json}})
-    [:body :data]))
+    :body))
