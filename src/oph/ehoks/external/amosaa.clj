fef4ce154b1291b0062ad4b915ae75533fade62f
Matti Ahinko <matti.ahinko@gmail.com>
2019-08-15 15:22:31 +0300

    Use data

--- src/oph/ehoks/external/amosaa.clj
+++ src/oph/ehoks/external/amosaa.clj
@@ -3,11 +3,11 @@
             [oph.ehoks.external.oph-url :as u]))
 
 (defn get-tutkinnon-osa-by-koodi [^String koodi]
-  (get
+  (get-in
     (cache/with-cache!
       {:method :get
        :service (u/get-url "amosaa-service-url")
        :url (u/get-url "amosaa-service.get-tutkinnon-osa-by-koodi"
                        (format "paikallinen_tutkinnonosa_%s" koodi))
        :options {:as :json}})
-    :body))
+    [:body :data]))
