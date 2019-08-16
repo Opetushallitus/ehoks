6154ee0a79814db5087e81b626f642074a01fc61
Matti Ahinko <matti.ahinko@gmail.com>
2019-08-16 14:27:54 +0300

    Use authentication with oid get

--- test/oph/ehoks/oppija/auth_handler_test.clj
+++ test/oph/ehoks/oppija/auth_handler_test.clj
@@ -3,17 +3,50 @@
             [oph.ehoks.oppija.handler :as handler]
             [oph.ehoks.common.api :as common-api]
             [ring.mock.request :as mock]
-            [oph.ehoks.utils :refer [parse-body]]))
+            [oph.ehoks.utils :refer [parse-body]]
+            [oph.ehoks.external.http-client :as client]))
 
-(def base-url "/ehoks-oppija-backend/api/v1/oppija/session/")
+(def base-url "/ehoks-oppija-backend/api/v1/oppija/session")
 
 (defn authenticate [app]
-  (app (-> (mock/request :get (str base-url "opintopolku/"))
-           (mock/header "FirstName" "Teuvo Testi")
-           (mock/header "cn" "Teuvo")
-           (mock/header "givenname" "Teuvo")
-           (mock/header "hetu" "190384-9245")
-           (mock/header "sn" "Testaaja"))))
+  (client/with-mock-responses
+    [(fn [url options]
+       (cond
+         (.endsWith url "/serviceValidate")
+         {:status 200
+          :body
+          (str "<cas:serviceResponse"
+               "  xmlns:cas='http://www.yale.edu/tp/cas'>"
+               "<cas:authenticationSuccess><cas:user>ehoks</cas:user>"
+               "<cas:attributes>"
+               "<cas:longTermAuthenticationRequestTokenUsed>false"
+               "</cas:longTermAuthenticationRequestTokenUsed>"
+               "<cas:isFromNewLogin>false</cas:isFromNewLogin>"
+               "<cas:authenticationDate>2019-02-20T10:14:24.046+02:00"
+               "</cas:authenticationDate></cas:attributes>"
+               "</cas:authenticationSuccess></cas:serviceResponse>")}
+         (.contains url "oppijanumerorekisteri-service")
+         {:status 200
+          :body {:results
+                 [{:oidHenkilo "1.2.246.562.24.44651722625"
+                   :hetu "250103-5360"
+                   :etunimet "Aarto Maurits"
+                   :kutsumanimi "Aarto"
+                   :sukunimi "Väisänen-perftest"}]}}))
+     (fn [url options]
+       (cond
+         (.endsWith url "/v1/tickets")
+         {:status 201
+          :headers {"location" "http://test.ticket/1234"}}
+         (= url "http://test.ticket/1234")
+         {:status 200
+          :body "ST-1234-testi"}))]
+    (app (-> (mock/request :get (str base-url "/opintopolku/"))
+             (mock/header "FirstName" "Teuvo Testi")
+             (mock/header "cn" "Teuvo")
+             (mock/header "givenname" "Teuvo")
+             (mock/header "hetu" "190384-9245")
+             (mock/header "sn" "Testaaja")))))
 
 (deftest session-without-authentication
   (testing "GET current session without authentication"
@@ -35,7 +68,7 @@
     (let [app (common-api/create-app handler/app-routes nil)
           response
           (app (-> (mock/request
-                     :get (str base-url "opintopolku/")
+                     :get (str base-url "/opintopolku/")
                      {"FirstName" "Teuvo Testi"
                       "cn" "Teuvo"
                       "hetu" "190384-9245"
@@ -57,7 +90,8 @@
                             (mock/header :cookie session-cookie)))
           body (parse-body (:body response))]
       (is (= (:status response) 200))
-      (is (= (:data body) [{:first-name "Teuvo Testi"
+      (is (= (:data body) [{:oid "1.2.246.562.24.44651722625"
+                            :first-name "Teuvo Testi"
                             :common-name "Teuvo"
                             :surname "Testaaja"}])))))
 
@@ -83,7 +117,8 @@
                             (mock/header :cookie session-cookie)))]
       (is (= (:status authenticated-response) 200))
       (is (= (:data authenticated-body)
-             [{:first-name "Teuvo Testi"
+             [{:oid "1.2.246.562.24.44651722625"
+               :first-name "Teuvo Testi"
                :common-name "Teuvo"
                :surname "Testaaja"}]))
       (is (= (:status response) 401))
