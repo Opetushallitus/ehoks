00400ed6ce1a9dd6059520acbbbb716db46a3733
Matti Ahinko <matti.ahinko@gmail.com>
2019-08-21 12:48:36 +0300

    Improve auth utils

--- test/oph/ehoks/utils.clj
+++ test/oph/ehoks/utils.clj
@@ -8,26 +8,71 @@
             [oph.ehoks.external.cache :as cache]
             [oph.ehoks.db.migrations :as m]))
 
-(defn get-auth-cookie [app]
-  (-> (mock/request
-        :get "/ehoks-oppija-backend/api/v1/oppija/session/opintopolku/")
-      (mock/header "FirstName" "Teuvo Testi")
-      (mock/header "cn" "Teuvo")
-      (mock/header "givenname" "Teuvo")
-      (mock/header "hetu" "190384-9245")
-      (mock/header "sn" "Testaaja")
-      (app)
-      (get-in [:headers "Set-Cookie"])
-      (first)))
+(def base-url "/ehoks-oppija-backend/api/v1/oppija/session")
+
+(defn authenticate
+  ([app oppija-oid]
+    (client/with-mock-responses
+      [(fn [url options]
+         (cond
+           (.endsWith url "/serviceValidate")
+           {:status 200
+            :body
+            (str "<cas:serviceResponse"
+                 "  xmlns:cas='http://www.yale.edu/tp/cas'>"
+                 "<cas:authenticationSuccess><cas:user>ehoks</cas:user>"
+                 "<cas:attributes>"
+                 "<cas:longTermAuthenticationRequestTokenUsed>false"
+                 "</cas:longTermAuthenticationRequestTokenUsed>"
+                 "<cas:isFromNewLogin>false</cas:isFromNewLogin>"
+                 "<cas:authenticationDate>2019-02-20T10:14:24.046+02:00"
+                 "</cas:authenticationDate></cas:attributes>"
+                 "</cas:authenticationSuccess></cas:serviceResponse>")}
+           (.contains url "oppijanumerorekisteri-service")
+           {:status 200
+            :body {:results
+                   [{:oidHenkilo (or oppija-oid "1.2.246.562.24.44651722625")
+                     :hetu "250103-5360"
+                     :etunimet "Aarto Maurits"
+                     :kutsumanimi "Aarto"
+                     :sukunimi "Väisänen-perftest"}]}}))
+       (fn [url options]
+         (cond
+           (.endsWith url "/v1/tickets")
+           {:status 201
+            :headers {"location" "http://test.ticket/1234"}}
+           (= url "http://test.ticket/1234")
+           {:status 200
+            :body "ST-1234-testi"}))]
+      (app (-> (mock/request :get (str base-url "/opintopolku/"))
+               (mock/header "FirstName" "Teuvo Testi")
+               (mock/header "cn" "Teuvo")
+               (mock/header "givenname" "Teuvo")
+               (mock/header "hetu" "190384-9245")
+               (mock/header "sn" "Testaaja")))))
+  ([app] (authenticate app nil)))
+
+(defn get-auth-cookie
+  ([app oppija-oid]
+    (-> (authenticate app oppija-oid)
+        (get-in [:headers "Set-Cookie"])
+        (first)))
+  ([app] (get-auth-cookie app nil)))
 
 (defn with-authentication [app request]
   (let [cookie (get-auth-cookie app)]
     (app (mock/header request :cookie cookie))))
 
-(defn with-authenticated-oid [store oid app request]
-  (let [cookie (get-auth-cookie app)]
+(defn with-authenticated-oid [store oid app & requests]
+  (let [cookie (get-auth-cookie app oid)]
     (swap! store assoc-in [(-> @store keys first) :user :oid] oid)
-    (app (mock/header request :cookie cookie))))
+    (let [responses (mapv
+                      (fn [request]
+                        (app (mock/header request :cookie cookie)))
+                      requests)]
+      (if (= (count responses) 1)
+        (first responses)
+        responses))))
 
 (defn set-auth-functions! [organisaatio-oid unmatched-fn]
   (client/set-post!
