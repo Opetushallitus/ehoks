5c93e7e5282a785171fc663c9e919bb552ee2251
Matti Ahinko <matti.ahinko@gmail.com>
2019-08-21 13:53:54 +0300

    Add delete route for share

--- src/oph/ehoks/oppija/share_handler.clj
+++ src/oph/ehoks/oppija/share_handler.clj
@@ -3,7 +3,8 @@
             [oph.ehoks.restful :as rest]
             [oph.ehoks.oppija.schema :as oppija-schema]
             [oph.ehoks.schema :as schema]
-            [oph.ehoks.db.db-operations.oppija :as db]))
+            [oph.ehoks.db.db-operations.oppija :as db]
+            [ring.util.http-response :as response]))
 
 (def routes
   (c-api/context "/share" []
@@ -11,12 +12,20 @@
 
     (c-api/context "/:koodi-uri" []
       :path-params [koodi-uri :- String]
+
       (c-api/GET "/" request
         :return (rest/response [oppija-schema/Jakolinkki])
         :summary "Tutkinnon osan jakolinkkien listaus"
         (rest/rest-ok
           (db/select-hoks-tutkinnon-osa-shares
             (get-in request [:hoks :id]) koodi-uri)))
+
+      (c-api/DELETE "/:uuid" request
+        :path-params [uuid :- String]
+
+        (db/delete-tutkinnon-osa-share!
+          uuid (get-in request [:hoks :id]))
+        (response/ok))
 
       (c-api/POST "/" request
         :body [values oppija-schema/JakolinkkiLuonti]
