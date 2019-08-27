986080984ddf6d65ea6902f3e3963f9657eea236
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-08-27 14:29:05 +0300

    eh-314 shortened names for sql files

--- resources/db/hoksit/delete_nayttoymparisto_by_osaamisen_osoittaminen_by_tutkinnon_osa_by_hoks.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM nayttoymparistot WHERE id IN
-(SELECT n.osaamisen_osoittaminen_id
- FROM :tutkinnon-osa-naytto-table AS n
-  LEFT OUTER JOIN :tutkinnon-osa-table AS t
-  ON
-  (n.:tutkinnon-osa-id = t.id AND t.hoks_id = ?))
-   RETURNING id
