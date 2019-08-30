f8c048ac555f41f163d821ae55a9a9fdd888d05b
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-08-30 11:13:06 +0300

    eh-314 corrected delete_oo_by_tutkiinon_osa query sql statement

--- resources/db/hoksit/delete_oo_by_tutkinnonosa_by_hoks_id.sql
+++ resources/db/hoksit/delete_oo_by_tutkinnonosa_by_hoks_id.sql
@@ -1,5 +1,6 @@
 DELETE FROM osaamisen_osoittamiset WHERE id IN (
-SELECT osaamisen_osoittaminen_id FROM :tutkinnon-osa-naytto-table as n
-WHERE n.:tutkinnon-osa-id IN
-(SELECT id FROM :tutkinnon-osa-table WHERE hoks_id=?))
+SELECT n.osaamisen_osoittaminen_id
+FROM :tutkinnon-osa-naytto-table AS n
+INNER JOIN :tutkinnon-osa-table AS h
+ON (n.:tutkinnon-osa-id = h.id  AND h.hoks_id=?))
 RETURNING id
