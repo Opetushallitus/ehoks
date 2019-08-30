596a2e0f5b62600237e24df097be02e70fbde951
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-08-30 12:57:08 +0300

    eh-314 rewrote query for delete kj oo yto

--- resources/db/hoksit/delete_kj_osaamisen_arvioijat_yto_osa_alueet.sql
+++ resources/db/hoksit/delete_kj_osaamisen_arvioijat_yto_osa_alueet.sql
@@ -1,13 +1,10 @@
 DELETE FROM koulutuksen_jarjestaja_osaamisen_arvioijat WHERE id IN
-(SELECT koulutuksen_jarjestaja_osaamisen_arvioija_id
-  FROM osaamisen_osoittamisen_koulutuksen_jarjestaja_arvioija WHERE osaamisen_osoittaminen_id
-  IN
-  (SELECT y.osaamisen_osoittaminen_id
-    FROM :yto-osa-alue-naytto-table AS y
-    LEFT OUTER JOIN :yto-osa-alueet-table AS o
-    ON
-    (y.:yto-osa-alue-id = o.id
-      AND o.:tutkinnon-osa-id IN
-    (SELECT id
-     FROM :tutkinnon-osa-table AS t WHERE t.hoks_id = ?))))
-   RETURNING id
+(SELECT o.koulutuksen_jarjestaja_osaamisen_arvioija_id
+ FROM osaamisen_osoittamisen_koulutuksen_jarjestaja_arvioija AS o
+ INNER JOIN :yto-osa-alue-naytto-table as n
+ ON (n.osaamisen_osoittaminen_id = o.osaamisen_osoittaminen_id)
+ INNER JOIN :yto-osa-alueet-table as y
+ ON (n.:yto-osa-alue-id = y.id)
+ INNER JOIN :tutkinnon-osa-table as t
+ ON (t.id = y.:tutkinnon-osa-id AND t.hoks_id=?))
+ RETURNING id
