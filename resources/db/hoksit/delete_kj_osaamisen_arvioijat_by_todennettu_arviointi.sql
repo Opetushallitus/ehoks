c7520a6d8fb9d36b672699afb8c5be5d56429b66
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-09-02 11:03:19 +0300

    eh-314 corrected sql query of deleting kj osaamisen arvioijat by todennettu arviointi

--- resources/db/hoksit/delete_kj_osaamisen_arvioijat_by_todennettu_arviointi.sql
+++ resources/db/hoksit/delete_kj_osaamisen_arvioijat_by_todennettu_arviointi.sql
@@ -1,9 +1,8 @@
-DELETE FROM koulutuksen_jarjestaja_osaamisen_arvioijat WHERE id IN
-(SELECT koulutuksen_jarjestaja_osaamisen_arvioija_id FROM todennettu_arviointi_arvioijat as t WHERE todennettu_arviointi_lisatiedot_id
-  IN
-(SELECT n.id
-  FROM todennettu_arviointi_lisatiedot AS n
-  LEFT OUTER JOIN :tutkinnon-osa-table AS t
-  ON
-  (t.hoks_id = ? AND t.tarkentavat_tiedot_osaamisen_arvioija_id = n.id)))
+  DELETE FROM koulutuksen_jarjestaja_osaamisen_arvioijat WHERE id  IN
+  (SELECT t.koulutuksen_jarjestaja_osaamisen_arvioija_id FROM todennettu_arviointi_arvioijat
+  AS t
+  INNER JOIN todennettu_arviointi_lisatiedot AS n
+  ON (n.id = t.todennettu_arviointi_lisatiedot_id)
+  INNER JOIN :tutkinnon-osa-table AS o
+  ON (o.hoks_id = ? AND o.tarkentavat_tiedot_osaamisen_arvioija_id = n.id))
   RETURNING id
