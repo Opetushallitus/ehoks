682a9de5ab4cb809ac471cf2459abe9a14df49b8
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-09-02 15:30:28 +0300

    eh-314 corrected sql query for deleting osaamisenhankkimistavat by tutkinnon osa

--- resources/db/hoksit/delete_osaamisen_hankkimistavat_by_tutkinnon_osa_by_hoks.sql
+++ resources/db/hoksit/delete_osaamisen_hankkimistavat_by_tutkinnon_osa_by_hoks.sql
@@ -1,7 +1,6 @@
 DELETE FROM osaamisen_hankkimistavat WHERE id IN
-(SELECT osaamisen_hankkimistapa_id
-  FROM :tutkinnon-osa-hankkimistapa-table as h
-  LEFT OUTER JOIN :tutkinnon-osa-table AS t
-  ON
-  (h.:tutkinnon-osa-id = t.id AND t.hoks_id = ?))
+  (SELECT h.osaamisen_hankkimistapa_id FROM
+    :tutkinnon-osa-hankkimistapa-table as h
+  INNER JOIN :tutkinnon-osa-table as t
+  ON (t.id = h.:tutkinnon-osa-id AND t.hoks_id = ?))
   RETURNING id
