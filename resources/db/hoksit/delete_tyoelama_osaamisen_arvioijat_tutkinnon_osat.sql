03da406af39861a36fed264b03580c454f0394fa
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-09-02 11:18:34 +0300

    eh-314 corrected sql query for deleting tyoelama-osaamisen-arvioija by tutkinnon-osa

--- resources/db/hoksit/delete_tyoelama_osaamisen_arvioijat_tutkinnon_osat.sql
+++ resources/db/hoksit/delete_tyoelama_osaamisen_arvioijat_tutkinnon_osat.sql
@@ -1,11 +1,7 @@
-DELETE FROM tyoelama_osaamisen_arvioijat WHERE id IN
-(SELECT tyoelama_arvioija_id
-  FROM osaamisen_osoittamisen_tyoelama_arvioija WHERE id
-  IN
-  (SELECT id FROM osaamisen_osoittamiset WHERE id IN (
-   SELECT n.osaamisen_osoittaminen_id
-   FROM :tutkinnon-osa-naytto-table AS n
-     LEFT OUTER JOIN :tutkinnon-osa-table AS t
-     ON
-     (n.:tutkinnon-osa-id = t.id AND t.hoks_id = ?))))
-     RETURNING id
+DELETE FROM tyoelama_osaamisen_arvioijat WHERE id IN (
+SELECT o.tyoelama_arvioija_id FROM osaamisen_osoittamisen_tyoelama_arvioija AS o
+INNER JOIN :tutkinnon-osa-naytto-table as t
+ON (t.osaamisen_osoittaminen_id = o.osaamisen_osoittaminen_id)
+INNER JOIN :tutkinnon-osa-table as y
+ON (y.hoks_id = ? AND t.:tutkinnon-osa-id = y.id))
+RETURNING id
