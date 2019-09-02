a4775bbe1ec541417a694e57afb819c59b6617fb
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-09-02 11:34:04 +0300

    eh-314 corrected sql query for tyoelama osaamisen arvioijat by yto osa-alue

--- resources/db/hoksit/delete_tyoelama_osaamisen_arvioijat_yto_osa_alueet.sql
+++ resources/db/hoksit/delete_tyoelama_osaamisen_arvioijat_yto_osa_alueet.sql
@@ -1,13 +1,9 @@
-DELETE FROM tyoelama_osaamisen_arvioijat WHERE id IN
-(SELECT tyoelama_arvioija_id
-  FROM osaamisen_osoittamisen_tyoelama_arvioija WHERE id
-  IN
-  (SELECT y.osaamisen_osoittaminen_id
-    FROM :yto-osa-alue-naytto-table AS y
-    LEFT OUTER JOIN :yto-osa-alueet-table AS o
-    ON
-    (y.:yto-osa-alue-id = o.id
-      AND o.:tutkinnon-osa-id IN
-    (SELECT id
-     FROM :tutkinnon-osa-table AS t WHERE t.hoks_id = ?))))
-   RETURNING id
+DELETE FROM tyoelama_osaamisen_arvioijat WHERE id IN (
+SELECT o.tyoelama_arvioija_id FROM osaamisen_osoittamisen_tyoelama_arvioija AS o
+INNER JOIN :yto-osa-alue-naytto-table as n
+ON (n.osaamisen_osoittaminen_id = o.osaamisen_osoittaminen_id)
+INNER JOIN :yto-osa-alueet-table as a
+ON (n.:yto-osa-alue-id = a.id)
+INNER JOIN :tutkinnon-osa-table as y
+ON (a.:tutkinnon-osa-id = y.id AND y.hoks_id = ?))
+RETURNING id
