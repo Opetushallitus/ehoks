aa5bd3c1c7a4a9aafe9fe90e35056ac26868594a
Matti Ahinko <matti.ahinko@gmail.com>
2019-09-10 14:58:26 +0300

    Add macro for handling not found exception

--- src/oph/ehoks/restful.clj
+++ src/oph/ehoks/restful.clj
@@ -1,5 +1,5 @@
 (ns oph.ehoks.restful
-  (:require [ring.util.http-response :refer [ok]]))
+  (:require [ring.util.http-response :refer [ok not-found]]))
 
 (defn response [body & meta-data]
   (let [meta-map (apply hash-map meta-data)]
@@ -8,3 +8,11 @@
 
 (defn rest-ok [body & meta-data]
   (ok (apply response body meta-data)))
+
+(defmacro with-not-found-handling [& body]
+  `(try
+     (rest-ok (do ~@body))
+     (catch Exception e#
+       (if (= (:status (ex-data e#)) 404)
+         (not-found)
+         (throw e#)))))
