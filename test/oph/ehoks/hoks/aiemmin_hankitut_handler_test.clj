d6f44bc742e59c9b7da548194065000bd8ccb1e8
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2019-09-12 08:26:23 +0300

    Moved two tests to another namespace

--- test/oph/ehoks/hoks/aiemmin_hankitut_handler_test.clj
+++ test/oph/ehoks/hoks/aiemmin_hankitut_handler_test.clj
@@ -1,82 +1,21 @@
 (ns oph.ehoks.hoks.aiemmin-hankitut-handler-test
   (:require [clojure.test :refer [deftest testing is use-fixtures]]
             [oph.ehoks.utils :as utils]
-            [oph.ehoks.hoks.hoks-test-utils :as hoks-utils]))
+            [oph.ehoks.hoks.hoks-test-utils :as hoks-utils]
+            [oph.ehoks.hoks.test-data :as test-data]))
 
 (use-fixtures :each utils/with-database)
 
-(def ahyto-of-hoks-updated
-  {:id 1
-   :ensikertainen-hyvaksyminen "2018-12-15"
-   :aiemmin-hankitut-yhteiset-tutkinnon-osat
-   [{:valittu-todentamisen-prosessi-koodi-uri
-     "osaamisentodentamisenprosessi_0002"
-     :valittu-todentamisen-prosessi-koodi-versio 4
-     :tutkinnon-osa-koodi-versio 2
-     :tutkinnon-osa-koodi-uri "tutkinnonosat_10203"
-     :koulutuksen-jarjestaja-oid "1.2.246.562.10.13490590921"
-     :tarkentavat-tiedot-osaamisen-arvioija
-     {:lahetetty-arvioitavaksi "2017-03-29"
-      :aiemmin-hankitun-osaamisen-arvioijat
-      [{:nimi "Arttu Arvioi"
-        :organisaatio {:oppilaitos-oid
-                       "1.2.246.562.10.54453931312"}}]}
-     :osa-alueet
-     [{:osa-alue-koodi-uri "ammatillisenoppiaineet_ru"
-       :osa-alue-koodi-versio 4
-       :koulutuksen-jarjestaja-oid
-       "1.2.246.562.10.54453923577"
-       :vaatimuksista-tai-tavoitteista-poikkeaminen
-       "Testaus ei kuulu."
-       :valittu-todentamisen-prosessi-koodi-uri
-       "osaamisentodentamisenprosessi_0004"
-       :valittu-todentamisen-prosessi-koodi-versio 4
-       :tarkentavat-tiedot-naytto
-       [{:sisallon-kuvaus ["kuvaus1" "kuvaus2"]
-         :osa-alueet [{:koodi-uri "ammatillisenoppiaineet_ai"
-                       :koodi-versio 5}]
-         :koulutuksen-jarjestaja-osaamisen-arvioijat
-         [{:nimi "Teppo Test"
-           :organisaatio {:oppilaitos-oid
-                          "1.2.246.562.10.54539267911"}}]
-         :jarjestaja {:oppilaitos-oid
-                      "1.2.246.562.10.55890967911"}
-         :nayttoymparisto {:nimi "Ab Yhtiö"
-                           :y-tunnus "1234128-2"
-                           :kuvaus "Testi1"}
-         :tyoelama-osaamisen-arvioijat
-         [{:nimi "Tellervo Tekijä"
-           :organisaatio {:nimi "Ab Yhtiö"
-                          :y-tunnus "1234128-1"}}]
-         :yksilolliset-kriteerit ["Joku kriteeri" "Toinen"]
-         :alku "2019-01-04"
-         :loppu "2021-03-01"}]}]
-     :tarkentavat-tiedot-naytto
-     [{:osa-alueet [{:koodi-uri "ammatillisenoppiaineet_ru"
-                     :koodi-versio 7}]
-       :koulutuksen-jarjestaja-osaamisen-arvioijat
-       [{:nimi "Erkki Esimerkkitest"
-         :organisaatio {:oppilaitos-oid
-                        "1.2.246.562.10.13490579091"}}]
-       :jarjestaja {:oppilaitos-oid
-                    "1.2.246.562.10.93270579093"}
-       :nayttoymparisto {:nimi "Testi"
-                         :y-tunnus "1289235-3"
-                         :kuvaus "Testiyht"}
-       :tyoelama-osaamisen-arvioijat
-       [{:nimi "Tapio Testi"
-         :organisaatio {:nimi "Testi Oyj"
-                        :y-tunnus "1289235-3"}}]
-       :sisallon-kuvaus
-       ["Testauksen suunnittelu" "Jokin toinen testi" "kolmas"]
-       :yksilolliset-kriteerit ["Ensimmäinen kriteeri"]
-       :alku "2019-03-01"
-       :loppu "2019-06-01"}]}]})
+(deftest put-ahyto-of-hoks
+  (testing "PUTs aiemmin hankitut yhteiset tutkinnon osat of HOKS"
+    (hoks-utils/assert-partial-put-of-hoks
+      test-data/ahyto-of-hoks-updated
+      :aiemmin-hankitut-yhteiset-tutkinnon-osat
+      test-data/hoks-data)))
 
-;TODO siirrä testidata omaan tiedostoon jollon tämä voidaan ottaa käyttöön
-;(deftest put-ahyto-of-hoks
-;  (testing "PUTs aiemmin hankitut yhteiset tutkinnon osat of HOKS"
-;    (hoks-utils/assert-partial-put-of-hoks
-;      ahyto-of-hoks-updated
-;      :aiemmin-hankitut-yhteiset-tutkinnon-osat
-;      hoks-data)))
+(deftest put-ahpto-of-hoks
+  (testing "PUTs aiemmin hankitut paikalliset tutkinnon osat of HOKS"
+    (hoks-utils/assert-partial-put-of-hoks
+      test-data/ahpto-of-hoks-updated
+      :aiemmin-hankitut-paikalliset-tutkinnon-osat
+      test-data/hoks-data)))
--- test/oph/ehoks/hoks/test_data.clj
+++ test/oph/ehoks/hoks/test_data.clj
@@ -253,3 +253,112 @@
    :aiemmin-hankitut-ammat-tutkinnon-osat [ahato-data]
    :aiemmin-hankitut-paikalliset-tutkinnon-osat [ahpto-data]
    :aiemmin-hankitut-yhteiset-tutkinnon-osat [ahyto-data]})
+
+(def ahyto-of-hoks-updated
+  {:id 1
+   :ensikertainen-hyvaksyminen "2018-12-15"
+   :aiemmin-hankitut-yhteiset-tutkinnon-osat
+   [{:valittu-todentamisen-prosessi-koodi-uri
+     "osaamisentodentamisenprosessi_0002"
+     :valittu-todentamisen-prosessi-koodi-versio 4
+     :tutkinnon-osa-koodi-versio 2
+     :tutkinnon-osa-koodi-uri "tutkinnonosat_10203"
+     :koulutuksen-jarjestaja-oid "1.2.246.562.10.13490590921"
+     :tarkentavat-tiedot-osaamisen-arvioija
+     {:lahetetty-arvioitavaksi "2017-03-29"
+      :aiemmin-hankitun-osaamisen-arvioijat
+      [{:nimi "Arttu Arvioi"
+        :organisaatio {:oppilaitos-oid
+                       "1.2.246.562.10.54453931312"}}]}
+     :osa-alueet
+     [{:osa-alue-koodi-uri "ammatillisenoppiaineet_ru"
+       :osa-alue-koodi-versio 4
+       :koulutuksen-jarjestaja-oid
+       "1.2.246.562.10.54453923577"
+       :vaatimuksista-tai-tavoitteista-poikkeaminen
+       "Testaus ei kuulu."
+       :valittu-todentamisen-prosessi-koodi-uri
+       "osaamisentodentamisenprosessi_0004"
+       :valittu-todentamisen-prosessi-koodi-versio 4
+       :tarkentavat-tiedot-naytto
+       [{:sisallon-kuvaus ["kuvaus1" "kuvaus2"]
+         :osa-alueet [{:koodi-uri "ammatillisenoppiaineet_ai"
+                       :koodi-versio 5}]
+         :koulutuksen-jarjestaja-osaamisen-arvioijat
+         [{:nimi "Teppo Test"
+           :organisaatio {:oppilaitos-oid
+                          "1.2.246.562.10.54539267911"}}]
+         :jarjestaja {:oppilaitos-oid
+                      "1.2.246.562.10.55890967911"}
+         :nayttoymparisto {:nimi "Ab Yhtiö"
+                           :y-tunnus "1234128-2"
+                           :kuvaus "Testi1"}
+         :tyoelama-osaamisen-arvioijat
+         [{:nimi "Tellervo Tekijä"
+           :organisaatio {:nimi "Ab Yhtiö"
+                          :y-tunnus "1234128-1"}}]
+         :yksilolliset-kriteerit ["Joku kriteeri" "Toinen"]
+         :alku "2019-01-04"
+         :loppu "2021-03-01"}]}]
+     :tarkentavat-tiedot-naytto
+     [{:osa-alueet [{:koodi-uri "ammatillisenoppiaineet_ru"
+                     :koodi-versio 7}]
+       :koulutuksen-jarjestaja-osaamisen-arvioijat
+       [{:nimi "Erkki Esimerkkitest"
+         :organisaatio {:oppilaitos-oid
+                        "1.2.246.562.10.13490579091"}}]
+       :jarjestaja {:oppilaitos-oid
+                    "1.2.246.562.10.93270579093"}
+       :nayttoymparisto {:nimi "Testi"
+                         :y-tunnus "1289235-3"
+                         :kuvaus "Testiyht"}
+       :tyoelama-osaamisen-arvioijat
+       [{:nimi "Tapio Testi"
+         :organisaatio {:nimi "Testi Oyj"
+                        :y-tunnus "1289235-3"}}]
+       :sisallon-kuvaus
+       ["Testauksen suunnittelu" "Jokin toinen testi" "kolmas"]
+       :yksilolliset-kriteerit ["Ensimmäinen kriteeri"]
+       :alku "2019-03-01"
+       :loppu "2019-06-01"}]}]})
+
+(def ahpto-of-hoks-updated
+  {:id 1
+   :ensikertainen-hyvaksyminen "2018-12-15"
+   :aiemmin-hankitut-paikalliset-tutkinnon-osat
+   [{:valittu-todentamisen-prosessi-koodi-versio 3
+     :laajuus 40
+     :nimi "Testiopintojaksoo"
+     :tavoitteet-ja-sisallot "Tavoitteena on oppiminen."
+     :valittu-todentamisen-prosessi-koodi-uri
+     "osaamisentodentamisenprosessi_0003"
+     :amosaa-tunniste "12345"
+     :koulutuksen-jarjestaja-oid "1.2.246.562.10.54453945325"
+     :vaatimuksista-tai-tavoitteista-poikkeaminen "Ei poikkeamaa"
+     :tarkentavat-tiedot-osaamisen-arvioija
+     {:lahetetty-arvioitavaksi "2021-01-01"
+      :aiemmin-hankitun-osaamisen-arvioijat
+      [{:nimi "Aarne Arvioija toinen"
+        :organisaatio {:oppilaitos-oid
+                       "1.2.246.562.10.54453923421"}}]}
+     :tarkentavat-tiedot-naytto
+     [{:osa-alueet [{:koodi-uri "ammatillisenoppiaineet_bi"
+                     :koodi-versio 6}]
+       :koulutuksen-jarjestaja-osaamisen-arvioijat
+       [{:nimi "Teuvo Test"
+         :organisaatio {:oppilaitos-oid
+                        "1.2.246.562.10.12346234691"}}]
+       :jarjestaja {:oppilaitos-oid
+                    "1.2.246.562.10.93270534263"}
+       :nayttoymparisto {:nimi "Testi Oy"
+                         :y-tunnus "1289212-2"
+                         :kuvaus "Testiyhtiöö"}
+       :tyoelama-osaamisen-arvioijat
+       [{:nimi "Terttu Test"
+         :organisaatio {:nimi "Testi Oy"
+                        :y-tunnus "1289211-4"}}]
+       :sisallon-kuvaus ["Testauksen suunnittelu"
+                         "Jokin toinen testi"]
+       :alku "2018-02-01"
+       :loppu "2021-03-01"
+       :yksilolliset-kriteerit ["Ensimmäinen kriteeri" "Toinen"]}]}]})
