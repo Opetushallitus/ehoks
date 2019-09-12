059528e41e541109e8b1f18cc9fd9dec364e8338
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2019-09-12 10:48:58 +0300

    Moved oto tests

--- test/oph/ehoks/hoks/handler_test.clj
+++ test/oph/ehoks/hoks/handler_test.clj
@@ -10,61 +10,6 @@
 
 (use-fixtures :each utils/with-database)
 
-(def oto-path "opiskeluvalmiuksia-tukevat-opinnot")
-
-(deftest post-and-get-opiskeluvalmiuksia-tukevat-opinnot
-  (testing "GET opiskeluvalmiuksia tukevat opinnot"
-    (hoks-utils/with-hoks-and-app
-      [hoks app]
-      (let [post-response (hoks-utils/create-mock-post-request
-                            oto-path test-data/oto-data app hoks)
-            get-response (hoks-utils/create-mock-hoks-osa-get-request oto-path app hoks)]
-        (hoks-utils/assert-post-response-is-ok oto-path post-response)
-        (is (= (:status get-response) 200))
-        (eq (utils/parse-body
-              (:body get-response))
-            {:meta {} :data (assoc test-data/oto-data :id 1)})))))
-
-(def ^:private one-value-of-oto-patched
-  {:nimi "Muuttunut Nimi"})
-
-(deftest patch-one-value-of-opiskeluvalmiuksia-tukevat-opinnot
-  (testing "PATCH one value of opiskeluvalmiuksia tukevat opinnot"
-    (hoks-utils/with-hoks-and-app
-      [hoks app]
-      (hoks-utils/create-mock-post-request
-        oto-path test-data/oto-data app hoks)
-      (let [patch-response (hoks-utils/create-mock-hoks-osa-patch-request
-                             oto-path app one-value-of-oto-patched)
-            get-response (hoks-utils/create-mock-hoks-osa-get-request oto-path app hoks)
-            get-response-data (:data (utils/parse-body (:body get-response)))]
-        (is (= (:status patch-response) 204))
-        (is (= (:nimi get-response-data)
-               (:nimi one-value-of-oto-patched))
-            "Patched value should change.")
-        (is (= (:kuvaus get-response-data)
-               (:kuvaus test-data/oto-data))
-            "Value should stay unchanged")))))
-
-(def ^:private all-values-of-oto-patched
-  {:nimi "Muuttunut Nimi"
-   :kuvaus "Uusi Kuvaus"
-   :alku "2020-01-12"
-   :loppu "2021-10-20"})
-
-(deftest patch-multiple-values-of-oto
-  (testing "PATCH all opiskeluvalmiuksia tukevat opinnot"
-    (hoks-utils/with-hoks-and-app
-      [hoks app]
-      (hoks-utils/create-mock-post-request
-        oto-path test-data/oto-data app hoks)
-      (let [patch-response (hoks-utils/create-mock-hoks-osa-patch-request
-                             oto-path app all-values-of-oto-patched)
-            get-response (hoks-utils/create-mock-hoks-osa-get-request oto-path app hoks)
-            get-response-data (:data (utils/parse-body (:body get-response)))]
-        (is (= (:status patch-response) 204))
-        (eq get-response-data (assoc all-values-of-oto-patched :id 1))))))
-
 (defn add-empty-hoks-values [hoks]
   (assoc
     hoks
