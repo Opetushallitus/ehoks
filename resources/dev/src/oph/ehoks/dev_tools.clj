bee6c92498568aafbec77cbd712cd488803150b7
Matti Ahinko <matti.ahinko@gmail.com>
2019-09-26 13:29:39 +0300

    Add routes for handling session

--- resources/dev/src/oph/ehoks/dev_tools.clj
+++ resources/dev/src/oph/ehoks/dev_tools.clj
@@ -1,5 +1,9 @@
 (ns oph.ehoks.dev-tools
-  (:require [oph.ehoks.db.session-store :as store]))
+  (:require [oph.ehoks.db.session-store :as store]
+            [compojure.api.sweet :as c-api]
+            [compojure.api.core :refer [route-middleware]]
+            [ring.util.http-response :as response]
+            [schema.core :as s]))
 
 (defn get-current-session-key [request]
   (get-in request [:cookies "ring-session" :value]))
@@ -12,20 +16,14 @@
                 [:virkailija-user :organisation-privileges]
                 privileges))))
 
-(defn get-session-organisation-privileges [session-key]
-  (get-in (store/get-session session-key)
-          [:virkailija-user :organisation-privileges]))
-
-(defn add-session-privilege [session-key organisation-oid privileges]
+(defn add-session-privilege [session-key privileges]
   (let [session (store/get-session session-key)]
     (store/save-session!
       session-key
       (update-in session
                  [:virkailija-user :organisation-privileges]
                  conj
-                 {:child-organisations []
-                  :oid organisation-oid
-                  :privileges privileges}))))
+                 privileges))))
 
 (defn remove-session-organisation [session-key organisation-oid]
   (let [session (store/get-session session-key)]
@@ -38,3 +36,71 @@
           (fn [o]
             (= (:oid o) organisation-oid))
           (get-in session [:virkailija-user :organisation-privileges]))))))
+
+(defn wrap-session-key [handler]
+  (fn
+    ([request respond raise]
+      (if-let [session-key (get-current-session-key request)]
+        (handler (assoc request :session-key session-key) respond raise)
+        (respond (response/bad-request "Session key not in cookie"))))
+    ([request]
+      (if-let [session-key (get-current-session-key request)]
+        (handler (assoc request :session-key session-key))
+        (response/bad-request "Session key not in cookie")))))
+
+(defn wrap-require-session [handler]
+  (fn
+    ([request respond raise]
+      (if (some? (store/get-session (:session-key request)))
+        (handler request respond raise)
+        (respond (response/bad-request "Please login first"))))
+    ([request]
+      (if (some? (store/get-session (:session-key request)))
+        (handler request)
+        (response/bad-request "Please login first")))))
+
+(s/defschema OrganisationPrivilege
+             {:oid s/Str
+              :roles [s/Str]
+              :privileges [s/Str]
+              :child-organisations [s/Str]})
+
+(def routes
+  (c-api/api
+    {:swagger
+     {:ui "/dev-tools/doc"
+      :spec "/dev-tools/doc/swagger.json"
+      :data {:info {:title "Dev tools"
+                    :description "Dev tools"}
+             :tags [{:name "api", :description ""}]}}}
+
+    (c-api/context "/dev-tools" []
+      :tags ["dev"]
+
+      (route-middleware
+        [wrap-session-key wrap-require-session]
+
+        (c-api/context "/session" []
+          (c-api/GET "/" request
+            :summary "Get current session from db"
+            (response/ok (store/get-session (:session-key request))))
+
+          (c-api/PATCH "/" request
+            :summary "Update current session in db. Be aware. This is a powerful
+                     tool and might brake things easily."
+            :body [session s/Any]
+            (store/save-session! (:session-key request) session)
+            (response/no-content)))
+
+        (c-api/context "/organisation-privileges" []
+          (c-api/DELETE "/:organisation-oid" request
+            :path-params [organisation-oid :- s/Str]
+            :summary "Remove organisation from current user"
+            (remove-session-organisation (:session-key request) organisation-oid)
+            (response/no-content))
+
+          (c-api/POST "/" request
+            :summary "Add organisation privileges"
+            :body [privileges OrganisationPrivilege]
+            (add-session-privilege (:session-key request) privileges)
+            (response/no-content)))))))
