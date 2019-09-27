88c4dd9a6d2af95b244f62323a9386526a82ef43
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2019-09-27 13:23:00 +0300

    Set oppijat without tutkinto query to use tutkinto_nimi instead of tutkinto. Added test.

--- test/oph/ehoks/oppijaindex_test.clj
+++ test/oph/ehoks/oppijaindex_test.clj
@@ -54,6 +54,29 @@
       (= (sut/get-opiskeluoikeudet-without-index-count)
          3))))
 
+(t/deftest get-opiskeluoikeudet-without-tutkinto-nimi
+  (t/testing "Get opiskeluoikeudet without tutkinto_nimi"
+    (db-oppija/insert-oppija!
+      {:oid "1.2.246.562.24.11111111111"
+       :nimi "Testi Oppija"})
+    (db-opiskeluoikeus/insert-opiskeluoikeus!
+       {:oid "1.2.246.562.15.76000000002"
+        :oppija_oid "1.2.246.562.24.11111111111"
+        :tutkinto "Testitutkinto 1"})
+    (db-opiskeluoikeus/insert-opiskeluoikeus!
+      {:oid "1.2.246.562.15.76000000003"
+       :oppija_oid "1.2.246.562.24.11111111111"
+       :tutkinto "Testitutkinto 2"})
+    (db-opiskeluoikeus/insert-opiskeluoikeus!
+      {:oid "1.2.246.562.15.76000000004"
+       :oppija_oid "1.2.246.562.24.11111111111"
+       :tutkinto-nimi {:fi "Testitutkinto" :sv "test"}})
+    (let [results (vec (sort-by :oid (sut/get-opiskeluoikeudet-without-tutkinto)))]
+      (t/is (= (get-in results [0 :oid])
+               "1.2.246.562.15.76000000002"))
+      (t/is (= (get-in results [1 :oid])
+             "1.2.246.562.15.76000000003")))))
+
 (t/deftest get-oppija-opiskeluoikeudet
   (t/testing "Get oppija opiskeluoikeudet"
     (db-oppija/insert-oppija! {:oid "1.2.246.562.24.11111111111"})
