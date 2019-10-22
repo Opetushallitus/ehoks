da9077e3a8265c3082f846ee490d7e15e05227ae
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2019-10-22 16:21:34 +0300

    Moved oppija external routes to a new namespace.

--- src/oph/ehoks/oppija/handler.clj
+++ src/oph/ehoks/oppija/handler.clj
@@ -10,10 +10,7 @@
             [oph.ehoks.common.schema :as common-schema]
             [oph.ehoks.oppija.schema :as oppija-schema]
             [oph.ehoks.hoks.hoks :as h]
-            [oph.ehoks.external.koodisto :as koodisto]
             [oph.ehoks.external.koski :as koski]
-            [oph.ehoks.external.eperusteet :as eperusteet]
-            [oph.ehoks.external.amosaa :as amosaa]
             [oph.ehoks.middleware :refer [wrap-authorize]]
             [oph.ehoks.oppija.auth-handler :as auth-handler]
             [oph.ehoks.lokalisointi.handler :as lokalisointi-handler]
@@ -24,7 +21,7 @@
             [oph.ehoks.oppijaindex :as oppijaindex]
             [oph.ehoks.oppija.share-handler :as share-handler]
             [oph.ehoks.oppija.middleware :as m]
-            [oph.ehoks.external.organisaatio :as organisaatio]))
+            [oph.ehoks.oppija.oppija-external :as oppija-external]))
 
 (defn wrap-match-user [handler]
   (fn
@@ -63,65 +60,7 @@
             :tags ["oppija-external"]
 
             lokalisointi-handler/routes
-
-            (route-middleware
-              [wrap-authorize]
-              (c-api/context "/koodisto" []
-                (c-api/GET "/:koodi-uri" [koodi-uri]
-                  :path-params [koodi-uri :- s/Str]
-                  :summary "Koodiston haku Koodisto-Koodi-Urilla."
-                  :return (rest/response s/Any)
-                  (rest/rest-ok (koodisto/get-koodi koodi-uri))))
-
-              (c-api/context "/eperusteet" []
-                (c-api/GET "/tutkinnonosat/:id/viitteet" [id]
-                  :path-params [id :- Long]
-                  :summary "Tutkinnon osan viitteet."
-                  :return (rest/response [s/Any])
-                  (rest/rest-ok (eperusteet/get-tutkinnon-osa-viitteet id)))
-
-                (c-api/GET "/tutkinnot" []
-                  :query-params [diaarinumero :- String]
-                  :summary "Tutkinnon haku diaarinumeron perusteella."
-                  :return (rest/response s/Any)
-                  (rest/rest-ok (eperusteet/find-tutkinto diaarinumero)))
-
-                (c-api/GET "/tutkinnot/:id/suoritustavat/reformi/rakenne" [id]
-                  :path-params [id :- Long]
-                  :summary "Tutkinnon rakenne."
-                  :return (rest/response s/Any)
-                  (rest/rest-ok (eperusteet/get-suoritustavat id)))
-
-                (c-api/GET "/tutkinnot/:id/suoritustavat/ops/tutkinnonosat" []
-                  :path-params [id :- Long]
-                  :summary "Tutkinnon ops suoritustavat"
-                  :return (rest/response s/Any)
-                  (rest/with-not-found-handling
-                    (eperusteet/get-ops-suoritustavat id)))
-
-                (c-api/GET "/:koodi-uri" [koodi-uri]
-                  :path-params [koodi-uri :- s/Str]
-                  :summary "Tutkinnon osan perusteiden
-                           haku Koodisto-Koodi-Urilla."
-                  :return (rest/response [s/Any])
-                  (rest/rest-ok (eperusteet/find-tutkinnon-osat koodi-uri))))
-
-              (c-api/context "/eperusteet-amosaa" []
-                (c-api/GET "/koodi/:koodi" []
-                  :path-params [koodi :- String]
-                  :summary "Amosaa tutkinnon osan hakeminen koodin perusteella.
-                 Koodiin täydennetään automaattisesti
-                 'paikallinen_tutkinnonosa'"
-                  :return (rest/response [s/Any])
-                  (rest/rest-ok (amosaa/get-tutkinnon-osa-by-koodi koodi))))
-
-              (c-api/context "/organisaatio" []
-                (c-api/GET "/:oid" []
-                  :path-params [oid :- s/Str]
-                  :summary "Organisaation tiedot oidin perusteella"
-                  :return (rest/response s/Any)
-                  (rest/with-not-found-handling
-                    (organisaatio/get-organisaatio-info oid))))))
+            oppija-external/routes)
 
           (c-api/context "/oppijat" []
             :tags ["oppijat"]
