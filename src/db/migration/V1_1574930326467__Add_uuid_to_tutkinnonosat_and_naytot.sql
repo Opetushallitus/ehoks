62910cae3f60dd28de98ea4e2720a439f3cf26da
Joona Haavisto <joona.haavisto@gofore.com>
2019-11-29 17:02:46 +0200

    Add uuid also to naytot

--- src/db/migration/V1_1574930326467__Add_uuid_to_tutkinnonosat_and_naytot.sql
+++ src/db/migration/V1_1574930326467__Add_uuid_to_tutkinnonosat_and_naytot.sql
@@ -15,3 +15,18 @@ ALTER TABLE hankittavat_paikalliset_tutkinnon_osat
 
 ALTER TABLE hankittavat_yhteiset_tutkinnon_osat
     ADD COLUMN uuid UUID UNIQUE DEFAULT gen_random_uuid();
+
+ALTER TABLE aiemmin_hankitun_ammat_tutkinnon_osan_naytto
+    ADD COLUMN uuid UUID UNIQUE DEFAULT gen_random_uuid();
+
+ALTER TABLE aiemmin_hankitun_paikallisen_tutkinnon_osan_naytto
+    ADD COLUMN uuid UUID UNIQUE DEFAULT gen_random_uuid();
+
+ALTER TABLE aiemmin_hankitun_yhteisen_tutkinnon_osan_naytto
+    ADD COLUMN uuid UUID UNIQUE DEFAULT gen_random_uuid();
+
+ALTER TABLE hankittavan_ammat_tutkinnon_osan_naytto
+    ADD COLUMN uuid UUID UNIQUE DEFAULT gen_random_uuid();
+
+ALTER TABLE hankittavan_paikallisen_tutkinnon_osan_naytto
+    ADD COLUMN uuid UUID UNIQUE DEFAULT gen_random_uuid();
