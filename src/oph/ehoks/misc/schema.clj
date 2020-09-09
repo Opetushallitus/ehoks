8649bc0f24be18f3df634eae31c1083cf71a3562
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-09-09 11:03:59 +0300

    EH-984: Made environment route to return cas-oppija-login-url

--- src/oph/ehoks/misc/schema.clj
+++ src/oph/ehoks/misc/schema.clj
@@ -9,4 +9,5 @@
               :opintopolku-logout-url-fi s/Str
               :opintopolku-logout-url-sv s/Str
               :virkailija-login-url s/Str
+              :cas-oppija-login-url s/Str
               :raamit-url s/Str})
