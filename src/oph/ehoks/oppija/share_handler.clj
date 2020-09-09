d40bc0c65ea3668e1b529a0e95d6d1a6093bbff1
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-09-09 10:36:00 +0300

    eh-981 fixed long lines of share handler

--- src/oph/ehoks/oppija/share_handler.clj
+++ src/oph/ehoks/oppija/share_handler.clj
@@ -34,7 +34,8 @@
           tutkinnonosa (get-tutkinnonosa-details
                          (:tutkinnonosa-tyyppi jakolinkki)
                          (:tutkinnonosa-module-uuid jakolinkki))
-          module (first (h/get-osaamisenosoittaminen-or-hankkimistapa-of-jakolinkki
+          module (first
+                   (h/get-osaamisenosoittaminen-or-hankkimistapa-of-jakolinkki
                    jakolinkki))]
       (cond
         (= (:shared-module-tyyppi jakolinkki) "osaamisenhankkiminen")
