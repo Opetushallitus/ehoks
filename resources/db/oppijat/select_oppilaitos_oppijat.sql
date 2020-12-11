db0618aba248437f715ebac2cf1d86755710de24
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-12-11 14:31:38 +0200

    OY-2720 : Get only oppilaat from oppilaitos with hoks.

--- resources/db/oppijat/select_oppilaitos_oppijat.sql
+++ resources/db/oppijat/select_oppilaitos_oppijat.sql
@@ -8,8 +8,11 @@ SELECT o.oid,
 FROM oppijat AS o
        LEFT OUTER JOIN opiskeluoikeudet AS oo
                        ON (o.oid = oo.oppija_oid)
+       LEFT OUTER JOIN hoksit AS h
+                       ON (oo.oid = h.opiskeluoikeus_oid)
 WHERE ((oo.oppilaitos_oid IS NOT NULL AND oo.oppilaitos_oid LIKE ?) OR
        (oo.koulutustoimija_oid IS NOT NULL AND oo.koulutustoimija_oid LIKE ?))
+  AND h.opiskeluoikeus_oid IS NOT NULL
   AND o.nimi ILIKE ?
       :tutkinto-filter
       :osaamisala-filter
