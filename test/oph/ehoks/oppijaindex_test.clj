c5cfa4466ae8d17a9ea72aaeadab9722d2fda34a
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-12-23 12:34:12 +0200

    OY-2720 : Format code

--- test/oph/ehoks/oppijaindex_test.clj
+++ test/oph/ehoks/oppijaindex_test.clj
@@ -274,8 +274,7 @@
            {:status 200
             :body   (assoc
                       opiskeluoikeus-data
-                      :oid "1.2.246.562.15.00000000001"
-                      )}))]
+                      :oid "1.2.246.562.15.00000000001")}))]
       (sut/add-oppija! "1.2.246.562.24.111111111111")
       (sut/add-opiskeluoikeus!
         "1.2.246.562.15.00000000001" "1.2.246.562.24.111111111111"))
@@ -289,24 +288,27 @@
                                 :sisältyyOpiskeluoikeuteen
                                 {:oppilaitos {:oid "1.2.246.562.15.99999123"
                                               :oppilaitosnumero
-                                                   {:koodiarvo "10076"}
+                                              {:koodiarvo "10076"}
                                               :nimi
-                                                   {:fi "Testi-yliopisto"
-                                                    :sv "Testi-universitetet"
-                                                    :en "Testi University"}}
+                                              {:fi "Testi-yliopisto"
+                                               :sv "Testi-universitetet"
+                                               :en "Testi University"}}
                                  :oid        "1.2.246.562.15.99999123"}))
                             (utils/eq
-                              (sut/get-opiskeluoikeus-by-oid "1.2.246.562.15.00000000001")
-                              {:osaamisala-nimi                     {:fi "",
-                                                                     :sv ""},
-                               :oid                                 "1.2.246.562.15.00000000001",
-                               :hankintakoulutus-opiskeluoikeus-oid "1.2.246.562.15.00000000001",
-                               :oppilaitos-oid                      "1.2.246.562.10.222222222222",
-                               :tutkinto-nimi                       {:en "Testing",
-                                                                     :fi "Testialan perustutkinto",
-                                                                     :sv "Grundexamen inom testsbranschen"},
-                               :oppija-oid                          "1.2.246.562.24.111111111111",
-                               :hankintakoulutus-jarjestaja-oid     "1.2.246.562.15.99999123"}))))
+                              (sut/get-opiskeluoikeus-by-oid
+                                "1.2.246.562.15.00000000001")
+                              {:osaamisala-nimi {:fi "", :sv ""},
+                               :oid "1.2.246.562.15.00000000001",
+                               :hankintakoulutus-opiskeluoikeus-oid
+                               "1.2.246.562.15.00000000001",
+                               :oppilaitos-oid "1.2.246.562.10.222222222222",
+                               :tutkinto-nimi
+                               {:en "Testing",
+                                :fi "Testialan perustutkinto",
+                                :sv "Grundexamen inom testsbranschen"},
+                               :oppija-oid "1.2.246.562.24.111111111111",
+                               :hankintakoulutus-jarjestaja-oid
+                               "1.2.246.562.15.99999123"}))))
 
 (t/deftest set-paattynyt-test
   (t/testing "Setting paattynyt timestamp"
