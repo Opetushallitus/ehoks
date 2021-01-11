853ece21c315aaa0845492cfeafa741095013b2d
Tatu Honkanen <tatu.honkanen@cybercom.com>
2021-01-11 15:07:02 +0200

    OY-2720 : Fix sql and oppilaitos oppijat count.

--- resources/db/oppijat/select_oppilaitos_oppijat_search_count.sql
+++ resources/db/oppijat/select_oppilaitos_oppijat_search_count.sql
@@ -1,10 +1,12 @@
-SELECT 
+SELECT
   COUNT(o.oid)
 FROM oppijat AS o
   LEFT OUTER JOIN opiskeluoikeudet AS oo
     ON (o.oid = oo.oppija_oid)
-  WHERE 
-    ((oo.oppilaitos_oid IS NOT NULL AND oo.oppilaitos_oid LIKE ?) OR 
+  INNER JOIN hoksit AS h
+    ON (oo.oid = h.opiskeluoikeus_oid)
+  WHERE
+    ((oo.oppilaitos_oid IS NOT NULL AND oo.oppilaitos_oid LIKE ?) OR
      (oo.koulutustoimija_oid IS NOT NULL AND oo.koulutustoimija_oid LIKE ?)) AND
     o.nimi ILIKE ?
     :tutkinto-filter
