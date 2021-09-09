da2a33fe03c7c096bc83ad6036acd947178cf07a
samje <shonkanen@gmail.com>
2021-09-09 10:49:46 +0300

    OY-3032: nil-check to opiskeluoikeus-active?

--- src/oph/ehoks/oppijaindex.clj
+++ src/oph/ehoks/oppijaindex.clj
@@ -346,8 +346,10 @@
   :prevent-finished-opiskeluoikeus-updates? is removed or changed to true."
   ([opiskeluoikeus-oid]
     (let [opiskeluoikeus (k/get-opiskeluoikeus-info opiskeluoikeus-oid)]
-      (not (opiskeluoikeus-tila-inactive?
-             (get-opiskeluoikeus-tila opiskeluoikeus))))))
+      (not (and
+             (nil? opiskeluoikeus)
+             (opiskeluoikeus-tila-inactive?
+               (get-opiskeluoikeus-tila opiskeluoikeus)))))))
 
 (defn opiskeluoikeus-still-active?
   "Checks if the given opiskeluoikeus is still valid, ie. not valmistunut,
