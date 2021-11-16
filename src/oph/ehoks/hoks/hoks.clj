a955fbf3b5f5bf9ad615e68bc0033a97854b8596
Kelvin Jackson <kelvin.jackson@gofore.com>
2021-11-16 12:16:10 +0200

    Virhe korjattu

--- src/oph/ehoks/hoks/hoks.clj
+++ src/oph/ehoks/hoks/hoks.clj
@@ -422,12 +422,14 @@
                                             hoks)
           old-osaamisen-hankkimisen-tarve (:osaamisen-hankkimisen-tarve
                                             hoks)
+          old-sahkoposti (:sahkoposti hoks)
           new-opiskeluoikeus-oid (:opiskeluoikeus-oid new-values)
           new-oppija-oid (:oppija-oid new-values)
           new-osaamisen-saavuttamisen-pvm (:osaamisen-saavuttamisen-pvm
                                             new-values)
           new-osaamisen-hankkimisen-tarve (:osaamisen-hankkimisen-tarve
                                             new-values)
+          new-sahkoposti (:sahkoposti new-values)
           osaamisen-hankkimistavat (get-osaamisen-hankkimistavat new-values)
           oh-missing-tyopaikan-y-tunnus (missing-tyopaikan-y-tunnus?
                                           osaamisen-hankkimistavat)]
@@ -452,9 +454,12 @@
                   old-osaamisen-saavuttamisen-pvm
                   new-osaamisen-saavuttamisen-pvm)
             (send-paattokysely hoks-id new-osaamisen-saavuttamisen-pvm hoks))
-          (when (and
-                  (true? new-osaamisen-hankkimisen-tarve)
-                  (not (true? old-osaamisen-hankkimisen-tarve)))
+          (when (or (and
+                      (true? new-osaamisen-hankkimisen-tarve)
+                      (not (true? old-osaamisen-hankkimisen-tarve)))
+                    (and
+                      (some? new-sahkoposti)
+                      (not (some? old-sahkoposti))))
             (sqs/send-amis-palaute-message
               (sqs/build-hoks-hyvaksytty-msg hoks-id hoks)))
           h)))))
