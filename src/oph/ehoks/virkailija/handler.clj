7093a3748e19871af8d8f7ae23a20d0b78c9ea0c
samje <shonkanen@gmail.com>
2021-11-18 11:06:42 +0200

    syntax fix

--- src/oph/ehoks/virkailija/handler.clj
+++ src/oph/ehoks/virkailija/handler.clj
@@ -532,7 +532,7 @@
                                                :new (assoc
                                                       hoks
                                                       :deleted_at
-                                                     "*ADDED*")})
+                                                      "*ADDED*")})
                                 (response/forbidden
                                   {:error (str "User privileges does not match "
                                                "organisation")}))))))
