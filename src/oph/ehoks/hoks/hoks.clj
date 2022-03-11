5e5148c6c8f5a8e12155576b4549eb41398b60c5
samih <shonkanen@gmail.com>
2022-03-11 14:35:39 +0200

    OY-3411

--- src/oph/ehoks/hoks/hoks.clj
+++ src/oph/ehoks/hoks/hoks.clj
@@ -161,6 +161,9 @@
                          "opiskeluoikeus-oid %s.")
                     hoks-id os-saavut-pvm (:opiskeluoikeus-oid hoks)))))
 
+(defn error-log-hoks-id [id]
+  (log/errorf "Error caused by hoks-id: %s" id))
+
 (defn save-hoks! [h]
   (jdbc/with-db-transaction
     [conn (db-ops/get-db-connection)]
