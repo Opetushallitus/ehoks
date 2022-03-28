c67e232c5db944fb230f994b2105e35718b4eacc
samje <shonkanen@gmail.com>
2022-03-28 14:15:53 +0300

    Muutetaan hakua hieman

--- resources/db/hoksit/select_hoksit_by_ensikert_hyvaks_and_saavutettu_tiedot.sql
+++ resources/db/hoksit/select_hoksit_by_ensikert_hyvaks_and_saavutettu_tiedot.sql
@@ -1,8 +1,9 @@
 SELECT h.id, h.opiskeluoikeus_oid, h.oppija_oid
 FROM hoksit h
-  LEFT OUTER JOIN kyselylinkit kl
-    ON (h.id = kl.hoks_id AND kl.tyyppi = 'tutkinnon_suorittaneet')
+       LEFT OUTER JOIN kyselylinkit kl ON (h.id = kl.hoks_id)
 WHERE
-  kl.hoks_id IS NULL
+  ((kl.tyyppi != 'tutkinnon_suorittaneet'
+    AND kl.tyyppi != 'tutkinnon_osia_suorittaneet')
+    OR kl.tyyppi IS NULL)
   AND osaamisen_saavuttamisen_pvm IS NULL
   AND ensikertainen_hyvaksyminen >= (current_date - INTERVAL '2 years');
