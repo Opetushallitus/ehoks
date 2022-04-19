e7a1eb33824b667546ecac31299124fddc4f4bb5
Kelvin Jackson <kelvin.jackson@gofore.com>
2022-04-19 14:06:28 +0300

    WIP: enemmän docstringeja

--- src/oph/ehoks/user_settings.clj
+++ src/oph/ehoks/user_settings.clj
@@ -1,12 +1,18 @@
 (ns oph.ehoks.user-settings
   (:require [oph.ehoks.db.db-operations.user-settings :as db]))
 
-(defn save-settings! [user-oid data]
+(defn save-settings!
+  "Save user settings to database"
+  [user-oid data]
   (db/insert-or-update-user-settings! user-oid data)
   data)
 
-(defn get-settings [user-oid]
+(defn get-settings
+  "Get user settings from database"
+  [user-oid]
   (:data (first (db/select-user-settings-by-user-oid user-oid))))
 
-(defn delete-settings! [user-oid]
+(defn delete-settings!
+  "Delete settings from database for user"
+  [user-oid]
   (db/delete-user-settings! user-oid))
