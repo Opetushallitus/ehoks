c10306ff6305dfc85805317d12ed51f9789ad403
samje <shonkanen@gmail.com>
2022-04-22 12:08:19 +0300

    Selectiin muutos

--- resources/db/oppijat/select_oppija_with_opiskeluoikeus_oid_by_oid.sql
+++ resources/db/oppijat/select_oppija_with_opiskeluoikeus_oid_by_oid.sql
@@ -1,4 +1,5 @@
-SELECT oppijat.*,
+SELECT oppijat.nimi AS nimi,
+       oppijat.oid AS oid,
        oo.oid as opiskeluoikeus_oid
   FROM oppijat
   LEFT JOIN opiskeluoikeudet oo ON oppijat.oid = oo.oppija_oid
