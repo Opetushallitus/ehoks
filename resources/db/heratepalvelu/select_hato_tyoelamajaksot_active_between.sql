a86870ee0aac2412257eb256135ae6be2bbc43dd
Kelvin Jackson <kelvin.jackson@gofore.com>
2022-06-16 13:29:53 +0300

    WIP

--- resources/db/heratepalvelu/select_hato_tyoelamajaksot_active_between.sql
+++ resources/db/heratepalvelu/select_hato_tyoelamajaksot_active_between.sql
@@ -1,20 +1,9 @@
 SELECT
-  h.id AS hoks_id,
   h.opiskeluoikeus_oid AS opiskeluoikeus_oid,
-  h.oppija_oid AS oppija_oid,
-  osa.id AS tutkinnonosa_id,
-  osa.tutkinnon_osa_koodi_uri AS tutkinnonosa_koodi,
   oh.id AS hankkimistapa_id,
-  oh.osaamisen_hankkimistapa_koodi_uri AS hankkimistapa_tyyppi,
   oh.alku AS jakso_alkupvm,
   oh.loppu AS jakso_loppupvm,
-  oh.osa_aikaisuustieto AS osa_aikaisuus,
-  oh.oppisopimuksen_perusta_koodi_uri AS oppisopimuksen_perusta,
-  tjk.tyopaikan_nimi AS tyopaikan_nimi,
-  tjk.tyopaikan_y_tunnus AS tyopaikan_ytunnus,
-  tjk.vastuullinen_tyopaikka_ohjaaja_nimi AS tyopaikkaohjaaja_nimi,
-  tjk.vastuullinen_tyopaikka_ohjaaja_sahkoposti AS tyopaikkaohjaaja_email,
-  tjk.vastuullinen_tyopaikka_ohjaaja_puhelinnumero AS tyopaikkaohjaaja_puhelinnumero
+  oh.osa_aikaisuustieto AS osa_aikaisuus
 FROM hoksit h
   LEFT OUTER JOIN hankittavat_ammat_tutkinnon_osat AS osa
     ON (h.id = osa.hoks_id AND osa.deleted_at IS NULL)
@@ -22,8 +11,6 @@ FROM hoksit h
     ON (osa.id = osajoin.hankittava_ammat_tutkinnon_osa_id)
   LEFT OUTER JOIN osaamisen_hankkimistavat AS oh
     ON (osajoin.osaamisen_hankkimistapa_id = oh.id)
-  LEFT OUTER JOIN tyopaikalla_jarjestettavat_koulutukset AS tjk
-    ON (oh.tyopaikalla_jarjestettava_koulutus_id = tjk.id)
 WHERE
   (oh.osaamisen_hankkimistapa_koodi_uri = 'osaamisenhankkimistapa_koulutussopimus' OR
   oh.osaamisen_hankkimistapa_koodi_uri = 'osaamisenhankkimistapa_oppisopimus')
