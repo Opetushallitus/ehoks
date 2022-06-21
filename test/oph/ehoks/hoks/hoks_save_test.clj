b4c065ad29cc1b10ac851a7eae60f5ae7bb3fc97
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2022-06-21 17:26:53 +0300

    OY-3711: hoksin tallennukseen hankittavat koulutuksen osat

--- test/oph/ehoks/hoks/hoks_save_test.clj
+++ test/oph/ehoks/hoks/hoks_save_test.clj
@@ -325,6 +325,7 @@
                 :aiemmin-hankitut-ammat-tutkinnon-osat ahato-data
                 :hankittavat-yhteiset-tutkinnon-osat hyto-data
                 :hankittavat-ammat-tutkinnon-osat hao-data
+                :hankittavat-koulutuksen-osat koulutuksen-osa-data
                 :opiskeluvalmiuksia-tukevat-opinnot oto-data})
 
 (def min-hoks-data {:opiskeluoikeus-oid "1.2.246.562.15.00000000001"})
