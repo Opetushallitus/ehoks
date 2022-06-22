6b1398fa30ea345eaeaacbdd1460c8b45b869c13
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2022-06-22 10:41:07 +0300

    OY-3711: Lisätytty koulutuksen osa koodi urille versio

--- src/db/migration/V1_1655471352314__Add_hankittavat_koulutuksen_osat.sql
+++ src/db/migration/V1_1655471352314__Add_hankittavat_koulutuksen_osat.sql
@@ -5,5 +5,6 @@ CREATE TABLE hankittavat_koulutuksen_osat
   updated_at                 TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
   deleted_at                 TIMESTAMP WITH TIME ZONE,
   hoks_id                    INTEGER REFERENCES hoksit (id),
-  koulutuksen_osa_koodi_uri  VARCHAR(256)
+  koulutuksen_osa_koodi_uri  VARCHAR(256),
+  koulutuksen_osa_koodi_uri_versio INTEGER
 );
