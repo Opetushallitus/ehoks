8b99ae83fb48b704aa612022e311326c07548fe6
samje <shonkanen@gmail.com>
2022-08-25 10:57:53 +0300

    on delete cascade

--- src/db/migration/V1_1659685875368__Add_hankittavat_koulutuksen_osat.sql
+++ src/db/migration/V1_1659685875368__Add_hankittavat_koulutuksen_osat.sql
@@ -4,7 +4,7 @@ CREATE TABLE hankittavat_koulutuksen_osat
   created_at                 TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
   updated_at                 TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
   deleted_at                 TIMESTAMP WITH TIME ZONE,
-  hoks_id                    INTEGER REFERENCES hoksit (id),
+  hoks_id                    INTEGER REFERENCES hoksit (id) ON DELETE CASCADE,
   koulutuksen_osa_koodi_uri  VARCHAR(256),
   koulutuksen_osa_koodi_versio INTEGER,
   alku DATE,
