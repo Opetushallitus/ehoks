bd9064172f0d559c446ef49b1d3319a9455cc037
samje <sami.honkanen@knowit.fi>
2023-02-21 10:22:36 +0200

    Parempi nimi muuttujalle

--- src/oph/ehoks/hoks/hoks.clj
+++ src/oph/ehoks/hoks/hoks.clj
@@ -240,7 +240,7 @@
 (defn save-hoks!
   "Tallentaa yhden HOKSin arvot tietokantaan."
   [h]
-  (let [saved-hoks-obj
+  (let [hoks-db
         (jdbc/with-db-transaction
           [conn (db-ops/get-db-connection)]
           (let [saved-hoks (db-hoks/insert-hoks! h conn)
@@ -293,13 +293,13 @@
     (future
       (when (and (:osaamisen-hankkimisen-tarve h)
                  (false? (tuva-related-hoks? h)))
-        (send-aloituskysely (:id saved-hoks-obj) h))
+        (send-aloituskysely (:id hoks-db) h))
       (when (and (:osaamisen-saavuttamisen-pvm h)
                  (false? (tuva-related-hoks? h)))
-        (send-paattokysely (:id saved-hoks-obj)
+        (send-paattokysely (:id hoks-db)
                            (:osaamisen-saavuttamisen-pvm h)
                            h)))
-    saved-hoks-obj))
+    hoks-db))
 
 (defn- merge-not-given-hoks-values
   "Varmistaa, että tietyt kentät ovat olemassa HOKSissa, vaikka niissä olisi
