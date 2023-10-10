37c337d0194055c87301fa23feb69bca4a85c9ae
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2023-10-10 16:21:32 +0300

    Improve helper scripts
    - curl-with-cas.sh: parameterise for SERVICE so that it can be used to
      call other services
    - upload-hoks.sh: don't output headers so the output is easier to parse

--- scripts/curl-with-cas.sh
+++ scripts/curl-with-cas.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 test -z "$VIRKAILIJA" && VIRKAILIJA=virkailija.testiopintopolku.fi
+test -z "$SERVICE" \
+&& SERVICE="https://$VIRKAILIJA/ehoks-virkailija-backend/cas-security-check"
 
 TGT_URL=$(cat scripts/cas-tgt-url.txt)
 test -z "$TGT_URL" && echo "Use scripts/create-cas-tgt.sh first" && exit 1
 
-ST=$(curl -s "$TGT_URL" -H 'Caller-id: panutest' --data-urlencode \
-  "service=https://$VIRKAILIJA/ehoks-virkailija-backend/cas-security-check")
+ST=$(curl -s "$TGT_URL" -H 'Caller-id: panutest' \
+	--data-urlencode "service=$SERVICE")
 test -z "$ST" && echo "Service ticket was not created" && exit 1
 
 curl -H "ticket: $ST" -H 'caller-id: panutesti' "$@"
