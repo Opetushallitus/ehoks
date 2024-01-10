b8752f6eaa8d7cf1c926d620d43a4cdd5b114218
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2024-01-10 13:13:35 +0200

    More robust ONR update handling
    - don't treat name changes and slave updates as alternatives, instead,
      do both as needed
    - always remove old duplicate oids
    - add new ONR data the same way as oppijaindex is usually updated

--- src/oph/ehoks/oppijaindex.clj
+++ src/oph/ehoks/oppijaindex.clj
@@ -541,66 +541,43 @@
           false))
       true)))
 
+(defn update-oppija-oid-in-db!
+  "Change the OID of an oppija to a new one in all tables in the database."
+  [old-oid new-oid]
+  (log/infof (str "Changing duplicate oppija-oid %s to %s for tables "
+                  "hoksit, oppijat and opiskeluoikeudet.") old-oid new-oid)
+  (jdbc/with-db-transaction
+    [db-conn (db-ops/get-db-connection)]
+    (db-hoks/update-hoks-by-oppija-oid! old-oid {:oppija-oid new-oid} db-conn)
+    (add-oppija! new-oid)
+    (db-opiskeluoikeus/update-opiskeluoikeus-by-oppija-oid!
+      old-oid {:oppija-oid new-oid})
+    (db-ops/delete! :oppijat ["oid = ?" old-oid])))
+
 (defn handle-onrmodified
   "Handles ONR-modified call from heratepalvelu which is triggered by
   data change in ONR service."
   [oid]
-  (if-let [oppija (get-oppija-by-oid oid)]
-    ;; Jos päivitetyn oppijan oid löytyy ehoksista, niin tiedetään
-    ;; että kyseessä ei ole oid-tiedon päivitys ja voidaan vain tarkastaa,
-    ;; että nimi täsmää ONR:n tiedon kanssa.
-    (let [onr-oppija (:body (onr/find-student-by-oid-no-cache oid))
-          ehoks-oppija-nimi (:nimi oppija)
-          onr-oppija-nimi (format-oppija-name onr-oppija)]
-      (if (= ehoks-oppija-nimi onr-oppija-nimi)
-        (log/info "Update for" oid "from ONR but no changes detected")
-        (do (log/infof "Updating changed name for oppija %s" oid)
-            (update-oppija! oid true))))
-    ;; Jos oppijaa ei löydy päivitetyllä oidilla ehoksista,
-    ;; niin ensin tarkastetaan, ettei kyseessä ole duplicate/slave oid.
-    ;; (tämä saattaa olla turha tarkastus, mutta ainakin se estää sen,
-    ;; että koskaan päivitettäisiin slave oideja ehoksin tauluihin.)
-    ;;
-    ;; Sitten haetaan kyseisen master-oidin slavet ja niiden oideilla
-    ;; oppijat ehoksin oppijat-taulusta.
-    ;;
-    ;; Jos oppijoita löytyy slave oideilla, niin päivitetään niiden
-    ;; hokseihin, opiskeluoikeuksiin ja oppijat-taulun riviin uusi
-    ;; master-oid. Lopuksi poistetaan slave-oidit oppijat taulusta.
-    (let [onr-oppija (:body (onr/find-student-by-oid-no-cache oid))]
-      (if (:duplicate onr-oppija)
-        (log/warn "Update for" oid "from ONR but it's marked as duplicate:"
-                  onr-oppija)
-        (let [slave-oppija-oids
-              (map
-                :oidHenkilo
-                (:body (onr/get-slaves-of-master-oppija-oid oid)))
-              oppijas-from-oppijaindex-by-slave-oids
-              (remove nil?
-                      (flatten
-                        (map
-                          #(:oid (get-oppija-by-oid %))
-                          slave-oppija-oids)))]
-          (if (empty? oppijas-from-oppijaindex-by-slave-oids)
-            (log/warn "Update for" oid "from ONR but no updatable oids found in"
-                      slave-oppija-oids)
-            (jdbc/with-db-transaction
-              [db-conn (db-ops/get-db-connection)]
-              (doseq [oppija-oid oppijas-from-oppijaindex-by-slave-oids]
-                (log/infof (str "Changing duplicate oppija-oid %s to %s "
-                                "for tables hoksit, oppijat and "
-                                "opiskeluoikeudet.")
-                           oppija-oid oid)
-                (db-hoks/update-hoks-by-oppija-oid! oppija-oid
-                                                    {:oppija-oid oid}
-                                                    db-conn)
-                (if (some? (get-oppija-by-oid oid))
-                  (do
-                    (db-opiskeluoikeus/update-opiskeluoikeus-by-oppija-oid!
-                      oppija-oid {:oppija-oid oid})
-                    (db-ops/delete!
-                      :oppijat ["oid = ?" oppija-oid]))
-                  (db-oppija/update-oppija!
-                    oppija-oid
-                    {:oid  oid
-                     :nimi (format-oppija-name onr-oppija)}))))))))))
+  (let [onr-oppija (:body (onr/find-student-by-oid-no-cache oid))]
+    ;; Tarkistetaan, että oppijan nimi on oikein.
+    (when-let [indexed-oppija (get-oppija-by-oid oid)]
+      (let [indexed-oppija-nimi (:nimi indexed-oppija)
+            onr-oppija-nimi (format-oppija-name onr-oppija)]
+        (if (= indexed-oppija-nimi onr-oppija-nimi)
+          (log/info "Update for" oid "from ONR but name not changed")
+          (do (log/infof "Updating changed name for oppija %s" oid)
+              (update-oppija! oid true)))))
+    ;; Jos OID on henkilön pää-OID (ei duplikaatti eikä yhdistetty toiseen),
+    ;; päivitetään mahdollisesti eHOKSista vanhoilla OIDeilla löytyvät tiedot
+    ;; uudelle OIDille.
+    (if (:duplicate onr-oppija)
+      (log/warn "Update for" oid "from ONR but it's marked as duplicate:"
+                onr-oppija)
+      (let [slaves (:body (onr/get-slaves-of-master-oppija-oid oid))
+            indexed-slaves (keep #(:oid (get-oppija-by-oid %))
+                                 (map :oidHenkilo slaves))]
+        (if (empty? indexed-slaves)
+          (log/warn "Update for" oid "from ONR but no updatable oids found in"
+                    (map :oidHenkilo slaves))
+          (doseq [slave-oid indexed-slaves]
+            (update-oppija-oid-in-db! slave-oid oid)))))))
