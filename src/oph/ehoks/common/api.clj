aaf3049b4aebde5da67112de463e66d28e579522
Severi Jääskeläinen <severi.jaaskelainen@gofore.com>
2024-03-15 18:12:33 +0200

    Relocate duplicate opiskeluoikeus exception handling

--- src/oph/ehoks/common/api.clj
+++ src/oph/ehoks/common/api.clj
@@ -1,15 +1,13 @@
 (ns oph.ehoks.common.api
-  (:require [clojure.string :as cstr]
-            [clojure.tools.logging :as log]
-            [compojure.api.coercion.core :as cc]
-            [compojure.api.exception :as c-ex]
+  (:require [compojure.api.exception :as c-ex]
             [oph.ehoks.config :refer [config]]
             [oph.ehoks.external.organisaatio :as organisaatio]
             [oph.ehoks.logging.access :refer [wrap-access-logger]]
             [oph.ehoks.middleware :as middleware]
             [ring.middleware.session :as session]
             [ring.middleware.session.memory :as mem]
-            [ring.util.http-response :as response]))
+            [ring.util.http-response :as response])
+  (:import [clojure.lang ExceptionInfo]))
 
 (defn dissoc-schema-validation-handler!
   [e data req]
@@ -28,6 +26,10 @@
   [_ __ ___]
   (response/unauthorized {:reason "Unable to check access rights"}))
 
+(defn bad-request-handler
+  [^ExceptionInfo e _ _]
+  (response/bad-request {:error (ex-message e)}))
+
 (def handlers
   "Map of request handlers"
   {::c-ex/request-parsing (c-ex/with-logging
@@ -39,7 +41,8 @@
    ::c-ex/response-validation (c-ex/with-logging
                                 c-ex/http-response-handler :error)
    ::organisaatio/organisation-not-found (c-ex/with-logging
-                                           organisaatio/not-found-handler :warn)
+                                           bad-request-handler :warn)
+   :opiskeluoikeus-already-exists (c-ex/with-logging bad-request-handler :warn)
    :not-found not-found-handler
    :unauthorized unauthorized-handler
    ::c-ex/default c-ex/safe-handler})
