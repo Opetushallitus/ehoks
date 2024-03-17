13430e631e985b5b84987f18a60fac19a95dbfd9
Severi Jääskeläinen <severi.jaaskelainen@gofore.com>
2024-03-17 20:42:31 +0200

    Namespace change: user-settings -> user.settings

--- src/oph/ehoks/user/settings.clj
+++ src/oph/ehoks/user/settings.clj
@@ -1,18 +1,18 @@
-(ns oph.ehoks.user-settings
+(ns oph.ehoks.user.settings
   (:require [oph.ehoks.db.db-operations.user-settings :as db]))
 
-(defn save-settings!
+(defn save!
   "Save user settings to database"
   [user-oid data]
   (db/insert-or-update-user-settings! user-oid data)
   data)
 
-(defn get-settings
+(defn get!
   "Get user settings from database"
   [user-oid]
   (:data (first (db/select-user-settings-by-user-oid user-oid))))
 
-(defn delete-settings!
+(defn delete!
   "Delete settings from database for user"
   [user-oid]
   (db/delete-user-settings! user-oid))
