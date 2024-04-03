7118edf270bc7efa3e99d208f5cd072cef687355
Severi Jääskeläinen <severi.jaaskelainen@gofore.com>
2024-04-03 11:08:02 +0300

    Don't do audit logging twice on exceptions
    Most of the exceptions pass through `audit/wrap-logger` middleware since
    `wrap-exception` is lower in the middleware stack. Don't do redundant
    audit logging in these cases.

--- src/oph/ehoks/common/api.clj
+++ src/oph/ehoks/common/api.clj
@@ -42,33 +42,34 @@
       (fn [^ExceptionInfo e _ _] (resp {custom-key (ex-message e)}))
       (fn [^ExceptionInfo e _ _] (resp {:error (ex-message e)})))))
 
-(defn with-logging
-  "Combines `compojure.api.exception/with-logging` and `audit/with-logging`."
-  ([handler]           (audit/with-logging (c-ex/with-logging handler)))
-  ([handler log-level] (audit/with-logging
-                         (c-ex/with-logging handler log-level))))
-
 ;; `bad-request` is currently the most commonly used response we want to return.
 ;; Opiskeluoikeus, oppija, and organisation information are prerequisites for
 ;; many request handlers (e.g., HOKS should always have an existing oppija and
 ;; opiskeluoikeus linked to it) so if the user provides an OID which doesn't
 ;; link to any existing entity, it's a bad request. Logging level in these cases
-;; should be `:warn`.
+;; should be `:warn`. We don't need to do audit logging in this handler because
+;; `audit/wrap-logger` is higher in the middleware stack than `wrap-exception`.
 (def bad-request-handler
-  (with-logging (custom-ex-handler response/bad-request) :warn))
+  (c-ex/with-logging (custom-ex-handler response/bad-request) :warn))
 
 (def handlers
   "Map of custom exception handlers"
-  {::c-ex/request-parsing                (with-logging
-                                           c-ex/request-parsing-handler :info)
-   ::c-ex/request-validation             (with-logging
+  {::c-ex/request-parsing                (audit/with-logging
+                                           (c-ex/with-logging
+                                             c-ex/request-parsing-handler
+                                             :info))
+   ::c-ex/request-validation             (c-ex/with-logging
                                            dissoc-schema-validation-handler!
                                            :info)
-   ; Do log response body validation errors, but don't give the user internal
-   ; server errors in response.
-   ::c-ex/response-validation            (with-logging
+   ;; Do log response body validation errors, but don't give the user internal
+   ;; server errors in response. We don't need to do audit logging here because
+   ;; response validation exception thrown passes through `audit/wrap-logger`
+   ;; middleware.
+   ::c-ex/response-validation            (c-ex/with-logging
                                            c-ex/http-response-handler :error)
 
+   ;; We don't need to do audit logging in the handlers below because
+   ;; exceptions thrown go through `audit/wrap-logger` middleware.
    ::organisaatio/organisation-not-found bad-request-handler
    :disallowed-update                    bad-request-handler
    :opiskeluoikeus-already-exists        bad-request-handler
@@ -76,11 +77,11 @@
    ::onr/oppija-not-found                bad-request-handler
    ::oi/opiskeluoikeus-not-found         bad-request-handler
    :shared-link-validation-error         bad-request-handler
-   :shared-link-expired                  (with-logging
+   :shared-link-expired                  (c-ex/with-logging
                                            (custom-ex-handler response/gone
                                                               :message)
                                            :warn)
-   :shared-link-inactive                 (with-logging
+   :shared-link-inactive                 (c-ex/with-logging
                                            (custom-ex-handler response/locked
                                                               :message)
                                            :warn)
