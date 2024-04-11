91449c271c0c02482ee89b9de8c38686dc8afaad
Severi Jääskeläinen <severi.jaaskelainen@gofore.com>
2024-04-11 10:21:08 +0300

    Store opiskelijapalaute info to `palautteet` DB table when needed

---
+++ src/oph/ehoks/db/sql/opiskelijapalaute.sql
@@ -0,0 +1,14 @@
+-- :name insert! :? :1
+-- :doc Insert opiskelijapalautekysely information to DB
+insert into palautteet (herate_source, heratepvm, kyselytyyppi, tila, hoks_id)
+values (:herate-source,
+        :heratepvm,
+        :kyselytyyppi,
+        'odottaa_kasittelya',
+        :hoks-id)
+returning id
+
+-- :name get-by-hoks-id-and-tyyppi! :? :1
+-- :doc Get opiskelijapalaute information by HOKS ID and kyselytyyppi
+select * from palautteet
+where hoks_id = :hoks-id AND kyselytyyppi = :kyselytyyppi
