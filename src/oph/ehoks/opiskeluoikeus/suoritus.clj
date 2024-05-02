4e79e1117f5c1f42d84d08b91e6669960ec79c4f
Severi Jääskeläinen <severi.jaaskelainen@gofore.com>
2024-05-02 21:55:13 +0300

    Add `opiskeluoikeus.suoritus` namespace

---
+++ src/oph/ehoks/opiskeluoikeus/suoritus.clj
@@ -0,0 +1,21 @@
+(ns oph.ehoks.opiskeluoikeus.suoritus
+  (:require
+   [clojure.string :as string]))
+
+(defn tyyppi [suoritus] (get-in suoritus [:tyyppi :koodiarvo]))
+
+(defn ammatillinen?
+  "Varmistaa, että suorituksen tyyppi on joko ammatillinen tutkinto tai
+  osittainen ammatillinen tutkinto."
+  [suoritus]
+  (some? (#{"ammatillinentutkinto" "ammatillinentutkintoosittainen"}
+           (tyyppi suoritus))))
+
+(defn telma?
+  "Tarkistaa, onko suorituksen tyyppi TELMA (työhön ja elämään valmentava)."
+  [suoritus]
+  (some? (#{"telma", "telmakoulutuksenosa"} (tyyppi suoritus))))
+
+(defn kieli
+  [suoritus]
+  (string/lower-case (:koodiarvo (:suorituskieli suoritus))))
