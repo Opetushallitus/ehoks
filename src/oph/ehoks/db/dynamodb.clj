15afa6d451b8eef61fcf9ffaa1f8e0e4312139a1
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2024-06-18 10:28:12 +0300

    Env vars to configure local/real DDB

--- src/oph/ehoks/db/dynamodb.clj
+++ src/oph/ehoks/db/dynamodb.clj
@@ -1,21 +1,37 @@
 (ns oph.ehoks.db.dynamodb
-  (:require [taoensso.faraday :as far])
+  (:require [taoensso.faraday :as far]
+            [environ.core :refer [env]])
   (:import (com.amazonaws.auth BasicAWSCredentials AWSStaticCredentialsProvider)
            (com.amazonaws.client.builder AwsClientBuilder$EndpointConfiguration)
            (com.amazonaws.services.dynamodbv2 AmazonDynamoDBClientBuilder)))
 
-(defn get-test-client []
+(defn get-test-client
+  "Local DynamoDB needs a region even when the endpoint has been
+  overridden, because DynamoDB identifies tables by region and account
+  (different regions may have tables with the same name).  Thus we
+  construct a client that uses AWS_REGION for signing API requests even
+  when the API endpoint is in localhost.
+
+  The correct parameters for connecting to the DynamoDB in
+  ../../../../Makefile are:
+  - AWS_REGION=eu-west-1
+  - AWS_ENDPOINT_URL=http://localhost:18000
+  - AWS_ACCESS_KEY_ID=foo
+  - AWS_SECRET_KEY=bar"
+  []
   (let [^AmazonDynamoDBClientBuilder builder
         (doto (AmazonDynamoDBClientBuilder/standard)
           (.setEndpointConfiguration
             (AwsClientBuilder$EndpointConfiguration.
-              "http://localhost:18000" "eu-west-1"))
-          (.setCredentials
-            (AWSStaticCredentialsProvider.
-              (BasicAWSCredentials. "foo" "bar"))))]
+              (env :aws-endpoint-url) (env :aws-region))))]
     (.build builder)))
 
-(def localtesting-opts
-  {:client (get-test-client)})
+(defn get-client
+  "Return a DynamoDB client, based on environment variables, usable for
+  either local testing or real DynamoDB."
+  []
+  (if (and (env :aws-endpoint-url) (env :aws-region))
+    (get-test-client)
+    (.build (AmazonDynamoDBClientBuilder/standard))))
 
-(defn test-faraday [] (far/list-tables localtesting-opts))
+(def faraday-opts (delay {:client (get-client)}))
