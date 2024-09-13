51c915823d62fef55491723e148178da7fa98fd5
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2024-09-13 10:58:21 +0300

    EH-1716: make existing-heratteet! resilient against missing koulutustoimija

--- src/oph/ehoks/db/sql/palaute.sql
+++ src/oph/ehoks/db/sql/palaute.sql
@@ -79,7 +79,7 @@ from palautteet p
 join hoksit h on (h.id = p.hoks_id)
 where h.oppija_oid = :oppija-oid
   and p.kyselytyyppi in (:v*:kyselytyypit)
-  and p.koulutustoimija = :koulutustoimija
+  and (p.koulutustoimija = :koulutustoimija or (:koulutustoimija)::text is null)
 
 -- :name get-by-hoks-id-and-yksiloiva-tunniste! :? :*
 -- :doc Get palaute information for työpaikkajakso by HOKS ID and yksiloiva
