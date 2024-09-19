4dbd0af804adda9070c87d62f3533fe963a30278
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2024-09-19 23:50:54 +0300

    EH-1584: upgrade local postgres to version on sade

--- scripts/ci-scripts/run_postgres.sh
+++ scripts/ci-scripts/run_postgres.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd scripts/postgres-docker
 docker build -t ehoks-postgres .
-docker run --rm --name ehoks-postgres -p 5433:5432 -d ehoks-postgres
+docker run --rm --name ehoks-postgres -p 5433:5432 -d \
+  -e POSTGRES_HOST_AUTH_METHOD=trust ehoks-postgres
