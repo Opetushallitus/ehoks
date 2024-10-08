d78a05118577b3e5dbc8238bcbfe4f675ce3484c
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2024-10-08 17:54:18 +0300

    EH-1584: mock routes for Arvo calls

--- resources/dev/src/oph/ehoks/mocked_routes/mock_routes.clj
+++ resources/dev/src/oph/ehoks/mocked_routes/mock_routes.clj
@@ -4,6 +4,7 @@
             [clj-time.core :as time]
             [oph.ehoks.mocked-routes.mock-gen :as mock-gen]
             [oph.ehoks.mocked-routes.mock-koodisto-routes :as koodisto-mocks]
+            [oph.ehoks.mocked-routes.mock-arvo-routes :as arvo-mocks]
             [oph.ehoks.mocked-routes.mock-eperusteet-routes
              :as eperusteet-mocks]
             [oph.ehoks.mocked-routes.mock-koski-routes :as koski-mocks]
@@ -14,7 +15,6 @@
              :as oppijanumerorekisteri-mocks]))
 
 (defroutes mock-routes
-  (routes
     (GET "/lokalisointi/cxf/rest/v1/localisation" []
       (mock-gen/json-response-file
         "dev-routes/lokalisointi_cxf_rest_v1_localisation.json"))
@@ -26,14 +26,10 @@
         (mock-gen/json-response-file
           "dev-routes/kayttooikeus-service_kayttooikeus_kayttaja.json")))
 
-    (GET "/api/vastauslinkki/v1/status/:linkId" []
-      (mock-gen/json-response
-        {:vastattu false
-         :voimassa_loppupvm (time/plus (time/now) (time/hours 2))}))
-
     auth-mocks/mock-routes
+    arvo-mocks/mock-routes
     oppijanumerorekisteri-mocks/mock-routes
     koodisto-mocks/mock-routes
     eperusteet-mocks/mock-routes
     koski-mocks/mock-routes
-    organisaatio-mocks/mock-routes))
+    organisaatio-mocks/mock-routes)
