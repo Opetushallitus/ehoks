6171f3e414d3557e224e33f7edd6bba38faf2563
Tomi Katajanheimo <tomi@katajanheimo.com>
2024-12-09 15:32:24 +0200

    WIP assume role [skip ci]

--- scripts/lampi-export/lampi-export.sh
+++ scripts/lampi-export/lampi-export.sh
@@ -149,7 +149,7 @@ upload_file_to_lampi() {
     log "INFO" "Uploading file" $file_s3_key "to local S3 $file_s3_url"
     local -r obj_version=$(aws s3api put-object --region eu-west-1 --body "$file" --bucket "$local_s3_bucket" --key "$file_s3_key" --output json | jq -r .VersionId)
     log "INFO" "Uploading file $file_s3_key with version $obj_version to Lampi s3://$lampi_s3_bucket/$file_s3_key"
-    AWS_ACCESS_KEY_ID="$access_key_id" AWS_SECRET_ACCESS_KEY="$secret_access_key" AWS_SESSION_TOKEN="$session_token" aws s3 cp "$file_s3_url" "s3://$lampi_s3_bucket/$file_s3_key"
+    aws s3 cp "$file_s3_url" - | AWS_ACCESS_KEY_ID="$access_key_id" AWS_SECRET_ACCESS_KEY="$secret_access_key" AWS_SESSION_TOKEN="$session_token" aws s3 cp - "s3://$lampi_s3_bucket/$file_s3_key"
     echo $(lampi_manifest_item "$file_s3_key" "$obj_version")
 }
 
