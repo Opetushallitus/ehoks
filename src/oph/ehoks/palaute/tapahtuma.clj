55ea0ec41cc12a8db5b9bea85c0b55086a1a4653
Severi Jääskeläinen <severi.jaaskelainen@gofore.com>
2024-12-16 19:02:19 +0200

    Generalize `tapahtuma/build` and add `tapatuma/build-and-insert!`

--- src/oph/ehoks/palaute/tapahtuma.clj
+++ src/oph/ehoks/palaute/tapahtuma.clj
@@ -1,17 +1,30 @@
 (ns oph.ehoks.palaute.tapahtuma
   (:require [hugsql.core :as hugsql]
-            [oph.ehoks.utils :as utils]
-            [medley.core :refer [map-vals]]))
+            [oph.ehoks.db :as db]
+            [oph.ehoks.utils :as utils]))
 
 (hugsql/def-db-fns "oph/ehoks/db/sql/palautetapahtuma.sql")
 
 (defn build
-  [{:keys [hoks jakso existing-palaute] :as ctx} state field reason palaute]
-  {:pre [(some? state)]}
-  {:palaute-id      (:id palaute)
-   :vanha-tila      (utils/to-underscore-str
-                      (or (:tila existing-palaute) state))
-   :uusi-tila       (utils/to-underscore-str state)
-   :tapahtumatyyppi "hoks_tallennus"
-   :syy             (utils/to-underscore-str (or reason :hoks-tallennettu))
-   :lisatiedot      (map-vals str (select-keys (merge jakso hoks) [field]))})
+  ([{:keys [existing-palaute] :as ctx} reason lisatiedot]
+    (build ctx (:tila existing-palaute) reason lisatiedot nil))
+  ([ctx state reason lisatiedot]
+    (build ctx state reason lisatiedot nil))
+  ([{:keys [tapahtumatyyppi existing-palaute] :as ctx}
+    state reason lisatiedot palaute]
+    {:pre [(some? tapahtumatyyppi) (some? state)]}
+    {:palaute-id      (or (:id palaute) (:id existing-palaute))
+     :vanha-tila      (utils/to-underscore-str
+                        (or (:tila existing-palaute) state))
+     :uusi-tila       (utils/to-underscore-str state)
+     :tapahtumatyyppi (utils/to-underscore-str tapahtumatyyppi)
+     :syy             (utils/to-underscore-str (or reason :hoks-tallennettu))
+     :lisatiedot      lisatiedot}))
+
+(defn build-and-insert!
+  ([{:keys [existing-palaute] :as ctx} reason lisatiedot]
+    (build-and-insert! ctx (:tila existing-palaute) reason lisatiedot nil))
+  ([ctx state reason lisatiedot]
+    (build-and-insert! ctx state reason lisatiedot nil))
+  ([{:keys [tx] :as ctx} state reason lisatiedot palaute]
+    (insert! (or tx db/spec) (build ctx state reason lisatiedot palaute))))
