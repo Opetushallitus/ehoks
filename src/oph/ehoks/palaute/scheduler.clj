8686654356989868776550f8d48b6a8402be8c2d
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2025-03-20 15:31:53 +0200

    EH-1804: add scheduled task for HOKS reinitiation

--- src/oph/ehoks/palaute/scheduler.clj
+++ src/oph/ehoks/palaute/scheduler.clj
@@ -3,7 +3,7 @@
             [clojure.tools.logging :as log]
             [oph.ehoks.palaute.vastaajatunnus :as palaute])
   (:import (java.lang AutoCloseable)
-           (java.time Instant LocalTime ZonedDateTime ZoneId Period)))
+           (java.time Instant LocalTime ZonedDateTime ZoneId Period Duration)))
 
 (defn daily-actions!
   "Run all palaute checks that need to be run on a daily basis."
@@ -19,16 +19,25 @@
   (log/info "Done palaute daily actions.")
   true)
 
+(defn reinit-uninitiated-hoksen!
+  "Look for HOKSes that are missing corresponding palaute records and
+  initiate all palautteet for those HOKSes."
+  [opts]
+  (log/info "Initialising palautteet for next batch of uninitialised HOKSes.")
+  )
+
 (defn time->instant
   "Converts a specific time of day into an instant on today"
   [hour minute sec]
   (-> (LocalTime/of hour minute sec)
       (.adjustInto (ZonedDateTime/now (ZoneId/of "Europe/Helsinki")))
-      Instant/from))
+      (Instant/from)))
 
 (def schedules
   [{:action daily-actions!
-    :start (time->instant 6 0 0) :period (Period/ofDays 1)}])
+    :start (time->instant 6 0 0) :period (Period/ofDays 1)}
+   {:action reinit-uninitiated-hoksen!
+    :start (time->instant 0 0 0) :period (Duration/ofHours 1)}])
 
 (defn run-scheduler!
   "Run a given action periodically starting at start-time and repeating
