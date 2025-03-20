30fc682f949f3c44864ef370af366aae1d91cbb7
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2025-03-20 19:59:03 +0200

    EH-1804: test that reinitiation copes with missing opiskeluoikeus

--- test/oph/ehoks/palaute/initiation_test.clj
+++ test/oph/ehoks/palaute/initiation_test.clj
@@ -7,10 +7,44 @@
             [oph.ehoks.external.koski :as koski]
             [oph.ehoks.external.koski-test :as koski-test]
             [oph.ehoks.palaute :as palaute]
-            [oph.ehoks.palaute.initiation :as init]))
+            [oph.ehoks.palaute.tapahtuma :as tapahtuma]
+            [oph.ehoks.palaute.initiation :as init]
+            [oph.ehoks.utils.date :as date])
+  (:import (java.time LocalDate)))
 
 (use-fixtures :once test-utils/migrate-database)
 (use-fixtures :each test-utils/empty-both-dbs-after-test)
+
+(deftest test-missing-opiskeluoikeus-reinit-palautteet-for-uninitiated-hokses!
+  (hoks/save! hoks-test/hoks-1)
+  (testing "palaute reinitiation succeeds with missing opiskeluoikeus"
+    (with-redefs [koski/get-opiskeluoikeus! (fn [oid] nil)
+                  date/now (constantly (LocalDate/of 2021 7 1))]
+      (init/reinit-palautteet-for-uninitiated-hokses! 2)
+      (is (= (->> {:hoks-id (:id hoks-test/hoks-1)
+                   :kyselytyypit ["aloittaneet" "valmistuneet"
+                                  "tyopaikkajakson_suorittaneet"]}
+                  (tapahtuma/get-all-by-hoks-id-and-kyselytyypit! db/spec)
+                  (map (juxt :uusi-tila :syy)))
+             [["odottaa_kasittelya" "ei_loydy"]
+              ["odottaa_kasittelya" "ei_loydy"]
+              ["odottaa_kasittelya" "ei_loydy"]
+              ["odottaa_kasittelya" "ei_loydy"]
+              ["odottaa_kasittelya" "ei_loydy"]
+              ["odottaa_kasittelya" "ei_loydy"]
+              ["odottaa_kasittelya" "ei_loydy"]]))
+      (is (= (->> {:hoks-id (:id hoks-test/hoks-1)
+                   :kyselytyypit ["aloittaneet" "valmistuneet"
+                                  "tyopaikkajakson_suorittaneet"]}
+                  (palaute/get-by-hoks-id-and-kyselytyypit! db/spec)
+                  (map (juxt :tila :kyselytyyppi)))
+             [["odottaa_kasittelya" "aloittaneet"]
+              ["odottaa_kasittelya" "valmistuneet"]
+              ["odottaa_kasittelya" "tyopaikkajakson_suorittaneet"]
+              ["odottaa_kasittelya" "tyopaikkajakson_suorittaneet"]
+              ["odottaa_kasittelya" "tyopaikkajakson_suorittaneet"]
+              ["odottaa_kasittelya" "tyopaikkajakson_suorittaneet"]
+              ["odottaa_kasittelya" "tyopaikkajakson_suorittaneet"]])))))
 
 (deftest test-reinit-palautteet-for-uninitiated-hokses!
   (with-redefs [koski/get-opiskeluoikeus-info-raw
