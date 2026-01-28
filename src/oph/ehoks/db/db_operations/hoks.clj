0867cf4b68d687f140bb6986ef836620d90ae56b
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2026-01-28 11:01:16 +0200

    EH-1690: ohjaajan yhteystiedot deletion endpoint returns list instead of set

--- src/oph/ehoks/db/db_operations/hoks.clj
+++ src/oph/ehoks/db/db_operations/hoks.clj
@@ -740,7 +740,7 @@
                          :vastuullinen_tyopaikka_ohjaaja_puhelinnumero nil}
                         ["id = ?" (:tjk-id jakso)]
                         conn))
-      (set (map :hankkimistapa-id jaksot)))))
+      (mapv :hankkimistapa-id jaksot))))
 
 ;; TODO: nää ei oikeasti kuuluis db-operations-nimiavaruuteen vaan johonkin
 ;; ylempään logiikkakerrokseen.
