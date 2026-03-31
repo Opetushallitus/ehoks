c38830f523cb5d3c1346d5aea9e64cc4d807b56b
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2026-03-31 13:56:25 +0300

    EH-1723: handle-palaute-waiting-for-sending-status!

--- src/oph/ehoks/db/sql/palauteviesti.sql
+++ src/oph/ehoks/db/sql/palauteviesti.sql
@@ -21,11 +21,24 @@ RETURNING id
 
 SELECT	p.*,
 	p.tila AS palaute_tila,
-	pv.*,
-	pv.tila AS viesti_tila
+	pv.id AS viesti_id,
+	pv.ulkoinen_tunniste,
+	pv.viestityyppi,
+	pv.tila AS viesti_tila,
+	pv.vastaanottaja
 FROM palaute_viestit pv
 LEFT JOIN palautteet p ON (pv.palaute_id = p.id)
 WHERE pv.viestityyppi in (:v*:viestityypit)
 AND pv.tila = :tila
 AND pv.deleted_at IS NULL
 AND p.deleted_at IS NULL
+
+-- :name update-tila!
+-- :doc update tila for palaute message.
+
+UPDATE	palaute_viestit
+SET	tila = :tila,
+	updated_at = now()
+WHERE	id = :id
+RETURNING id
+
