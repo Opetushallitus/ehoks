750d226816314ee18bfad0f61dd8122b2d8ea92d
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2026-04-08 11:13:41 +0300

    EH-1723: PATCH support in http-client

--- resources/test/src/oph/ehoks/external/http_client.clj
+++ resources/test/src/oph/ehoks/external/http_client.clj
@@ -2,38 +2,38 @@
   (:refer-clojure :exclude [get])
   (:require [clj-http.client :as client]))
 
+(def original-client-functions
+  {:delete client/delete
+   :get client/get
+   :post client/post
+   :patch client/patch})
+
 (def ^:private client-functions
-  (atom {:delete client/delete
-         :get client/get
-         :post client/post}))
+  (atom original-client-functions))
 
 (defn get-client-functions [] @client-functions)
-
-(defn reset-functions! []
-  (reset! client-functions {:delete client/delete
-                            :get client/get
-                            :post client/post}))
-
-(defn restore-functions! [fns]
-  (reset! client-functions fns))
+(defn restore-functions! [fns] (reset! client-functions fns))
+(defn reset-functions! [] (restore-functions! original-client-functions))
 
 (defn delete [url options]
   (or ((:delete @client-functions) url options) (client/delete url options)))
 
-(defn set-delete! [f]
-  (swap! client-functions assoc :delete f))
+(defn set-delete! [f] (swap! client-functions assoc :delete f))
 
 (defn get [url options]
   (or ((:get @client-functions) url options) (client/get url options)))
 
-(defn set-get! [f]
-  (swap! client-functions assoc :get f))
+(defn set-get! [f] (swap! client-functions assoc :get f))
 
 (defn post [url options]
   (or ((:post @client-functions) url options) (client/post url options)))
 
-(defn set-post! [f]
-  (swap! client-functions assoc :post f))
+(defn set-post! [f] (swap! client-functions assoc :post f))
+
+(defn patch [url options]
+  (or ((:patch @client-functions) url options) (client/patch url options)))
+
+(defn set-patch! [f] (swap! client-functions assoc :patch f))
 
 (defmacro with-mock-responses [[get-response post-response] & body]
   `(do
